6eefe8a7c5771c26907cdcad720838a061ac3ace
Matt Fuller <fuller@adobe.com>
2016-05-28 14:59:09 -0400

    improved cacher with retries

--- helpers/cache.js
+++ helpers/cache.js
@@ -1,21 +1,67 @@
+var async = require('async');
+
+var MAX_RETRIES = 15;		// Maximum times to retry calling the AWS API
+var MAX_TIMEOUT = 20000;	// How long to wait for each AWS response before failing
+
 var cacher = function(cache, service, command, callback) {
 	if (!service.config.region) { service.config.region = 'global'; }
 
-	if (cache[service.config.endpoint] &&
-		cache[service.config.endpoint][command] &&
-		cache[service.config.endpoint][command][service.config.region]) {
-		return callback(null, cache[service.config.endpoint][command][service.config.region]);
-	}
+	var endpoint = service.config.endpoint;
+	var region = service.config.region;
 
-	service[command](function(err, data){
-		if (!err && data) {
-			if (!cache[service.config.endpoint]) { cache[service.config.endpoint] = {}; }
-			if (!cache[service.config.endpoint][command]) { cache[service.config.endpoint][command] = {}; }
-			cache[service.config.endpoint][command][service.config.region] = data;
+	if (cache[endpoint] &&
+		cache[endpoint][command] &&
+		cache[endpoint][command][region]) {
+
+		if (cache[endpoint][command][region].data) {
+			return callback(null, cache[endpoint][command][region].data);
 		}
 
-		callback(err, data);
-	});
+		if (cache[endpoint][command][region].err) {
+			return callback(cache[endpoint][command][region].err);
+		}
+
+		if (cache[endpoint][command][region].calling) {
+			cache[endpoint][command][region].callbacks.push(callback);
+		}
+	}
+
+	if (!cache[endpoint]) { cache[endpoint] = {}; }
+	if (!cache[endpoint][command]) { cache[endpoint][command] = {}; }
+	
+	if (!cache[endpoint][command][region]) {
+		cache[endpoint][command][region] = {
+			calling: true,
+			callbacks: [callback]
+		};
+
+		var cbCalled = false;
+		var tryNum = 0;
+
+		async.retry(MAX_RETRIES, async.timeout(function(cb){
+			service[command](function(err, data){
+				if (!cbCalled) cb(err,data);
+			});
+		}, MAX_TIMEOUT), function(err, data) {
+			cbCalled = true;
+
+			if (!err && data) {
+				cache[endpoint][command][region].data = data;
+
+				for (c in cache[endpoint][command][region].callbacks) {
+					cache[endpoint][command][region].callbacks[c](null, data);
+				}
+			} else {
+				cache[endpoint][command][region].err = (err || 'No data in response');
+
+				for (c in cache[endpoint][command][region].callbacks) {
+					cache[endpoint][command][region].callbacks[c]((err || 'No data in response'));
+				}
+			}
+
+			cache[endpoint][command][region].callbacks = [];
+		});
+	}
 }
 
 module.exports = cacher;
