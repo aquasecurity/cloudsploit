29b75e131c6ecf6ce11f3e73a696221bf0077e98
Citizen Josh <citizenjosh@users.noreply.github.com>
2016-10-19 10:29:56 -0400

    Update region.js with US East (Ohio) us-east-2

--- helpers/regions.js
+++ helpers/regions.js
@@ -1,5 +1,6 @@
 var regions = [
 	'us-east-1',		// Northern Virginia
+	'us-east-2',		// US East (Ohio)
 	'us-west-1',		// Northern California
 	'us-west-2',		// Oregon
 	'ap-northeast-1',	// Asia Pacific (Tokyo)
@@ -21,11 +22,11 @@ module.exports = {
 	kms: regions,
 	vpc: regions,
 	rds: regions,
-	apigateway: ['us-east-1', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-southeast-1', 'ap-northeast-1'],
+	apigateway: ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-southeast-1', 'ap-northeast-1'],
 	cloudwatch: regions,
 	dynamodb: regions,
-	ecr: ['us-east-1', 'us-west-2', 'eu-west-1'],
+	ecr: ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1'],
 	ecs: ['us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2'],
-	ses: ['us-east-1', 'us-west-2', 'eu-west-1'],
-	flowlogs: ['us-east-1', 'us-west-1', 'us-west-2', 'ap-northeast-1', 'ap-southeast-2', 'ap-southeast-1', 'eu-west-1', 'eu-central-1']
+	ses: ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1'],
+	flowlogs: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ap-northeast-1', 'ap-southeast-2', 'ap-southeast-1', 'eu-west-1', 'eu-central-1']
 };
