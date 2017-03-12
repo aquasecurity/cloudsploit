75e9061c5826c92e4d81360c787e2c93bf8f0cfd
Matt Fuller <matthewdf10@gmail.com>
2017-03-12 15:24:47 -0400

    small fixes for multi-subnet plugin

--- plugins/vpc/multipleSubnets.js
+++ plugins/vpc/multipleSubnets.js
@@ -3,17 +3,19 @@ var async = require('async');
 var helpers = require('../../helpers');
 
 module.exports = {
-	title: 'Multiple networks/subnets are used in a VPC',
+	title: 'VPC Multiple Subnets',
 	category: 'VPC',
-	description: 'Ensures that VPC has multiple networks which provides a layered architecture',
-	more_info: 'A single network within a VPC exposes of a risk of increasing the impact radius in case of a compromisation.',
+	description: 'Ensures that VPCs have multiple networks to provide a layered architecture',
+	more_info: 'A single network within a VPC increases the risk of a broader blast radius in the event of a compromise.',
 	link: 'https://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Subnets.html#SubnetSecurity',
-	recommended_action: 'Create multiple networks/subnets and change your architecture',
+	recommended_action: 'Create multiple networks/subnets in each VPC and change the architecture to take advantage of public and private tiers.',
 
 	run: function(AWSConfig, cache, includeSource, callback) {
 		var results = [];
 		var source = {};
-		var vpcId;
+
+		if (includeSource) source['describeVpcs'] = {};
+		if (includeSource) source['describeSubnets'] = {};
 
 		async.eachLimit(helpers.regions.vpc, helpers.MAX_REGIONS_AT_A_TIME, function(region, rcb){
 			var LocalAWSConfig = JSON.parse(JSON.stringify(AWSConfig));
@@ -22,14 +24,10 @@ module.exports = {
 			LocalAWSConfig.region = region;
 			var ec2 = new AWS.EC2(LocalAWSConfig);
 
-			var params = {
+			helpers.cache(cache, ec2, 'describeVpcs', function(err, data) {
+				if (includeSource) source['describeVpcs'][region] = {error: err, data: data};
 
-			};
-
-			ec2.describeVpcs(params, function(err, data){
-				if (includeSource) source[region] = {error: err, data: data};
-
-				if (err || !data || !data.Vpcs) {
+				if (err || !data || !data.Vpcs || !data.Vpcs.length) {
 					results.push({
 						status: 3,
 						message: 'Unable to query for VPCs',
@@ -50,8 +48,17 @@ module.exports = {
 					return rcb();
 				} else {
 					// Looks like we have only one VPC
-					vpcId = data.Vpcs[0].VpcId;
+					var vpcId = data.Vpcs[0].VpcId;
 
+					if (!vpcId) {
+						results.push({
+							status: 3,
+							message: 'Unable to query for subnets for VPC.',
+							region: region
+						});
+
+						return rcb();
+					}
 
 					// Check for the multiple subnets in that single VPC
 					var params = {
@@ -63,51 +70,42 @@ module.exports = {
 								]
 							}
 						]
-					}
+					};
 
-					if (!vpcId) {
-						results.push({
-							status: 3,
-							message: 'Unable to query for Subnets.',
-							region: region
-						});
+					ec2.describeSubnets(params, function(subnetErr, subnetData) {
+						if (includeSource) source['describeSubnets'][region] = {error: subnetErr, data: subnetData};
 
-						return rcb();
-					}
-
-					ec2.describeSubnets(params, function(err, data) {
-						if (includeSource) source[region] = {error: err, data: data};
-
-						if (err || !data || !data.Subnets) {
+						if (subnetErr || !subnetData || !subnetData.Subnets || !subnetData.Subnets.length) {
 							results.push({
 								status: 3,
-								message: 'Unable to query for Subnets.',
-								region: region
+								message: 'Unable to query for subnets in VPC.',
+								region: region,
+								resource: vpcId
 							});
 
 							return rcb();
 						}
 
-						if (data.Subnets.length > 2) {
+						if (subnetData.Subnets.length > 2) {
 							results.push({
 								status: 0,
-								message: 'There are ' + data.Subnets.length + ' different CIDR Blocks used in one VPC.',
+								message: 'There are ' + subnetData.Subnets.length + ' different subnets used in one VPC.',
 								region: region,
 								resource: vpcId
 							});
-						} else if (data.Subnets.length > 1) {
+						} else if (subnetData.Subnets.length > 1) {
 							results.push({
 								status: 1,
-								message: 'Using ' + data.Subnets.length + ' subnets may not be sufficient for a multi-layered architecture',
+								message: 'Using ' + subnetData.Subnets.length + ' subnets may not be sufficient for a multi-layered architecture',
 								region: region,
 								resource: vpcId
 							});
 						} else {
 							results.push({
 								status: 2,
-								message: 'Only one subnet, one VPC is used.',
+								message: 'Only one subnet (' + subnetData.Subnets[0].SubnetId + ') in one VPC is used.',
 								region: region,
-								resource: vpcId + ' & ' + data.Subnets[0].SubnetId
+								resource: vpcId
 							});
 						}
 
