fc56be5cc9bcfb500b9ecdbe9988132e88848156
Matt Fuller <matthewdf10@gmail.com>
2017-11-22 19:19:54 -0500

    EBS Volume Encryption Max Results

--- plugins/ec2/ebsEncryptionEnabled.js
+++ plugins/ec2/ebsEncryptionEnabled.js
@@ -2,9 +2,9 @@ var async = require('async');
 var helpers = require('../../helpers');
 
 module.exports = {
-    title: 'EBS Encryption Enabled ',
+    title: 'EBS Encryption Enabled',
     category: 'EC2',
-    description: 'Ensures EBS volumes are encrypted at rest.',
+    description: 'Ensures EBS volumes are encrypted at rest',
     more_info: 'EBS volumes should have at-rest encryption enabled through AWS using KMS. If the volume is used for a root volume, the instance must be launched from an AMI that has been encrypted as well.',
     link: 'http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/EBSEncryption.html',
     recommended_action: 'Enable encryption for EBS volumes.',
@@ -31,16 +31,23 @@ module.exports = {
                 return rcb();
             }
 
+            var unencryptedVolumes = [];
+
             describeVolumes.data.forEach(function(Volume){
-                if (Volume.Encrypted){
-                    helpers.addResult(results, 0,
-                        'Volume Encrypted ' + Volume.VolumeId, region);
-                }
-                else{
-                    helpers.addResult(results, 2,
-                        'Volume Not Encrypted ' + Volume.VolumeId, region);
+                if (!Volume.Encrypted){
+                    unencryptedVolumes.push(Volume.VolumeId);
                 }
             });
+
+            if (unencryptedVolumes.length > 20) {
+                helpers.addResult(results, 2, 'More than 20 EBS volumes are unencrypted', region);
+            } else if (unencryptedVolumes.length) {
+                for (u in unencryptedVolumes) {
+                    helpers.addResult(results, 2, 'EBS volume is unencrypted', region, unencryptedVolumes[u]);
+                }
+            } else {
+                helpers.addResult(results, 0, 'No unencrypted volumes found', region);
+            }
 
             rcb();
         }, function(){
