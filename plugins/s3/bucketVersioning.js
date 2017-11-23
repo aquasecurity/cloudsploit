764c1a1b7d4ec2693847ab41b8b0c3cf5ca22df0
Abhishek Jaiswal <2536001+jkabhishek@users.noreply.github.com>
2017-11-23 06:03:02 +0530

    Plugin: S3 Bucket Versioning (#74)

---
+++ plugins/s3/bucketVersioning.js
@@ -0,0 +1,64 @@
+var async = require('async');
+var helpers = require('../../helpers');
+
+module.exports = {
+    title: 'S3 Bucket Versioning',
+    category: 'S3',
+    description: 'Ensures object versioning is enabled on S3 buckets',
+    more_info: 'Object versioning can help protect against the overwriting of \
+                objects or data loss in the event of a compromise.',
+    recommended_action: 'Enable object versioning for buckets with \
+                        sensitive contents at a minimum and for all buckets \
+                        ideally.',
+    link: 'http://docs.aws.amazon.com/AmazonS3/latest/dev/Versioning.html',
+    apis: ['S3:listBuckets', 'S3:getBucketVersioning'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+
+        var region = 'us-east-1';
+
+        var listBuckets = helpers.addSource(cache, source,
+            ['s3', 'listBuckets', region]);
+
+        if (!listBuckets) return callback(null, results, source);
+
+        if (listBuckets.err || !listBuckets.data) {
+            helpers.addResult(results, 3,
+                'Unable to query for S3 buckets: ' + helpers.addError(listBuckets));
+            return callback(null, results, source);
+        }
+
+        if (!listBuckets.data.length) {
+            helpers.addResult(results, 0, 'No S3 buckets to check');
+            return callback(null, results, source);
+        }
+
+        listBuckets.data.forEach(function(bucket){
+            var getBucketVersioning = helpers.addSource(cache, source,
+                ['s3', 'getBucketVersioning', region, bucket.Name]);
+
+            if (!getBucketVersioning || getBucketVersioning.err || !getBucketVersioning.data) {
+                helpers.addResult(results, 3,
+                    'Error querying bucket versioning for : ' + bucket.Name,
+                    region, 'arn:aws:s3:::' + bucket.Name)
+            }
+
+            if(getBucketVersioning.Status == 'Suspended'){
+                helpers.addResult(results, 2,
+                    'Bucket : ' + bucket.Name + 'has versioning disabled',
+                    region, 'arn:aws:s3:::' + bucket.Name)
+            }
+
+            if(getBucketVersioning.Status == 'Enabled'){
+                helpers.addResult(results, 0,
+                    'Bucket : ' + bucket.Name + 'has versioning enabled',
+                    region, 'arn:aws:s3:::' + bucket.Name)
+            }
+
+        });
+
+        callback(null, results, source);
+    }
+};
