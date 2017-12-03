b4b4ceae4446c7bf3cbb86d6d7d574fbce121107
Matt Fuller <matthewdf10@gmail.com>
2017-12-03 10:27:41 -0700

    new plugin: elb no instances

--- plugins/elb/elbNoInstances.js
+++ plugins/elb/elbNoInstances.js
@@ -2,17 +2,15 @@ var async = require('async');
 var helpers = require('../../helpers');
 
 module.exports = {
-    title: 'ELB HTTPS Only',
+    title: 'ELB No Instances',
     category: 'ELB',
-    description: 'Ensures ELBs are configured to only accept' + 
-                 ' connections on HTTPS ports.',
-    more_info: 'For maximum security, ELBs can be configured to only'+
-                ' accept HTTPS connections. Standard HTTP connections '+
-                ' will be blocked. This should only be done if the '+
-                ' client application is configured to query HTTPS '+
-                ' directly and not rely on a redirect from HTTP.',
-    link: 'http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/elb-security-policy-options.html',
-    recommended_action: 'Remove non-HTTPS listeners from load balancer.',
+    description: 'Detects ELBs that have no backend instances attached',
+    more_info: 'All ELBs should have backend server resources. ' +
+               'Those without any are consuming costs without providing ' +
+               'any functionality. Additionally, old ELBs with no instances ' +
+               'present a security concern if new instances are accidentally attached.',
+    link: 'http://docs.aws.amazon.com/elasticloadbalancing/latest/classic/elb-backend-instances.html',
+    recommended_action: 'Delete old ELBs that no longer have backend resources.',
     apis: ['ELB:describeLoadBalancers', 'STS:getCallerIdentity'],
 
     run: function(cache, settings, callback) {
@@ -43,26 +41,10 @@ module.exports = {
                               region + ':' + accountId + ':' +
                               'loadbalancer/' + lb.LoadBalancerName;
 
-                // loop through listeners
-                var non_https_listner = [];
-                lb.ListenerDescriptions.forEach(function(listener){
-                    // if it is not https add errors to results
-                    if (listener.Listener.Protocol != 'HTTPS'){
-                        non_https_listner.push(
-                            listener.Listener.Protocol + ' / ' +  
-                            listener.Listener.LoadBalancerPort
-                        );
-                    }
-
-                });
-                if (non_https_listner){
-                    //helpers.addResult(results, 2, non_https_listner.join(', '), region);
-                    msg = "The following listeners are not using HTTPS: ";
-                    helpers.addResult(
-                        results, 2, msg + non_https_listner.join(', '), region, elbArn
-                    );
+                if (lb.Instances.length){
+                    helpers.addResult(results, 0, 'ELB has ' + lb.Instances.length + ' backend instances', region, elbArn);
                 }else{
-                    helpers.addResult(results, 0, 'No listeners found', region, elbArn);
+                    helpers.addResult(results, 1, 'ELB does not have backend instances', region, elbArn);
                 }
                 cb();
             }, function(){
