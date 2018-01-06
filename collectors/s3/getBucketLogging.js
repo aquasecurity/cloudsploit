0a7889c928ae62e690cb95a2ff4c3e051eedc3ee
AbhishekJ <aj355y@att.com>
2018-01-06 13:31:53 +0530

    S3 Bucket Logging
    Ensures S3 bucket logging is enabled for S3 buckets

--- collectors/s3/getBucketLogging.js
+++ collectors/s3/getBucketLogging.js
@@ -1,5 +1,5 @@
 var index = require(__dirname + '/index.js');
 
 module.exports = function(AWSConfig, collection, callback) {
-	index('getBucketLogging', true, AWSConfig, collection, callback);
+	index('getBucketLogging', false, AWSConfig, collection, callback);
 };
