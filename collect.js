eb943c65cb312f214743fab01524d87912507962
Abhishek Jaiswal <2536001+jkabhishek@users.noreply.github.com>
2018-04-01 01:52:21 +0530

    Plugin: KMS Key Policy (#91)
    added plugin for KMS keyPolicy checks
    matched calling user acount and keys user are same or not,
    user should not be more that 10
    it should not be *

--- collect.js
+++ collect.js
@@ -157,7 +157,7 @@ var calls = {
 	KMS: {
 		listKeys: {
 			property: 'Keys'
-		}
+		},
 	},
 	Lambda: {
 		listFunctions: {
@@ -331,6 +331,11 @@ var postcalls = [
 				reliesOnCall: 'listKeys',
 				filterKey: 'KeyId',
 				filterValue: 'KeyId'
+			},
+			getKeyPolicy: {
+				reliesOnService: 'kms',
+				reliesOnCall: 'listKeys',
+				override: true
 			}
 		},
 		SES: {
--- exports.js
+++ exports.js
@@ -82,6 +82,7 @@ module.exports = {
 
     'kmsKeyRotation'                : require(__dirname + '/plugins/kms/kmsKeyRotation.js'),
     'kmsScheduledDeletion'          : require(__dirname + '/plugins/kms/kmsScheduledDeletion.js'),
+    'kmsKeyPolicy'                  : require(__dirname + '/plugins/kms/kmsKeyPolicy.js'),
 
     'rdsAutomatedBackups'           : require(__dirname + '/plugins/rds/rdsAutomatedBackups.js'),
     'rdsEncryptionEnabled'          : require(__dirname + '/plugins/rds/rdsEncryptionEnabled.js'),
