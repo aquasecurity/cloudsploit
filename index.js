f8291dc1395cb6072682ead58a51acb612aa9761
Cariel Cohen <ccohen55@gmail.com>
2018-07-01 17:58:53 -0700

    Adding Instance Max Count plugin, checks for running instances over the instance_count_threshold (Resetting exports and index.js to defaults)

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ collector(AWSConfig, {api_calls: apiCalls, skip_regions: skipRegions}, function(
                     statusWord + '\t' + results[r].message);
             }
 
-            callback(err);
+            setTimeout(function() { callback(err); }, 0);
         });
     }, function(err){
         if (err) return console.log(err);
