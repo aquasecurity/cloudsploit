2aa95ce2f728a9b505f0dacb57ccc7d3a5c2adce
Cariel Cohen <ccohen55@gmail.com>
2018-07-01 17:48:29 -0700

    Adding Instance Max Count plugin, checks for running instances over the instance_count_threshold

--- plugins/ec2/instanceMaxCount.js
+++ plugins/ec2/instanceMaxCount.js
@@ -10,9 +10,15 @@ module.exports = {
 	recommended_action: 'Go to the ec2 dashboard and audit the instances running, apparently additional unauthorized instances have been launched.',
 	apis: ['EC2:describeInstances'],
 	settings: {
-		instance_count_threshold: {
-			name: 'Instance Count Threshold',
-			description: 'Checks for the number of running instances in an account and triggers a failing result if it exceeds a certain count',
+        instance_count_global_threshold: {
+            name: 'Instance Count Global Threshold',
+            description: 'Checks for the number of running instances globally and triggers a failing result if it exceeds the specified count',
+            regex: '^[1-2]{1}[0-9]{0,2}$',
+            default: 200
+        },
+		instance_count_region_threshold: {
+			name: 'Instance Count Region Threshold',
+			description: 'Checks for the number of running instances in each region and triggers a failing result if it exceeds the specified count',
 			regex: '^[1-2]{1}[0-9]{0,2}$',
 			default: 100
 		}
@@ -20,14 +26,16 @@ module.exports = {
 
 	run: function(cache, settings, callback) {
 		var config = {
-            instance_count_threshold: settings.instance_count_threshold || this.settings.instance_count_threshold.default
-		};
+            instance_count_global_threshold: settings.instance_count_global_threshold || this.settings.instance_count_global_threshold.default,
+            instance_count_region_threshold: settings.instance_count_region_threshold || this.settings.instance_count_region_threshold.default
+        };
 
 		var custom = helpers.isCustom(settings, this.settings);
 
 		var results = [];
 		var source = {};
 		var instance_count = 0;
+        var instance_count_global_threshold = 0;
 
 		async.each(helpers.regions.ec2, function(region, rcb){
 			var describeInstances = helpers.addSource(cache, source,
@@ -53,25 +61,34 @@ module.exports = {
 					var instance = describeInstances.data[i].Instances[j];
 
 					if (instance.State.Name == "running") {
+                        instance_count_global_threshold +=1;
                         instance_count +=1;
 					}
 				}
 			}
 
-            // Too many results to print individually
-            if (instance_count > config.instance_count_threshold) {
+			// Print region results
+            if (instance_count > config.instance_count_region_threshold) {
                 results = [];
-
                 helpers.addResult(results, 2,
-                    'Over ' + config.instance_count_threshold + ' EC2 instances running, exceeds limits!', region, null, custom);
+                    'Over ' + config.instance_count_region_threshold + ' EC2 instances running, exceeds ' + region + ' limits!', region, null, custom);
             } else {
 				helpers.addResult(results, 0,
 					'All ' + instance_count + ' instances are within the expected count.', region);
 			}
 
 			rcb();
-		}, function(){
-			callback(null, results, source);
 		});
+
+        // Print global results
+        if (instance_count_global_threshold > config.instance_count_global_threshold) {
+            helpers.addResult(results, 2,
+                'Over ' + config.instance_count_global_threshold + ' EC2 instances running in all regions, exceeds limits!', null, null, custom);
+        } else {
+            helpers.addResult(results, 0,
+                'All ' + instance_count_global_threshold + ' instances in the account are within the global expected count.', null);
+        }
+
+        callback(null, results, source);
 	}
 };
