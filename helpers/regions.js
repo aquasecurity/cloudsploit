8717ea6df6443d79a18d559bc6e58386a374ee99
Jason Cornell <jcornell123@users.noreply.github.com>
2018-07-24 21:30:47 -0400

    Updating AWS Service Regions (#124)
    * Region update
    
    Reordered the regions to match AWS list.
    updated services that are available in all regions to reflect that.
    
    * fixing regions.js file
    
    * fixing commas

--- helpers/regions.js
+++ helpers/regions.js
@@ -1,19 +1,21 @@
+// Source: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html
+
 var regions = [
 	'us-east-1',		// Northern Virginia
 	'us-east-2',		// Ohio
 	'us-west-1',		// Northern California
 	'us-west-2',		// Oregon
+    	'ca-central-1',		// Canada (Montreal)
+    	'eu-central-1',		// EU (Frankfurt)
+    	'eu-west-1',		// EU (Ireland)
+    	'eu-west-2',		// London
+    	'eu-west-3',		// Paris
 	'ap-northeast-1',	// Asia Pacific (Tokyo)
 	'ap-northeast-2',	// Asia Pacific (Seoul)
 	'ap-southeast-1',	// Asia Pacific (Singapore)
 	'ap-southeast-2',	// Asia Pacific (Sydney)
-	'eu-central-1',		// EU (Frankfurt)
-	'eu-west-1',		// EU (Ireland)
-	'eu-west-2',		// London
-	'eu-west-3',		// Paris
-	'sa-east-1',		// South America (São Paulo)
-	'ap-south-1',		// Mumbai
-	'ca-central-1'		// Canada (Montreal)
+    	'ap-south-1',		// Asia Pacific (Mumbai)
+	'sa-east-1'		// South America (São Paulo)
 ];
 
 module.exports = {
@@ -36,30 +38,18 @@ module.exports = {
 	flowlogs: regions,
 	rds: regions,
 	redshift: regions,
-	apigateway: [
-		'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-		'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1',
-		'ap-southeast-1', 'ap-northeast-1'],
+	apigateway: regions,
 	cloudwatch: regions,
 	dynamodb: regions,
-	ecr: [
-		'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ca-central-1',
-		'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1',
-		'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2'],
-	ecs: [
-		'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ca-central-1',
-		'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-central-1',
-		'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2'],
+	ecr: regions,
+	ecs: regions,
 	ses: [
 		'us-east-1', 'us-west-2',
 		'eu-west-1'],
 	sns: regions,
 	sqs: regions,
 	sts: ['us-east-1'],
-	lambda: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-			 'eu-west-1', 'eu-central-1', 'eu-west-2', 'eu-west-3', 'ap-southeast-1',
-			 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2',
-			 'ap-south-1'],
+	lambda: regions,
 	directconnect: regions,
 	directoryservice: ['us-east-1', 'us-east-2', 'us-west-2', 'us-west-1', 'ca-central-1',
 					   'sa-east-1', 'eu-west-1', 'eu-central-1', 'eu-west-2',
