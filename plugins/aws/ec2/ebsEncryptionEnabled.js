c8c9de9c201482bc5784a100c219a8c5bdc6b041
Matt Fuller <matthewdf10@gmail.com>
2018-11-18 03:19:07 -0500

    PCI Compliance: Requirement 3 pt2

--- plugins/aws/ec2/ebsEncryptionEnabled.js
+++ plugins/aws/ec2/ebsEncryptionEnabled.js
@@ -13,6 +13,9 @@ module.exports = {
         hipaa: 'HIPAA requires that all data is encrypted, including data at rest. ' +
                 'EBS is a HIPAA-compliant solution that provides automated encryption ' +
                 'of EC2 instance data at rest.'
+        pci: 'PCI requires proper encryption of cardholder data at rest. EBS ' +
+             'encryption should be enabled for all volumes storing this type ' +
+             'of data.'
     },
 
     run: function(cache, settings, callback) {
