b8a21eca31eac472079b3876df3ae9de099a99a1
Cariel Cohen <ccohen55@gmail.com>
2019-04-03 10:37:18 -0700

    Text copy update to address TLSv1.1 minimum requirement

--- plugins/aws/cloudfront/insecureProtocols.js
+++ plugins/aws/cloudfront/insecureProtocols.js
@@ -5,9 +5,9 @@ module.exports = {
 	title: 'Insecure CloudFront Protocols',
 	category: 'CloudFront',
 	description: 'Detects the use of insecure HTTPS SSL/TLS protocols for use with HTTPS traffic between viewers and CloudFront',
-	more_info: 'CloudFront supports SSLv3 and TLSv1 protocols for use with HTTPS traffic, but only TLSv1 should be used unless there is a valid business justification to support the older, insecure SSLv3.',
+	more_info: 'CloudFront supports SSLv3 and TLSv1 protocols for use with HTTPS traffic, but only TLSv1.1 or higher should be used unless there is a valid business justification to support the older, insecure SSLv3.',
 	link: 'http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/secure-connections-supported-viewer-protocols-ciphers.html',
-	recommended_action: 'Ensure that traffic sent between viewers and CloudFront is passed over HTTPS and uses TLSv1, not SSLv3.',
+	recommended_action: 'Ensure that traffic sent between viewers and CloudFront is passed over HTTPS and uses TLSv1.1 or higher.',
 	apis: ['CloudFront:listDistributions'],
 	compliance: {
         hipaa: 'The transmission security aspect of HIPAA requires communication containing ' +
