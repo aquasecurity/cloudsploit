ec950247f9587d23a7ea0b20f23a1b06b5e91047
Cariel Cohen <ccohen55@gmail.com>
2019-05-06 15:46:05 -0700

    Add a plugin that ensures EFS is configured to encrypt data at rest

--- helpers/aws/regions.js
+++ helpers/aws/regions.js
@@ -71,5 +71,7 @@ module.exports = {
 	directoryservice: ['us-east-1', 'us-east-2', 'us-west-2', 'us-west-1', 'ca-central-1',
 					   'sa-east-1', 'eu-west-1', 'eu-central-1', 'eu-west-2',
 					   'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2', 'ap-northeast-2',
-					   'ap-south-1']
+					   'ap-south-1'],
+	efs: [ 'eu-west-2', 'eu-west-1', 'ap-northeast-2', 'ap-northeast-1', 'ap-southeast-1',
+		'ap-southeast-2', 'eu-central-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
 };
