d55b9fff880f7dbe3450287b96d9378747d0a126
Matt Fuller <matthewdf10@gmail.com>
2019-05-12 20:06:38 -0400

    bypassing aws hong kong region temporarily

--- plugins/aws/ec2/instanceMaxCount.js
+++ plugins/aws/ec2/instanceMaxCount.js
@@ -106,6 +106,12 @@ module.exports = {
             regex: '^[0-9]{1,4}$',
             default: 100
         },
+        instance_count_region_threshold_ap_east_1: {
+            name: 'Instance Count Region Threshold: ap-east-1',
+            description: 'Checks for the number of running instances in the ap-east-1 region and triggers a failing result if it exceeds the specified count',
+            regex: '^[0-9]{1,4}$',
+            default: 100
+        },
         instance_count_region_threshold_ca_central_1: {
             name: 'Instance Count Region Threshold: ca-central-1',
             description: 'Checks for the number of running instances in the ca-central-1 region and triggers a failing result if it exceeds the specified count',
@@ -143,6 +149,7 @@ module.exports = {
             instance_count_region_threshold_eu_west_3: settings.instance_count_region_threshold_eu_west_3 || this.settings.instance_count_region_threshold_eu_west_3.default,
             instance_count_region_threshold_sa_east_1: settings.instance_count_region_threshold_sa_east_1 || this.settings.instance_count_region_threshold_sa_east_1.default,
             instance_count_region_threshold_ap_south_1: settings.instance_count_region_threshold_ap_south_1 || this.settings.instance_count_region_threshold_ap_south_1.default,
+            instance_count_region_threshold_ap_east_1: settings.instance_count_region_threshold_ap_east_1 || this.settings.instance_count_region_threshold_ap_east_1.default,
             instance_count_region_threshold_ca_central_1: settings.instance_count_region_threshold_ca_central_1 || this.settings.instance_count_region_threshold_ca_central_1.default
         };
 
