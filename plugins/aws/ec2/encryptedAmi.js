559468bfe341f8a5390a64f65e35239c16370a4b
Matt Fuller <matthewdf10@gmail.com>
2019-08-18 13:28:14 -0400

    adding plugins

---
+++ plugins/aws/ec2/encryptedAmi.js
@@ -0,0 +1,71 @@
+var async = require('async');
+var helpers = require('../../../helpers/aws');
+
+module.exports = {
+    title: 'Encrypted AMI',
+    category: 'EC2',
+    description: 'Ensures EBS-backed AMIs are configured to use encryption',
+    more_info: 'AMIs with unencrypted data volumes can be used to launch unencrypted instances that place data at risk.',
+    link: 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AMIEncryption.html',
+    recommended_action: 'Ensure all AMIs have encrypted EBS volumes.',
+    apis: ['EC2:describeImages'],
+    compliance: {
+        hipaa: 'HIPAA data should not be stored on EC2 AMIs. However, if data is ' +
+                'accidentally included within an AMI, encrypting that data will ' +
+                'allow it to remain compliant with the encryption at-rest ' +
+                'regulatory requirement.'
+    },
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var regions = helpers.regions(settings);
+
+         async.each(regions.ec2, function(region, rcb){
+            var describeImages = helpers.addSource(cache, source,
+                ['ec2', 'describeImages', region]);
+
+            if (!describeImages) return rcb();
+
+            if (describeImages.err || !describeImages.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for AMIs: ' + helpers.addError(describeImages), region);
+                return rcb();
+            }
+
+            if (!describeImages.data.length) {
+                helpers.addResult(results, 0, 'No AMIs found', region);
+                return rcb();
+            }
+
+            var unencryptedAmis = [];
+
+            describeImages.data.forEach(function(image){
+                image.BlockDeviceMappings.forEach(function(volume){
+                    if (volume.Ebs && !volume.Ebs.Encrypted) {
+                        if (unencryptedAmis.indexOf(image.ImageId) == -1) {
+                            unencryptedAmis.push(image.ImageId);
+                        }
+                    }
+                });
+            });
+
+            if (unencryptedAmis.length > 20) {
+                helpers.addResult(results, 2,
+                    'More than 20 unencrypted AMI EBS volumes found', region);
+            } else if (unencryptedAmis.length) {
+                unencryptedAmis.forEach(function(ami){
+                    helpers.addResult(results, 2,
+                        'AMI EBS volume is unencrypted', region, ami);
+                });
+            } else {
+                helpers.addResult(results, 0,
+                    'No AMIs with unencrypted volumes found', region);
+            }
+
+            rcb();
+        }, function(){
+            callback(null, results, source);
+        });
+    }
+};
---
+++ plugins/aws/ec2/multipleSubnets.js
@@ -0,0 +1,76 @@
+var async = require('async');
+var helpers = require('../../../helpers/aws');
+
+module.exports = {
+    title: 'VPC Multiple Subnets',
+    category: 'EC2',
+    description: 'Ensures that VPCs have multiple subnets to provide a layered architecture',
+    more_info: 'VPCs should be designed to have separate public and private subnets, ideally across availability zones, enabling a DMZ-style architecture.',
+    link: 'https://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Subnets.html#SubnetSecurity',
+    recommended_action: 'Create at least two subnets in each VPC, utilizing one for public traffic and the other for private traffic.',
+    apis: ['EC2:describeVpcs', 'EC2:describeSubnets'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var regions = helpers.regions(settings);
+
+        async.each(regions.ec2, function(region, rcb){
+            var describeVpcs = helpers.addSource(cache, source,
+                ['ec2', 'describeVpcs', region]);
+
+            if (!describeVpcs) return rcb();
+
+            if (describeVpcs.err || !describeVpcs.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for VPCs: ' + helpers.addError(describeVpcs), region);
+                return rcb();
+            }
+
+            if (!describeVpcs.data.length) {
+                helpers.addResult(results, 0, 'No VPCs found', region);
+                return rcb();
+            }
+
+            if (describeVpcs.data.length > 1) {
+                helpers.addResult(results, 0,
+                    'Multiple (' + describeVpcs.data.length + ') VPCs are used.', region);
+                return rcb();
+            }
+
+            var vpcId = describeVpcs.data[0].VpcId;
+
+            if (!vpcId) {
+                helpers.addResult(results, 3, 'Unable to query for subnets for VPC.', region);
+                return rcb();
+            }
+
+            var describeSubnets = helpers.addSource(cache, source,
+                ['ec2', 'describeSubnets', region, vpcId]);
+
+            if (!describeSubnets || describeSubnets.err || !describeSubnets.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for subnets in VPC: ' + helpers.addError(describeSubnets), region, vpcId);
+                return rcb();
+            }
+
+            if (describeSubnets.data.Subnets.length > 1) {
+                helpers.addResult(results, 0,
+                    'There are ' + describeSubnets.data.Subnets.length + ' subnets used in one VPC.',
+                    region, vpcId);
+            } else if (describeSubnets.data.Subnets.length === 1) {
+                helpers.addResult(results, 2,
+                    'Only one subnet (' + describeSubnets.data.Subnets[0].SubnetId + ') in one VPC is used.',
+                    region, vpcId);
+            } else {
+                helpers.addResult(results, 0,
+                    'The VPC does not contain any subnets',
+                    region, vpcId);
+            }
+
+            rcb();
+        }, function(){
+            callback(null, results, source);
+        });
+    }
+};
