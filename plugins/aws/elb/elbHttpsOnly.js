1ca6d6b2d1b057de9f4d39ad64500dbeca8dc24b
Matt Fuller <matt@cloudsploit.com>
2019-08-18 13:28:03 -0400

    Temporary Code Swap (#201)
    * swapping code files
    
    * package fix
    
    * add exports

--- plugins/aws/elb/elbHttpsOnly.js
+++ plugins/aws/elb/elbHttpsOnly.js
@@ -60,7 +60,7 @@ module.exports = {
                 });
                 if (non_https_listner){
                     //helpers.addResult(results, 2, non_https_listner.join(', '), region);
-                    msg = "The following listeners are not HTTPS-only: ";
+                    msg = "The following listeners are not using HTTPS only: ";
                     helpers.addResult(
                         results, 2, msg + non_https_listner.join(', '), region, elbArn
                     );
