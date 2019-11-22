071c9e6f1a15fcaf4e635e8bba626a7ddb34ce02
Robert Ramsey <rramsey@trek10.com>
2019-11-22 14:48:07 -0500

    added spec file for testing

---
+++ plugins/aws/iam/iamUserAdmins.spec.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var iamUserAdmins = require('./iamUserAdmins');
+
+const createCache = (users) => {
+    return {
+        iam: {
+            generateCredentialReport: {
+                'us-east-1': {
+                    data: users
+                }
+            }
+        }
+    }
+}
+
+describe('iamUserAdmins', function () {
+    describe('run', function () {
+        it('should FAIL when no users are found', function (done) {
+            const settings = {
+                iam_admin_count_minimum: 1,
+                iam_admin_count_maximum: 8
+            }
+
+            const cache = createCache(
+                [{}]
+            )
+
+            const callback = (err, results) => {
+                expect(results.length).to.equal(0)
+                done()
+            }
+
+            iamUserAdmins._run(cache, settings, callback)
+        })
+
+        it('should PASS when users are found and fit within range', function (done) {
+            const settings = {
+                iam_admin_count_minimum: 1,
+                iam_admin_count_maximum: 8
+            }
+
+            const cache = createCache(
+                [{
+                    user: '<root_account>'
+                }]
+            )
+
+            const callback = (err, results) => {
+                expect(results.length).to.equal(1)
+                done()
+            }
+
+            iamUserAdmins._run(cache, settings, callback)
+        })
+
+    
+    })
+})
