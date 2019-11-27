deab59d7a16166498bef1f870d5728eae315fb9d
jhaubold <jhaubold@trek10.com>
2019-11-27 11:18:38 -0500

    update plugin title

--- plugins/aws/organizations/enableAllFeatures.js
+++ plugins/aws/organizations/enableAllFeatures.js
@@ -2,9 +2,9 @@ var async = require('async');
 var helpers = require('../../../helpers/aws');
 
 module.exports = {
-    title: 'Enable All Features',
+    title: 'Enable All Organization Features',
     category: 'Organizations',
-    description: 'Ensure all Organizations features are enabled',
+    description: 'Ensure all Organization features are enabled',
     recommended_action: 'Enable all AWS Organizations features',
     link: 'https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_org_support-all-features.html?icmpid=docs_orgs_console',
     apis: ['Organizations:describeOrganization'],
