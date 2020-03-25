d85ba86e3ebf2b1ea6c8edc79d145ee2d675ea01
Ryan Farina <rfarina@trek10.com>
2020-03-25 12:01:34 -0400

    merge conflicts

--- plugins/aws/s3/s3Encryption.js
+++ plugins/aws/s3/s3Encryption.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-var async = require('async');
-var helpers = require('../../../helpers/aws');
-const encryptionLevelMap = {
-    sse: 1,
-    awskms: 2,
-    awscmk: 3,
-    externalcmk: 4,
-    cloudhsm: 5
-};
-
-function getEncryptionLevel(kmsKey) {
-=======
 var minimatch = require('minimatch');
 var helpers = require('../../../helpers/aws');
 
@@ -55,26 +42,12 @@ function getEncryptionLevel(statement) {
 }
 
 function getKeyEncryptionLevel(kmsKey) {
->>>>>>> 50fcd6efd141b484db3d586bd8f8f1d5bc08af34
     return kmsKey.Origin === 'AWS_CLOUDHSM' ? 'cloudhsm' :
            kmsKey.Origin === 'EXTERNAL' ? 'externalcmk' :
            kmsKey.KeyManager === 'CUSTOMER' ? 'awscmk' : 'awskms'
 }
 
 module.exports = {
-<<<<<<< HEAD
-    title: 'S3 Encryption',
-    category: 'S3',
-    description: 'Ensures S3 buckets are configured for Encryption at a level required by the organization.',
-    more_info: '',
-    recommended_action: 'Enable Encryption on S3 buckets.',
-    link: 'https://docs.aws.amazon.com/AmazonS3/latest/user-guide/default-bucket-encryption.html',
-    apis: ['S3:listBuckets', 'S3:getBucketEncryption', 'kms:describeKey'],
-    compliance: {},
-    settings: {
-        s3_encryption_level: {
-            name: 'S3 Minimum Encryption Level',
-=======
     title: 'S3 Bucket Encryption Enforcement',
     category: 'S3',
     description: 'All statements in all S3 bucket policies must have a condition that requires encryption at a certain level',
@@ -84,7 +57,6 @@ module.exports = {
     settings: {
         s3_required_encryption_level: {
             name: 'S3 Minimum Default Encryption Level',
->>>>>>> 50fcd6efd141b484db3d586bd8f8f1d5bc08af34
             description: 'In order (lowest to highest) \
                 sse=Server-Side Encryption; \
                 awskms=AWS-managed KMS; \
@@ -100,24 +72,6 @@ module.exports = {
         var results = [];
         var source = {};
 
-<<<<<<< HEAD
-        var desiredEncryptionLevelString = settings.s3_encryption_level || this.settings.s3_encryption_level.default
-        if(!desiredEncryptionLevelString.match(this.settings.s3_encryption_level.regex)) {
-            helpers.addResult(results, 3, 'Settings misconfigured for S3 Encryption Level.');
-            return callback(null, results, source);
-        }
-
-        var desiredEncryptionLevel = encryptionLevelMap[desiredEncryptionLevelString]
-        var currentEncryptionLevelString, currentEncryptionLevel
-        var region = helpers.defaultRegion(settings);
-        var listBuckets = helpers.addSource(cache, source,
-            ['s3', 'listBuckets', region]);
-
-        if (!listBuckets) return callback(null, results, source);
-        if (listBuckets.err || !listBuckets.data) {
-            helpers.addResult(results, 3,
-                'Unable to query for S3 buckets: ' + helpers.addError(listBuckets));
-=======
         var desiredEncryptionLevelString = settings.s3_required_encryption_level || this.settings.s3_required_encryption_level.default
         console.log(desiredEncryptionLevelString)
         if(!desiredEncryptionLevelString.match(this.settings.s3_required_encryption_level.regex)) {
@@ -132,7 +86,6 @@ module.exports = {
         if (!listBuckets) return callback(null, results, source);
         if (listBuckets.err || !listBuckets.data) {
             helpers.addResult(results, 3, `Unable to query for S3 buckets: ${helpers.addError(listBuckets)}`);
->>>>>>> 50fcd6efd141b484db3d586bd8f8f1d5bc08af34
             return callback(null, results, source);
         }
 
@@ -141,60 +94,6 @@ module.exports = {
             return callback(null, results, source);
         }
 
-<<<<<<< HEAD
-        async.each(listBuckets.data, function(bucket, bcb) {
-            if (!bucket.Name) return bcb();
-
-            var bucketResource = 'arn:aws:s3:::' + bucket.Name;
-            var getBucketEncryption = helpers.addSource(cache, source,
-                ['s3', 'getBucketEncryption', region, bucket.Name]);
-
-            if (!getBucketEncryption) {
-                helpers.addResult(results, 3, 'Unable locate Bucket for getBucketEncryption: ' + bucket.Name, region, bucketResource);
-                return bcb();
-            }
-            if (getBucketEncryption.err || !getBucketEncryption.data) {
-                if(getBucketEncryption.err && getBucketEncryption.err.message === 'The server side encryption configuration was not found') {
-                    helpers.addResult(results, 2,
-                        'No default Encryption set for bucket: ' + bucket.Name,
-                        'global', bucketResource);
-                } else {
-                    helpers.addResult(results, 3,
-                        'Error querying for bucket Encryption for bucket: ' + bucket.Name +
-                        ': ' + helpers.addError(getBucketEncryption),
-                        'global', bucketResource);
-                }
-            } else {
-                var algorithm = getBucketEncryption.data.ServerSideEncryptionConfiguration.Rules[0].ApplyServerSideEncryptionByDefault.SSEAlgorithm
-                if(algorithm === 'aws:kms' && desiredEncryptionLevel > 1) { //if only sse is required, reduce chance for errors and computation.
-                    var keyId = getBucketEncryption.data.ServerSideEncryptionConfiguration.Rules[0].ApplyServerSideEncryptionByDefault.KMSMasterKeyID.split("/")[1]
-                    var describeKey = helpers.addSource(cache, source, ['kms', 'describeKey', region, keyId]);
-
-                    if(!describeKey) {
-                        helpers.addResult(results, 3, 'Unable locate KMS key for describeKey: ' + keyId, region);
-                        return bcb();
-                    }
-                    if (describeKey.err || !describeKey.data) {
-                        helpers.addResult(results, 3, 'Unable to query for KMS Key: ' + helpers.addError(describeKey), region);
-                        return bcb();
-                    }
-                    currentEncryptionLevelString = getEncryptionLevel(describeKey.data.KeyMetadata)
-                } else {
-                    currentEncryptionLevelString = 'sse'
-                }
-                currentEncryptionLevel = encryptionLevelMap[currentEncryptionLevelString]
-                if (currentEncryptionLevel < desiredEncryptionLevel) {
-                    helpers.addResult(results, 1, `s3 is encrypted to ${currentEncryptionLevelString}, which is lower than the desired ${desiredEncryptionLevelString} level.`, region, bucketResource);
-                } else {
-                    helpers.addResult(results, 0, `s3 is encrypted to a minimum of ${desiredEncryptionLevelString}`, region, bucketResource);
-                }
-            }
-            return bcb();
-        })
-        callback(null, results, source);
-    }
-};
-=======
         for (let bucket of listBuckets.data) {
             var bucketResource = `arn:aws:s3:::${bucket.Name}`;
 
@@ -255,4 +154,3 @@ module.exports = {
         callback(null, results, source);
     }
 };
->>>>>>> 50fcd6efd141b484db3d586bd8f8f1d5bc08af34
