69fc10825779338d50d878d41ad68f70379a7e0f
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2020-08-16 02:19:25 +0500

    A6: Added 'Certificate Minimum Size' plugin

--- plugins/aws/iam/certificateMinimumSize.js
+++ plugins/aws/iam/certificateMinimumSize.js
@@ -20,31 +20,59 @@ module.exports = {
 
         var listServerCertificates = helpers.addSource(cache, source,
             ['iam', 'listServerCertificates', region]);
-        // console.log(listServerCertificates);
-        // console.log("data received");
+
         if (!listServerCertificates) return callback(null, results, source);
 
         if (listServerCertificates.err || !listServerCertificates.data) {
             helpers.addResult(results, 3,
-                'Unable to find any server certificates: ' + helpers.addError(listServerCertificates));
+                'Unable to find any server certificates ' + helpers.addError(listServerCertificates), region);
             return callback(null, results, source);
         }
 
         if (listServerCertificates.data.length == 0) {
-            helpers.addResult(results, 0, 'No server certificate found');
+            helpers.addResult(results, 2, 'No server certificate found', region);
             return callback(null, results, source);
         }
 
-        listServerCertificates.data.forEach(function(certificate){
-            console.log(certificate.ServerCertificateName);
+        async.each(listServerCertificates.data, function(certificate, cb){
             var serverCertificate = helpers.addSource(cache, source,
-                ['iam', 'getServerCertificate', 'us-east-1', certificate.ServerCertificateName]);
-             console.log('response');
-             console.log(serverCertificate);
+                ['iam', 'getServerCertificate', region, certificate.ServerCertificateName]);
+            var resource = certificate.ServerCertificateName;
+
+            if (!serverCertificate || serverCertificate.err || !serverCertificate.data) {
+                helpers.addResult(results, 3,
+                    'Unable to get server certificate ' + helpers.addError(listServerCertificates), region);
+                return cb();
+            }
+
+            if (serverCertificate.data.length == 0) {
+                helpers.addResult(results, 2, 'No server certificate found', region);
+                return cb();
+            }
+
+            if(!serverCertificate.data.ServerCertificate  || !serverCertificate.data.ServerCertificate.CertificateBody) {
+                helpers.addResult(results, 3,
+                    'Unable to get IAM server certificate body ' + helpers.addError(listServerCertificates), region);
+                return cb();
+            }
+
+            var resource = serverCertificate.data.ServerCertificate.ServerCertificateMetadata.ServerCertificateName;
+
+            const certificatePem = forge.pki.certificateFromPem(serverCertificate.data.ServerCertificate.CertificateBody);
+
+            const certificateBitLength = certificatePem.publicKey.n.bitLength();
+            if (certificateBitLength >= 2048) {
+                helpers.addResult(results, 0,
+                    'IAM Certificate is security complient with 2048 bit key length', region, resource);
+            }
+            else {
+                helpers.addResult(results, 1,
+                    'IAM Certificate should have atleast 2048 bit key length', region, resource);
+            }
+            return cb();
+        }, function(){
+            callback(null, results, source);
         });
-        CertificateBody =  "-----BEGIN CERTIFICATE-----\nMIIC6jCCAdKgAwIBAgIJAMCqFZnVrZiRMA0GCSqGSIb3DQEBBQUAMCExHzAdBgNV\nBAMTFnd3dy5zc2xjZXJ0aWZpY2F0ZS5jb20wHhcNMjAwODEyMjAxOTUxWhcNMzAw\nODEwMjAxOTUxWjAhMR8wHQYDVQQDExZ3d3cuc3NsY2VydGlmaWNhdGUuY29tMIIB\nIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoiFyqwi2NvNb1Ky9RmEx4JQP\nXSvmWYLc9DD+g6QR4wvEdq4U/X3D8jVw82tbMKOZYHVtlcPzl79LnuLo/rAqjyTA\nOJLR37c+BK7xghpb8Aozt1VX9si9exBSmndDx7r5hMIA4PInXz3vCdtQiEJk1G3D\nXOjelHfkiFwnF7166Bxv4n2IsnqaV3bdIPEWOc5+aqROtd3xonzzVS6jEKW7S9bD\n+3QCILMimcbUu5MkYetVse1VBe4ZTRItmSTY3j6HXeHn58GPqoteiCClK7Xzvrkg\nFFZ1deGzQDf6ZNJYUba93m2V3Gn4kjyBimbAHOE+04OXPEkznzF0mQHnbleFewID\nAQABoyUwIzAhBgNVHREEGjAYghZ3d3cuc3NsY2VydGlmaWNhdGUuY29tMA0GCSqG\nSIb3DQEBBQUAA4IBAQBZi/MO23r5xjYsiM2jM8lDHngoS1EwP9RqIYx7cXdwIHR/\nSKEllu2/VLIWbq6WI0cg5No2QWqbW3KzmIinARHIjgcd+WI7mod68i1EnZHsLU75\n1lEfVdFqhYVmT3WqXNWeyTstedhGUWJocRM41nUbmR+E+XUA40L0XSXoReBXCdQ9\nc3ia3jQpyq/qt86zreUeaIllCgcIcAUR0XKz+lb0w6/mPNgJgLuBllbnuRDDqvs/\nVfskYw+lmEFSP7pL7MaYTYZomlsAya4jc8Uw5AmMPRRV0K6TOBPVGvgXWBPX2v05\nOmbDpF3aoYW7Z05AXcu7CCezAzLFYInJSqt6ruWH\n-----END CERTIFICATE-----"
-        const certificate = forge.pki.certificateFromPem(CertificateBody);
-        console.log(certificate.publicKey.n.bitLength());
        
     }
 };
---
+++ plugins/aws/iam/certificateMinimumSize.spec.js
@@ -0,0 +1,215 @@
+var expect = require('chai').expect;
+const certificateMinimumSize = require('./certificateMinimumSize');
+
+const listCertificates =  [
+    {
+        Path: '/',
+        ServerCertificateName: 'ExampleCertificate',
+        ServerCertificateId: 'ASCAYE32SRU5Z4FTTGYXD',
+        Arn: 'arn:aws:iam::560213429563:server-certificate/ExampleCertificate',
+        UploadDate: '2020-08-13T11:37:27.000Z',
+        Expiration: '2030-08-10T20:19:51.000Z'
+    }
+];
+
+const certificates = [
+    { // Valid Server Certificate with 2048 bit length for getServerCertificate response
+        ResponseMetadata: { RequestId: '1fc0aab6-e57c-4f05-9327-007419203c88' },
+        ServerCertificate: {
+            ServerCertificateMetadata: {
+                Path: '/',
+                ServerCertificateName: 'ExampleCertificate',
+                ServerCertificateId: 'ASCAYE32SRU5Z4FTTGYXD',
+                Arn: 'arn:aws:iam::560213429563:server-certificate/ExampleCertificate',
+                UploadDate: '2020-08-13T11:37:27.000Z',
+                Expiration: '2030-08-10T20:19:51.000Z'
+            },
+            CertificateBody: '-----BEGIN CERTIFICATE-----\n' +
+                'MIIC6jCCAdKgAwIBAgIJAMCqFZnVrZiRMA0GCSqGSIb3DQEBBQUAMCExHzAdBgNV\n' +
+                'BAMTFnd3dy5zc2xjZXJ0aWZpY2F0ZS5jb20wHhcNMjAwODEyMjAxOTUxWhcNMzAw\n' +
+                'ODEwMjAxOTUxWjAhMR8wHQYDVQQDExZ3d3cuc3NsY2VydGlmaWNhdGUuY29tMIIB\n' +
+                'IjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoiFyqwi2NvNb1Ky9RmEx4JQP\n' +
+                'XSvmWYLc9DD+g6QR4wvEdq4U/X3D8jVw82tbMKOZYHVtlcPzl79LnuLo/rAqjyTA\n' +
+                'OJLR37c+BK7xghpb8Aozt1VX9si9exBSmndDx7r5hMIA4PInXz3vCdtQiEJk1G3D\n' +
+                'XOjelHfkiFwnF7166Bxv4n2IsnqaV3bdIPEWOc5+aqROtd3xonzzVS6jEKW7S9bD\n' +
+                '+3QCILMimcbUu5MkYetVse1VBe4ZTRItmSTY3j6HXeHn58GPqoteiCClK7Xzvrkg\n' +
+                'FFZ1deGzQDf6ZNJYUba93m2V3Gn4kjyBimbAHOE+04OXPEkznzF0mQHnbleFewID\n' +
+                'AQABoyUwIzAhBgNVHREEGjAYghZ3d3cuc3NsY2VydGlmaWNhdGUuY29tMA0GCSqG\n' +
+                'SIb3DQEBBQUAA4IBAQBZi/MO23r5xjYsiM2jM8lDHngoS1EwP9RqIYx7cXdwIHR/\n' +
+                'SKEllu2/VLIWbq6WI0cg5No2QWqbW3KzmIinARHIjgcd+WI7mod68i1EnZHsLU75\n' +
+                '1lEfVdFqhYVmT3WqXNWeyTstedhGUWJocRM41nUbmR+E+XUA40L0XSXoReBXCdQ9\n' +
+                'c3ia3jQpyq/qt86zreUeaIllCgcIcAUR0XKz+lb0w6/mPNgJgLuBllbnuRDDqvs/\n' +
+                'VfskYw+lmEFSP7pL7MaYTYZomlsAya4jc8Uw5AmMPRRV0K6TOBPVGvgXWBPX2v05\n' +
+                'OmbDpF3aoYW7Z05AXcu7CCezAzLFYInJSqt6ruWH\n' +
+                '-----END CERTIFICATE-----'
+        }
+    },
+    {// Valid Server Certificate with 1024 bit length for getServerCertificate response
+        ResponseMetadata: { RequestId: '1fc0aab6-e57c-4f05-9327-007419203c88' },
+        ServerCertificate: {
+            ServerCertificateMetadata: {
+                Path: '/',
+                ServerCertificateName: 'ExampleCertificate',
+                ServerCertificateId: 'ASCAYE32SRU5Z4FTTGYXD',
+                Arn: 'arn:aws:iam::560213429563:server-certificate/ExampleCertificate',
+                UploadDate: '2020-08-13T11:37:27.000Z',
+                Expiration: '2030-08-10T20:19:51.000Z'
+            },
+            CertificateBody:    '-----BEGIN CERTIFICATE-----\n' +
+                'MIICCDCCAXGgAwIBAgIUWb+eZfe6bQpzESrh2yKAiJqrPUswDQYJKoZIhvcNAQEL\n' +
+                'BQAwADAeFw0yMDA4MTUxNDE2MjNaFw0zMDA4MTMxNDE2MjNaMAAwgZ8wDQYJKoZI\n' +
+                'hvcNAQEBBQADgY0AMIGJAoGBAMAFcnpC632EkANCt+uH9m3G9ZLLNU5f5AG39l00\n' +
+                'wPQMII7UnQrUS2qQhpQfgpxY0SH02hIe4iDzTJwQ+iwk5M0SOKQhlwoJykYPPctF\n' +
+                '8RRvjvD0w2wONZUQ2cFZO3Fexoc+dNRC35uTzwFqRfvcOG9rxRyiNtfyAhIPxLRQ\n' +
+                'ehNtAgMBAAGjfzB9MB0GA1UdDgQWBBSvDp68Y22SIbRltkCPAUo8oGQpsjAfBgNV\n' +
+                'HSMEGDAWgBSvDp68Y22SIbRltkCPAUo8oGQpsjAOBgNVHQ8BAf8EBAMCBaAwHQYD\n' +
+                'VR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwDQYJKoZI\n' +
+                'hvcNAQELBQADgYEAOX0OF6+uDngJjqocyhVL7G1c+35nkdQGonoRaCBCUeefNrvP\n' +
+                'JDtv8obMunuVKgxTFdgmoKOAvRfRWX+Sao3+DWq81f5w9InoHoYcijJY8LxYU0ig\n' +
+                '2BGpVoARHE6oeaqco5Sn1X1pTKUsG/Z0LxHZkt1fBrf/f9btHfDzq6i3ERM=\n' +
+                '-----END CERTIFICATE-----'
+         
+        }
+    },
+    { // ServerCertificate not found in getServerCertificate response
+        ResponseMetadata: { RequestId: '1fc0aab6-e57c-4f05-9327-007419203c88' },
+        ServerCertificate : {
+            ServerCertificateMetadata: {
+                Path: '/',
+                ServerCertificateName: 'ExampleCertificate',
+                ServerCertificateId: 'ASCAYE32SRU5Z4FTTGYXD',
+                Arn: 'arn:aws:iam::560213429563:server-certificate/ExampleCertificate',
+                UploadDate: '2020-08-13T11:37:27.000Z',
+                Expiration: '2030-08-10T20:19:51.000Z'
+            },
+            CertificateBody: null
+        },
+    },
+];
+
+const createCache = (certificatesMetadataList, certificates) => {
+    return {
+        iam: {
+            listServerCertificates: {
+                'us-east-1': {
+                    data: certificatesMetadataList
+                },
+            },
+            getServerCertificate: {
+                'us-east-1': {
+                        data: certificates
+                },
+            },
+        },
+    };
+};
+
+const createCertificateCache = (certificatesMetadataList, certificates) => {
+    return {
+        iam: {
+            listServerCertificates: {
+                'us-east-1': {
+                    data: certificatesMetadataList
+                },
+            },
+            getServerCertificate: {
+                'us-east-1': {
+                    'ExampleCertificate': {
+                        data: certificates
+                    },
+                },
+            },
+        },
+    };
+};
+
+const createErrorCache = () => {
+    return {
+        iam: {
+            listServerCertificates: {
+                'us-east-1': {
+                    err: {
+                        message: 'error describing server certificates'
+                    },
+                },
+            },
+        },
+    };
+};
+
+const createNullCache = () => {
+    return {
+        iam: {
+            listServerCertificates: {
+                'us-east-1': null,
+            },
+        },
+    };
+};
+
+describe('certificateMinimumSize', function () {
+    describe('run', function () {
+
+        it('should PASS if unable to get server certificates metadata list', function (done) {
+            const cache = createCache([]);
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                done();
+            });
+        });
+
+        it('should not return any results if unable to get server certificates metadata list', function (done) {
+            const cache = createNullCache();
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(0);
+                done();
+            });
+        });
+
+        it('should UNKNOWN if error occurs while fetching server certificates metadata list', function (done) {
+            const cache = createErrorCache();
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                done();
+            });
+        });
+
+        it('should FAIL if server certificate data is empty', function (done) {
+            const cache = createCache([listCertificates[0]], []);
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                done();
+            });
+        });
+
+        it('should FAIL if server certificate body empty', function (done) {
+            const cache = createCertificateCache([listCertificates[0]], certificates[2]);
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                done();
+            });
+        });
+
+        it('should PASS if server certificate has 2048 bit key length', function (done) {
+            const cache = createCertificateCache([listCertificates[0]], certificates[0]);
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                done();
+            });
+        });
+
+        it('should WARN if server certificate has less than 2048 bit key length', function (done) {
+            const cache = createCertificateCache([listCertificates[0]], certificates[1]);
+            certificateMinimumSize.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(1);
+                done();
+            });
+        });
+    });
+});
