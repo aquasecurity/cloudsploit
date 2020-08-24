6b2af78a324f7d538ddb14e1879eb1a5c565bb75
Fenil P <fpatel@trek10.com>
2020-08-24 15:25:41 -0400

    working unit tests w/ KMS key check

--- plugins/aws/workspaces/workspacesVolumeEncryption.js
+++ plugins/aws/workspaces/workspacesVolumeEncryption.js
@@ -8,7 +8,7 @@ module.exports = {
     more_info: "Checking the existence of volume encryption on different volumes of Workspaces",
     link: "https://docs.aws.amazon.com/workspaces/latest/adminguide/encrypt-workspaces.html",
     recommended_action: "Enable encryption for all workspaces",
-    apis: ["WorkSpaces:describeWorkspaces", "STS:getCallerIdentity"],
+    apis: ["WorkSpaces:describeWorkspaces", "STS:getCallerIdentity","KMS:describeKey", "KMS:listKeys", "KMS:listAliases"],
 
     run: function(cache, settings, callback) {
         var results = [];
@@ -28,22 +28,24 @@ module.exports = {
 
         async.each(regions.workspaces, function(region, rcb){
             var listWorkspaces = helpers.addSource(cache, source, ["workspaces", "describeWorkspaces", region, "data"]);
+            var listKeys = helpers.addSource(cache, source, ['kms', 'listKeys', region]);
 
             if (!listWorkspaces) {
                 return rcb()
             }
 
             if (listWorkspaces.err) {
-                helpers.addResult(
-                    results, 3, "Unable to query for WorkSpaces information: " + helpers.addError(listWorkspaces), region
-                );
+                helpers.addResult(results, 3, "Unable to query for WorkSpaces information: " + helpers.addError(listWorkspaces), region);
+                return rcb()
+            }
+
+            if (listKeys.err || !listKeys.data){
+                helpers.addResult(results, 3, "Unable to get keys" + helpers.addError(listKeys), region);
                 return rcb()
             }
 
             if (!listWorkspaces.length) {
-                helpers.addResult(
-                    results, 0, "No Workspaces found.", region
-                );
+                helpers.addResult(results, 0, "No Workspaces found.", region);
                 return rcb()
             }
 
@@ -51,30 +53,41 @@ module.exports = {
                 var arn = "arn:" + awsOrGov + ":workspaces:" + region + ":" + accountId + ":workspace/" + workspace.WorkspaceId;
 
                 if (workspace.UserVolumeEncryptionEnabled && workspace.RootVolumeEncryptionEnabled){
-                    helpers.addResult(
-                        results, 0, enabledString, region, arn
-                    );
+                    if (workspace.VolumeEncryptionKey.includes('alias/')){
+                        var getAliases = helpers.addSource(cache, source, ['kms', 'listAliases', region]);
+                        if (getAliases.err || !getAliases.data){
+                            helpers.addResult(results, 3, "Unable to get aliases", region, arn);
+                        } else {
+                            var queryAlias = getAliases.data.find(alias => alias.AliasArn === workspace.VolumeEncryptionKey);
+                            if (!queryAlias){
+                                var aliasName = workspace.VolumeEncryptionKey.slice(workspace.VolumeEncryptionKey.search('alias/'), workspace.VolumeEncryptionKey.length);
+                                helpers.addResult(results, 3, `Unable to locate Alias: ${aliasName}`, region, arn);
+                            } else {
+                                var queryKey = listKeys.data.find(key => key.KeyId === queryAlias.TargetKeyId);
+                                if (!queryKey){
+                                    helpers.addResult(results, 3, `Unable to locate Key for Alias: ${aliasName}`, region, arn);
+                                } else {
+                                    helpers.addResult(results, 0, enabledString, region, arn);
+                                }}}
+                    } else {
+                        var queryKey = listKeys.data.find(key => key.KeyArn === workspace.VolumeEncryptionKey);
+                        if (queryKey){
+                            helpers.addResult(results, 0, enabledString, region, arn);
+                        } else {
+                            helpers.addResult(results, 3, unknownStatusString, region, arn);
+                        }
+                    }
                 } else  if (workspace.UserVolumeEncryptionEnabled && !workspace.RootVolumeEncryptionEnabled){
-                    helpers.addResult(
-                        results, 2, enabledUser, region, arn
-                    );
+                    helpers.addResult(results, 2, enabledUser, region, arn);
                 } else if (!workspace.UserVolumeEncryptionEnabled && workspace.RootVolumeEncryptionEnabled){
-                    helpers.addResult(
-                        results, 2, enabledRoot, region, arn
-                    );
+                    helpers.addResult(results, 2, enabledRoot, region, arn);
                 } else if (!workspace.UserVolumeEncryptionEnabled && !workspace.RootVolumeEncryptionEnabled){
-                    helpers.addResult(
-                        results, 2, disabledString, region, arn
-                    );
+                    helpers.addResult(results, 2, disabledString, region, arn);
                 } else {
-                    helpers.addResult(
-                        results, 3, unknownStatusString, region, arn
-                    );
+                    helpers.addResult(results, 3, unknownStatusString, region, arn);
                 }
             }
-
             return rcb();
-
         }, function(){
             callback(null, results, source);
         });
--- plugins/aws/workspaces/workspacesVolumeEncryption.spec.js
+++ plugins/aws/workspaces/workspacesVolumeEncryption.spec.js
@@ -21,44 +21,53 @@ const noWorkspaces = (statement) => {
                 },
             },
         },
+        kms:{describeKey: {"us-east-1":{data: []}},
+            listKeys: {"us-east-1":{data:[]}},
+            listAliases: {"us-east-1":{data:[]}}}
     }
 };
 
 const testWorkspaces = (statement) => {
-    return {workspaces:{
-            describeWorkspaces: {
-                "us-east-1":{
-                    data: [
-                        {
-                            WorkspaceId: "test01",
-                            UserVolumeEncryptionEnabled: true,
-                            RootVolumeEncryptionEnabled: true
-                        },
-                        {
-                            WorkspaceId: "test02",
-                            RootVolumeEncryptionEnabled: true
-                        },
-                    ]
-                },
-            },
+    return {workspaces:{describeWorkspaces: {"us-east-1":{data: [
+        {
+            WorkspaceId: "test01",
+            UserVolumeEncryptionEnabled: true,
+            RootVolumeEncryptionEnabled: true,
+            VolumeEncryptionKey: "arn:aws:kms:us-east-1:null:alias/test01"
         },
+        {
+            WorkspaceId: "test02",
+            RootVolumeEncryptionEnabled: true
+        },]},},},
+        kms:{describeKey: {"us-east-1":{data: [
+                    ]}},
+            listKeys: {"us-east-1":{data:[{
+                        "KeyId": "12345",
+                        "KeyArn": "arn:aws:kms:us-east-1:null:key/12345"}]}},
+            listAliases: {"us-east-1":{data:[{
+                        "AliasName": "alias/test-fenil",
+                        "AliasArn": "arn:aws:kms:us-east-1:null:alias/test01",
+                        "TargetKeyId": "12345"}]}}}
     }};
 
 const testWorkspaces2 = (statement) => {
     return {workspaces:{
-            describeWorkspaces: {
-                "us-east-1":{
-                    data: [
-                        {
-                            WorkspaceId: "test01"
-                        },
-                        {
-                            WorkspaceId: "test02"
-                        },
-                    ]
+            describeWorkspaces: {"us-east-1":{data: [
+                {
+                    WorkspaceId: "test01",
+                    UserVolumeEncryptionEnabled: true,
+                    RootVolumeEncryptionEnabled: true,
+                    VolumeEncryptionKey: "arn:aws:kms:us-east-1:null:key/12345"
                 },
-            },
+                {
+                    WorkspaceId: "test02"
+                },]},},
         },
+        kms:{describeKey: {"us-east-1":{data: []}},
+            listKeys: {"us-east-1":{data:[{
+                        "KeyId": "12345",
+                        "KeyArn": "arn:aws:kms:us-east-1:null:key/12345"}]}},
+            listAliases: {"us-east-1":{data:[]}}}
     }};
 
 
@@ -70,6 +79,7 @@ describe("workspacesIPAccessControl", function () {
             const callback = (err, results) => {
                 expect(results.length).to.equal(0)
             };
+
             metrics.run(cache, settings, callback);
             done();
         });
@@ -79,6 +89,7 @@ describe("workspacesIPAccessControl", function () {
             const cache = noWorkspaces();
 
             const callback = (err, results) => {
+                console.log(results)
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
             };
@@ -101,13 +112,13 @@ describe("workspacesIPAccessControl", function () {
             done();
         })
 
-        it("should give no volume encryption on both workspaces", function (done) {
+        it("should give no volume encryption on one workspace02", function (done) {
             const settings = {};
             const cache = testWorkspaces2();
 
             const callback = (err, results) => {
                 expect(results.length).to.equal(2);
-                expect(results[0].status).to.equal(2);
+                expect(results[0].status).to.equal(0);
                 expect(results[1].status).to.equal(2);
             };
 
