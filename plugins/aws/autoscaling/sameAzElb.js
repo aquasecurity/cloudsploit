89a3f7849888cb57309a87e2ea275fa7bca54201
AkhtarAmir <makhtar.pucit@gmail.com>
2020-08-30 05:09:51 +0500

    Updated sameAzElb plugin

--- plugins/aws/autoscaling/sameAzElb.js
+++ plugins/aws/autoscaling/sameAzElb.js
@@ -4,10 +4,10 @@ var helpers = require('../../../helpers/aws');
 module.exports = {
     title: 'AutoScaling Group Missing ELB',
     category: 'AutoScaling',
-    description: 'Ensures all autoscaling groups are referencing active load balancers.',
-    more_info: 'Each autoscaling group should with a load balancer configured should reference an active ELB.',
-    link: 'https://docs.aws.amazon.com/autoscaling/ec2/userguide/attach-load-balancer-asg.html',
-    recommended_action: 'Ensure that the autoscaling group load balancer has not been deleted. If so, remove it from the ASG.',
+    description: 'Ensures all autoscaling groups with attached ELBs are operating in the same availability zone.',
+    more_info: 'To work properly and prevent orphaned instances, ELBs must be created in the same availability zones as the backend instances in the autoscaling group.',
+    link: 'https://docs.aws.amazon.com/autoscaling/ec2/userguide/as-add-availability-zone.html',
+    recommended_action: 'Update the ELB to use the same availability zones as the autoscaling group.',
     apis: ['AutoScaling:describeAutoScalingGroups', 'ELB:describeLoadBalancers', 'ELBv2:describeLoadBalancers'],
 
     run: function(cache, settings, callback) {
@@ -66,35 +66,33 @@ module.exports = {
 
             autoScalingGroups.data.forEach(function(asg) {
                 var asgAvailabilityZones = asg.AvailabilityZones;
-                var differentAzFound = false;
+                var distinctAzs = [];
                 var resource = asg.AutoScalingGroupARN;
 
                 if(asg.HealthCheckType == 'ELB') {
-                    if(!loadBalancers.length) {
-                        helpers.addResult(results, 0, 'No Load Balancer found', region);
-                        return rcb();
-                    }
-
                     if (asg.LoadBalancerNames && asg.LoadBalancerNames.length) {
 
                         asg.LoadBalancerNames.forEach(function(elbName) {
                             if(elbName in loadBalancers) {
                                 var loadBalancer = loadBalancers[elbName];
-                                differentAzFound = false;
                                 var elbAvailabilityZones = loadBalancer.AvailabilityZones;
 
                                 // if Load Balancer is in any AZ different from AutoScaling group's AZs then add an error result
                                 elbAvailabilityZones.forEach(function(elbAz) {
                                     if(!asgAvailabilityZones.includes(elbAz)) {
-                                        differentAzFound = true;
+                                        distinctAzs.push(elbAz);
                                     }
                                 });
 
-                                if(differentAzFound) {
-                                    helpers.addResult(results, 2, 'Load balancer "' + elbName + '" is not in the same AZ as of AutoScaling group', region, resource);
+                                if(distinctAzs.length) {
+                                    helpers.addResult(results, 2,
+                                        'Auto scaling group "' + asg.AutoScalingGroupName + '" has these load balancers in different AZs: ' + distinctAzs.join(', '),
+                                    region, resource);
                                 }
                                 else {
-                                    helpers.addResult(results, 0, 'Load balancer "' + elbName + '" is in the same AZ as of AutoScaling group', region, resource);
+                                    helpers.addResult(results, 0,
+                                        'Auto scaling group "' + asg.AutoScalingGroupName + '" has all load balancers in same AZ',
+                                    region, resource);
                                 }
                             }
                             else {
