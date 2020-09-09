2612c5c0aa249e0e8fc84c7dffcab1dcd4830748
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2020-09-09 19:44:58 +0500

    SPLOIT-69: Accommodated PR changes

--- plugins/aws/ec2/ebsUnusedVolumes.js
+++ plugins/aws/ec2/ebsUnusedVolumes.js
@@ -8,12 +8,16 @@ module.exports = {
     more_info: 'EBS volumes should be deleted if the parent instance has been deleted to prevent accidental exposure of data.',
     recommended_action: 'Delete the unassociated EBS volume.',
     link: 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-deleting-volume.html',
-    apis: ['EC2:describeInstances', 'EC2:describeVolumes'],
+    apis: ['EC2:describeInstances', 'EC2:describeVolumes', 'STS:getCallerIdentity'],
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
+
+        var acctRegion = helpers.defaultRegion(settings);
+        var awsOrGov = helpers.defaultPartition(settings);
+        var accountId = helpers.addSource(cache, source, ['sts', 'getCallerIdentity', acctRegion, 'data']);
 
         async.each(regions.ec2, function(region, rcb) {
             var describeInstances = helpers.addSource(cache, source,
@@ -54,21 +58,21 @@ module.exports = {
                 });
             }
 
-            var unusedEbsVolumeFound = false;
             describeVolumes.data.forEach(function(volume) {
                 if (volume.VolumeId) {
+                    var volumeArn = 'arn:' + awsOrGov + ':ec2:' + region + ':' + accountId + ':volume/' + volume.VolumeId;
                     if(!usedEbsVolumes.includes(volume.VolumeId)) {
-                        unusedEbsVolumeFound = true;
                         helpers.addResult(results, 2,
                             'EBS Volume is not attached to any EC2 instance',
-                            region, volume.VolumeId);
+                            region, volumeArn);
+                    }
+                    else {
+                        helpers.addResult(results, 0,
+                            'EBS Volume is attached to an EC2 instance',
+                            region, volumeArn);
                     }
                 }
             });
-
-            if(!unusedEbsVolumeFound){
-                helpers.addResult(results, 0, 'All EBS Volumes are attached to EC2 instances', region);
-            }
 
             rcb();
         }, function(){
