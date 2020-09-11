e0fab079274d3558b3dd2e491b721747ab141b20
Matthew Skillman <mskillman@trek10.com>
2020-09-11 09:33:09 -0400

    revision of correction

--- collectors/aws/rds/describeDBParameters.js
+++ collectors/aws/rds/describeDBParameters.js
@@ -13,15 +13,16 @@ module.exports = function(AWSConfig, collection, callback) {
             DBParameterGroupName: parameterGroupName.DBParameterGroupName
         };
 
-        rds.describeDBParameters(params).eachPage(function(err, data) {
+        rds.describeDBParameters(params).eachPage(function(err, data, done) {
             if (err) {
                 collection.rds.describeDBParameters[AWSConfig.region][parameterGroupName.DBParameterGroupName].err = err;
                 collection.rds.describeDBParameters[AWSConfig.region][parameterGroupName.DBParameterGroupName].data = null;
             } else if (!data) {
-                cb(); // once eachPage is done iterating, data will be null, so then we call the callback once.
+                cb();
             } else {
                 collection.rds.describeDBParameters[AWSConfig.region][parameterGroupName.DBParameterGroupName].data.Parameters =
                     collection.rds.describeDBParameters[AWSConfig.region][parameterGroupName.DBParameterGroupName].data.Parameters.concat(data.Parameters);
+                done();
             }
         });
     }, function(){
