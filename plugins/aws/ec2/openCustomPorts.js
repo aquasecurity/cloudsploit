c24eae0feb7f61526bb95ace79373df23f3d7ae1
AkhtarAmir <makhtar.pucit@gmail.com>
2020-09-23 17:03:27 +0500

    Accommodated PR changes

--- plugins/aws/ec2/openCustomPorts.js
+++ plugins/aws/ec2/openCustomPorts.js
@@ -8,13 +8,13 @@ module.exports = {
     more_info: 'Security groups should be used to restrict access to ports from known networks.',
     link: 'https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html',
     recommended_action: 'Modify the security group to ensure the ports are not exposed publicly.',
-    apis: ['EC2:describeSecurityGroups', 'STS:getCallerIdentity'],
+    apis: ['EC2:describeSecurityGroups'],
     settings: {
         open_port_allowed_list: {
             name: 'EC2 Allowed Open Ports',
             description: 'A comma-delimited list of ports that indicates open ports allowed for any connection',
-            regex: '[a-zA-Z0-9,]',
-            default: [80, 443]
+            regex: '[0-9,]',
+            default: '80,443'
         }
     },
 
@@ -22,17 +22,17 @@ module.exports = {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
-        var acctRegion = helpers.defaultRegion(settings);
         var awsOrGov = helpers.defaultPartition(settings);
 
-        var accountId = helpers.addSource(cache, source, ['sts', 'getCallerIdentity', acctRegion, 'data']);
-
-        var allowed_open_ports = this.settings.open_port_allowed_list.default;
+        var allowed_open_ports = settings.open_port_allowed_list || this.settings.open_port_allowed_list.default;
+        allowed_open_ports = allowed_open_ports.split(',');
 
         async.each(regions.ec2, function(region, rcb){
             var describeSecurityGroups = helpers.addSource(cache, source,
                 ['ec2', 'describeSecurityGroups', region]);
+
             if (!describeSecurityGroups) return rcb();
+
             if (describeSecurityGroups.err || !describeSecurityGroups.data) {
                 helpers.addResult(results, 3,
                     'Unable to query for security groups: ' + helpers.addError(describeSecurityGroups), region);
@@ -47,7 +47,8 @@ module.exports = {
             // Loop through each security group
             for (var g in describeSecurityGroups.data) {
                 var group = describeSecurityGroups.data[g];
-                var arn = 'arn:' + awsOrGov + ':ec2:' + region + ':' + accountId + ':security-group/' + group.GroupId;
+                var arn = 'arn:' + awsOrGov + ':ec2:' + region + ':' + group.OwnerId + ':security-group/' + group.GroupId;
+
                 var openPorts = [];
 
                 if (!group.IpPermissions) continue;
@@ -62,12 +63,10 @@ module.exports = {
 
                         if (range.CidrIp && range.CidrIp === '0.0.0.0/0') {
                             var portRange = permission.ToPort - permission.FromPort;
-
                             // Check for all the ports in port range
                             for (let p=0; p <= portRange; p++) {
                                 var port = permission.FromPort + p;
-
-                                if (!allowed_open_ports.includes(port)) {
+                                if (!allowed_open_ports.includes(port.toString())) {
                                     var openPort = permission.IpProtocol.toUpperCase() + ' port ' + port;
                                     if (openPorts.indexOf(openPort) === -1) openPorts.push(openPort);
                                 }
@@ -76,13 +75,13 @@ module.exports = {
                     }
                 }
 
-                if (openPorts.length) {
-                    helpers.addResult(results, 2,
-                        'Security group ' + group.GroupName + ' has: ' + openPorts.join(' , ') + ' open to 0.0.0.0/0', 
+                if (!openPorts.length) {
+                    helpers.addResult(results, 0,
+                        'Security group "' + group.GroupName + '" has no open ports',
                         region, arn);
                 } else {
-                    helpers.addResult(results, 0,
-                        'Security group: ' + group.GroupName + ' has no open ports',
+                    helpers.addResult(results, 2,
+                        'Security group "' + group.GroupName + '" has: ' + openPorts.join(' , ') + ' open to 0.0.0.0/0', 
                         region, arn);
                 }
             }
