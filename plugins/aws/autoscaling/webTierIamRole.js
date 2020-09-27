8967cb5251e0389adc106209cfb587411d8d450b
AkhtarAmir <makhtar.pucit@gmail.com>
2020-09-27 20:01:16 +0500

    Refactored plugin and spec file

--- plugins/aws/autoscaling/webTierIamRole.js
+++ plugins/aws/autoscaling/webTierIamRole.js
@@ -2,19 +2,19 @@ var async = require('async');
 var helpers = require('../../../helpers/aws');
 
 module.exports = {
-    title: 'IAM Roles for Web-Tier ASG Launch Configurations',
+    title: 'Web-Tier Launch Configurations IAM Roles',
     category: 'AutoScaling',
-    description: 'Ensures that Web-Tier ASG launch configuration is configured to use a customer created Web-Tier IAM role',
-    more_info: 'Web-Tier ASG launch configuration should have a customer created Web-Tier IAM role to provide necessary credentials to access AWS services',
+    description: 'Ensures that Web-Tier Auto Scaling launch configuration is configured to use a customer created IAM role.',
+    more_info: 'Web-Tier Auto Scaling launch configuration should have a customer created Web-Tier IAM role to provide necessary credentials to access AWS services.',
     link: 'https://docs.aws.amazon.com/autoscaling/ec2/userguide/us-iam-role.html',
-    recommended_action: 'Update Web-Tier ASG launch configuration and attach a customer created Web-Tier IAM role',
+    recommended_action: 'Update Web-Tier Auto Scaling launch configuration and attach a customer created Web-Tier IAM role',
     apis: ['AutoScaling:describeAutoScalingGroups', 'AutoScaling:describeLaunchConfigurations'],
     settings: {
         web_tier_tag_key: {
             name: 'Auto Scaling Web-Tier Tag Key',
             description: 'Web-Tier tag key used by Auto Scaling groups to indicate Web-Tier groups',
-            regex: '[a-zA-Z0-9,]',
-            default: 'web_tier'
+            regex: '^.*$',
+            default: ''
         }
     },
 
@@ -23,6 +23,8 @@ module.exports = {
         var source = {};
         var regions = helpers.regions(settings);
         var web_tier_tag_key = settings.web_tier_tag_key || this.settings.web_tier_tag_key.default;
+
+        if (!web_tier_tag_key.length) return callback();
 
         async.each(regions.autoscaling, function(region, rcb){
             var describeAutoScalingGroups = helpers.addSource(cache, source,
@@ -47,69 +49,74 @@ module.exports = {
 
             if (!describeLaunchConfigurations || describeLaunchConfigurations.err || !describeLaunchConfigurations.data) {
                 helpers.addResult(results, 3,
-                    `Unable to query for Auto Scaling launch configurations: ${helpers.addError(describeLaunchConfigurations)}`,
+                    `Unable to query for Auto Scaling group launch configurations: ${helpers.addError(describeLaunchConfigurations)}`,
                     region);
                 return rcb();
             }
 
             if (!describeLaunchConfigurations.data.length) {
-                helpers.addResult(results, 2, 'No Auto Scaling launch configurations found', region);
+                helpers.addResult(results, 0, 'No Auto Scaling launch configurations found', region);
                 return rcb();
             }
 
             var launchConfigurations = {};
-            describeLaunchConfigurations.data.forEach(function(config){
+            describeLaunchConfigurations.data.forEach(config => {
+                if (!config.IamInstanceProfile) return;
+
                 launchConfigurations[config.LaunchConfigurationName] = config.IamInstanceProfile;
             });
 
             var launchConfigurationAsgFound = false;
-            async.each(describeAutoScalingGroups.data, function(asg, cb){
+            var webTierAsgFound = false;
+
+            for (var g in describeAutoScalingGroups.data) {
+                var asg = describeAutoScalingGroups.data[g];
+
+                if (!asg.AutoScalingGroupARN) continue;
+
                 var resource = asg.AutoScalingGroupARN;
+
                 if(asg.LaunchConfigurationName && asg.LaunchConfigurationName.length){
                     launchConfigurationAsgFound = true;
-                    var launchConfigurationName = asg.LaunchConfigurationName;
 
-                    if (!asg.Tags || !asg.Tags.length) {
-                        helpers.addResult(results, 0,
-                            `Auto Scaling group "${asg.AutoScalingGroupName}" does not contain any tags`,
-                            region, resource);
-                    }
-                    else {
-                        var webTierTagFound = false;
-                        asg.Tags.forEach(function(tag){
+                    if (asg.Tags && asg.Tags.length) {
+                        var webTierTag = false;
+
+                        for (var t in asg.Tags) {
+                            var tag = asg.Tags[t];
+
                             if (tag.Key === web_tier_tag_key) {
-                                webTierTagFound = true;
-                            }
-                        });
-
-                        if(webTierTagFound){
-                            if(launchConfigurations[launchConfigurationName]) {
-                                helpers.addResult(results, 0,
-                                    `Auto Scaling launch configuration "${launchConfigurationName}" has "${launchConfigurations[launchConfigurationName]}" customer created Web-Tier IAM role configured`,
-                                    region, resource);
-                            }
-                            else {
-                                helpers.addResult(results, 2,
-                                    `Auto Scaling launch configuration "${launchConfigurationName}" does have use customer created Web-Tier IAM role configured`,
-                                    region, resource);
+                                webTierTag = true;
+                                webTierAsgFound = true;
+                                break;
                             }
                         }
-                        else {
-                            helpers.addResult(results, 0,
-                                `Auto Scaling group "${asg.AutoScalingGroupName}" does not contain Web-Tier tag`,
-                                region, resource);
+
+                        if (webTierTag) {
+                            if(launchConfigurations[asg.LaunchConfigurationName]) {
+                                helpers.addResult(results, 0,
+                                    `Launch configuration for Web-Tier group "${asg.AutoScalingGroupName}" has customer created IAM role configured`,
+                                    region, resource);
+                            } else {
+                                helpers.addResult(results, 2,
+                                    `Launch configuration for Web-Tier group "${asg.AutoScalingGroupName}" does not have customer created IAM role configured`,
+                                    region, resource);
+                            }
                         }
                     }
                 }
+            }
 
-                if (!launchConfigurationAsgFound) {
-                    helpers.addResult(results, 0,
-                        'No Auto Scaling group with launch configurations found',
-                        region);
-                }
+            if (!launchConfigurationAsgFound) {
+                helpers.addResult(results, 0,
+                    'No Auto Scaling groups utilizing launch configurations found', region);
+                return rcb();
+            }
 
-                cb();
-            });
+            if (!webTierAsgFound) {
+                helpers.addResult(results, 0,
+                    'No Web-Tier Auto Scaling groups with found', region);
+            }
 
             rcb();
         }, function(){
