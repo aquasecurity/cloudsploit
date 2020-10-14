435e331729af63fdfea372f42e0fcef0ad546d9d
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2020-10-14 06:22:45 +0500

    Added 'Public IP Address EC2 Instances' Plugin

--- exports.js
+++ exports.js
@@ -91,6 +91,7 @@ module.exports = {
         'ebsEncryptedSnapshots'         : require(__dirname + '/plugins/aws/ec2/ebsEncryptedSnapshots.js'),
         'ebsUnusedVolumes'              : require(__dirname + '/plugins/aws/ec2/ebsUnusedVolumes.js'),
         'ec2MetadataOptions'            : require(__dirname + '/plugins/aws/ec2/ec2MetadataOptions.js'),
+        'publicIpAddress'               : require(__dirname + '/plugins/aws/ec2/publicIpAddress.js'),
 
         'efsEncryptionEnabled'          : require(__dirname + '/plugins/aws/efs/efsEncryptionEnabled.js'),
 
