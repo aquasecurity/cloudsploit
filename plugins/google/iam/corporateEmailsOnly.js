d8b42902847b488755c1d4d321b1df5600e5f3a7
AlisonVilelaTenchi <avilela@tenchisecurity.com>
2020-11-18 17:51:55 -0300

    Fix google plugin

--- plugins/google/iam/corporateEmailsOnly.js
+++ plugins/google/iam/corporateEmailsOnly.js
@@ -36,10 +36,12 @@ module.exports = {
             iamPolicy.bindings.forEach(roleBinding => {
                 if (roleBinding.members && roleBinding.members.length) {
                     roleBinding.members.forEach(member => {
-                        var emailArr = member.split('@');
-                        var provider = emailArr[1].split('.');
-                        if (provider[0] === 'gmail' && (gmailUsers.indexOf(member) === -1)) {
-                            gmailUsers.push(member);
+                        if (member.indexOf('user:') ===0) {
+                            var emailArr = member.split('@');
+                            var provider = emailArr[1].split('.');
+                            if (provider[0] === 'gmail' && (gmailUsers.indexOf(member) === -1)) {
+                                gmailUsers.push(member);
+                            }
                         }
                     })
                 }
