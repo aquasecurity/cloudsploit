b1b9d8d122099e0d6e8710bc6bbc1b9b93d27970
AkhtarAmir <makhtar.pucit@gmail.com>
2020-11-20 16:00:59 +0500

    Added remediation for Dynamo KMS Encryption plugin

--- plugins/aws/dynamodb/dynamoKmsEncryption.js
+++ plugins/aws/dynamodb/dynamoKmsEncryption.js
@@ -9,6 +9,18 @@ module.exports = {
     link: 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/EncryptionAtRest.html',
     recommended_action: 'Create a new DynamoDB table using a CMK KMS key.',
     apis: ['DynamoDB:listTables', 'DynamoDB:describeTable', 'STS:getCallerIdentity'],
+    remediation_description: 'The impacted DynamoDB table will be configured to use either AES-256 encryption, or CMK-based encryption if a KMS key ID is provided.',
+    remediation_min_version: '202010110730',
+    apis_remediate: ['DynamoDB:listTables'],
+    actions: {
+        remediate: ['S3:updateTable'],
+        rollback: ['S3:updateTable']
+    },
+    permissions: {
+        remediate: ['s3:UpdateTable'],
+        rollback: ['s3:UpdateTable']
+    },
+    realtime_triggers: ['s3:UpdateTable', 's3:CreateTable'],
 
     run: function(cache, settings, callback) {
 
@@ -65,5 +77,64 @@ module.exports = {
         }, function(){
             callback(null, results, source);
         });
-    }
+    },
+    remediate: function(config, cache, settings, resource, callback) {
+        var putCall = this.actions.remediate;
+        var pluginName = 'dynamoKmsEncryption';
+        var tableNameArr = resource.split(':');
+        var tableName = tableNameArr[tableNameArr.length - 1].split('/')[1];
+
+        var tableLocation = tableNameArr[3];
+
+        // add the location of the table to the config
+        config.region = tableLocation;
+        var params = {};
+
+        // create the params necessary for the remediation
+        if (settings.input &&
+            settings.input.kmsKeyId) {
+            params = {
+                'TableName': tableName,
+                'SSESpecification': {
+                    'Enabled': true,
+                    'KMSMasterKeyId': settings.input.kmsKeyId,
+                    'SSEType': 'KMS'
+                  }
+            };
+        } else {
+            params = {
+                'TableName': tableName,
+                'SSESpecification': {
+                    'Enabled': true,
+                    'SSEType': 'KMS'
+                  }
+            };
+        }
+
+        var remediation_file = settings.remediation_file;
+
+        remediation_file['pre_remediate']['actions'][pluginName][resource] = {
+            'Encryption': 'Default',
+            'Bucket': bucketName
+        };
+
+        // passes the config, put call, and params to the remediate helper function
+        helpers.remediatePlugin(config, putCall[0], params, function(err) {
+            if (err) {
+                remediation_file['remediate']['actions'][pluginName]['error'] = err;
+                return callback(err, null);
+            }
+
+            let action = params;
+            action.action = putCall;
+
+            remediation_file['post_remediate']['actions'][pluginName][resource] = action;
+            remediation_file['remediate']['actions'][pluginName][resource] = {
+                'Action': 'ENCRYPTED',
+                'Bucket': bucketName
+            };
+            settings.remediation_file = remediation_file;
+            return callback(null, action);
+        });
+    },
 };
