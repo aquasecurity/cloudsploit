acfc9e13a84bad2e4d8d7c8c9c2dfca9b8ba1f89
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2020-11-26 03:50:20 +0500

    feature/ak-29: Added AWS EBS Volumes Too Old Snapshots plugin, test cases (#403)
    * Added AWS EBS Volumes Too Old Snapshots plugin, test cases
    
    * Update plugins/aws/ec2/ebsOldSnapshots.js
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -97,6 +97,7 @@ module.exports = {
         'encryptedAmi'                  : require(__dirname + '/plugins/aws/ec2/encryptedAmi.js'),
         'instanceIamRole'               : require(__dirname + '/plugins/aws/ec2/instanceIamRole.js'),
         'ebsEncryptionEnabled'          : require(__dirname + '/plugins/aws/ec2/ebsEncryptionEnabled.js'),
+        'ebsOldSnapshots'               : require(__dirname + '/plugins/aws/ec2/ebsOldSnapshots.js'),
         'ebsSnapshotPrivate'            : require(__dirname + '/plugins/aws/ec2/ebsSnapshotPrivate.js'),
         'natMultiAz'                    : require(__dirname + '/plugins/aws/ec2/natMultiAz.js'),
         'defaultVpcInUse'               : require(__dirname + '/plugins/aws/ec2/defaultVpcInUse.js'),
