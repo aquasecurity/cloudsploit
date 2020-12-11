45bd015ae78422e4086a18078bf501f9e620446c
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2020-12-12 03:24:02 +0500

    feature/AKD-19: Added 'API Gateway Content Encoding' plugin and test cases

--- exports.js
+++ exports.js
@@ -4,6 +4,7 @@ module.exports = {
     aws : {
         'acmValidation'                 : require(__dirname + '/plugins/aws/acm/acmValidation.js'),
         'acmCertificateExpiry'          : require(__dirname + '/plugins/aws/acm/acmCertificateExpiry.js'),
+        'apigatewayContentEncoding'     : require(__dirname + '/plugins/aws/apigateway/apigatewayContentEncoding.js'),
         'apigatewayWafEnabled'          : require(__dirname + '/plugins/aws/apigateway/apigatewayWafEnabled.js'),
         'asgMultiAz'                    : require(__dirname + '/plugins/aws/autoscaling/asgMultiAz.js'),
         'asgActiveNotifications'        : require(__dirname + '/plugins/aws/autoscaling/asgActiveNotifications.js'),
