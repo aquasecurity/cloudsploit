7b140942ad4d185cbaa2abbdfc61db69c8596a3d
Gio Rodriguez <gioroddev@gmail.com>
2021-01-20 11:58:06 -0800

    Update plugins/aws/glue/bookmarkEncryptionEnabled.js

--- plugins/aws/glue/bookmarkEncryptionEnabled.js
+++ plugins/aws/glue/bookmarkEncryptionEnabled.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'Glue',
     description: 'Ensures that AWS Glue job bookmark encryption is enabled.',
     more_info: 'AWS Glue security configuration should have job bookmark encryption enabled in order to encrypt the bookmark data before it is sent to Amazon S3.',
-    recommended_action: 'Recreate Glue security configurations and enabled job bookmark encryption',
+    recommended_action: 'Recreate Glue security configurations and enable job bookmark encryption',
     link: 'https://docs.aws.amazon.com/glue/latest/dg/console-security-configurations.html',
     apis: ['Glue:getSecurityConfigurations', 'STS:getCallerIdentity'],
 
