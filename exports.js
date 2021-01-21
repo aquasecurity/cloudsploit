28afc4983cb10a415d08ca137d03d94e733ecf37
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-01-21 21:29:21 +0500

    feature/AKD-05: Added AWS Redshift Cluster Default Master Username plugin, test cases (#431)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Feature/AKD-05: Added 'Redshift Cluster Default Username' Plugin
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -265,6 +265,7 @@ module.exports = {
         'redshiftPubliclyAccessible'    : require(__dirname + '/plugins/aws/redshift/redshiftPubliclyAccessible.js'),
         'redshiftAllowVersionUpgrade'   : require(__dirname + '/plugins/aws/redshift/redshiftAllowVersionUpgrade.js'),
         'redshiftSSLEnabled'            : require(__dirname + '/plugins/aws/redshift/redshiftSSLEnabled.js'),
+        'redshiftClusterMasterUsername' : require(__dirname + '/plugins/aws/redshift/redshiftClusterMasterUsername.js'),
         'snapshotRetentionPeriod'       : require(__dirname + '/plugins/aws/redshift/snapshotRetentionPeriod.js'),
         'userActivityLoggingEnabled'    : require(__dirname + '/plugins/aws/redshift/userActivityLoggingEnabled.js'),
 
