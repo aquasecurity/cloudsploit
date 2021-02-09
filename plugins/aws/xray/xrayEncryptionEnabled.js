32dd3d4959f36d0397cd1343ac663762d47d7b3f
AkhtarAmir <makhtar.pucit@gmail.com>
2021-02-09 11:36:45 +0500

    Modified plugin to add encryption level setting

--- plugins/aws/xray/xrayEncryptionEnabled.js
+++ plugins/aws/xray/xrayEncryptionEnabled.js
@@ -8,10 +8,18 @@ module.exports = {
     more_info: 'AWS XRay supports default encryption based on an AWS-managed KMS key as well as encryption using a customer managed key (CMK). For maximum security, the CMK-based encryption should be used.',
     link: 'https://docs.aws.amazon.com/xray/latest/devguide/xray-console-encryption.html',
     recommended_action: 'Update XRay encryption configuration to use a CMK.',
-    apis: ['XRay:getEncryptionConfig'],
+    apis: ['XRay:getEncryptionConfig', 'KMS:listKeys', 'KMS:describeKey'],
+    settings: {
+        xray_encryption_level: {
+            name: 'X-Ray Minimum Encryption Level',
+            description: 'In order (lowest to highest) sse=Default Encryption; awskms=AWS-managed KMS; awscmk=Customer managed KMS; externalcmk=Customer managed externally sourced KMS; cloudhsm=Customer managed CloudHSM sourced KMS',
+            regex: '^(sse|awskms|awscmk|externalcmk|cloudhsm)$',
+            default: 'awskms',
+        }
+    },
     remediation_description: 'Encryption for the affected Cloud trails will be enabled.',
     remediation_min_version: '202011271430',
-    apis_remediate: ['XRay:getEncryptionConfig'],
+    apis_remediate: ['XRay:getEncryptionConfig', 'KMS:listKeys', 'KMS:describeKey'],
     actions: {
         remediate: ['XRay:putEncryptionConfig'],
         rollback: ['XRay:putEncryptionConfig']
@@ -22,15 +30,23 @@ module.exports = {
     },
     remediation_inputs: {
         kmsKeyIdforXray: {
-            name: '(Mandatory) XRay KMS Key ID',
+            name: '(Optional) XRay KMS Key ID',
             description: 'The KMS Key ID used for encryption',
             regex: '^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-4[0-9A-Fa-f]{3}-[89ABab][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$',
-            required: true
+            required: false
         }
     },
     realtime_triggers: ['xray:PutEncryptionConfig'],
 
     run: function(cache, settings, callback) {
+        var config = {
+            xray_encryption_level: settings.xray_encryption_level || this.settings.xray_encryption_level.default
+        };
+
+        var desiredEncryptionLevel = helpers.ENCRYPTION_LEVELS.indexOf(config.xray_encryption_level);
+        var currentEncryptionLevel;
+        var currentEncryptionLevelString;
+
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
@@ -49,14 +65,34 @@ module.exports = {
 
             if (getEncryptionConfig.data &&
                 getEncryptionConfig.data.Type &&
-                getEncryptionConfig.data.Type == 'KMS') {
-                if (getEncryptionConfig.data.KeyId) {
-                    helpers.addResult(results, 0, 'XRay is configured to use KMS encryption with a CMK', region);
+                getEncryptionConfig.data.Type == 'KMS' &&
+                getEncryptionConfig.data.KeyId) {
+                var kmsKeyId = getEncryptionConfig.data.KeyId.split('/')[1];
+
+                var describeKey = helpers.addSource(cache, source, ['kms', 'describeKey', region, kmsKeyId]);
+
+                if (!describeKey || describeKey.err || !describeKey.data || !describeKey.data.KeyMetadata) {
+                    helpers.addResult(results, 3,
+                        `Unable to query KMS key: ${helpers.addError(describeKey)}`, region);
+                    return rcb();
+                }
+
+                currentEncryptionLevel = helpers.getEncryptionLevel(describeKey.data.KeyMetadata, helpers.ENCRYPTION_LEVELS);
+                currentEncryptionLevelString = helpers.ENCRYPTION_LEVELS[currentEncryptionLevel];
+
+                if (currentEncryptionLevel >= desiredEncryptionLevel) {
+                    helpers.addResult(results, 0, `XRay is configured to use encryption at level ${currentEncryptionLevelString} which is greater than or equal to desired level ${config.xray_encryption_level}`, region);
                 } else {
-                    helpers.addResult(results, 2, 'XRay is configured to use KMS encryption but is not using a CMK', region);
+                    helpers.addResult(results, 2, `XRay is configured to use encryption at level ${currentEncryptionLevelString} which is less than desired level ${config.xray_encryption_level}`, region);
                 }
             } else {
-                helpers.addResult(results, 2, 'XRay is configured to use default encryption without CMK', region);
+                currentEncryptionLevel = 1; //sse
+                currentEncryptionLevelString = 'sse';
+                if (currentEncryptionLevel >= desiredEncryptionLevel) {
+                    helpers.addResult(results, 0, `XRay is configured to use encryption at level ${currentEncryptionLevelString} which is greater than or equal to desired level ${config.xray_encryption_level}`, region);
+                } else {
+                    helpers.addResult(results, 2, `XRay is configured to use encryption at level ${currentEncryptionLevelString} which is less than desired level ${config.xray_encryption_level}`, region);
+                }
             }
 
             rcb();
@@ -67,11 +103,9 @@ module.exports = {
     remediate: function(config, cache, settings, resource, callback) {
         var putCall = this.actions.remediate;
         var pluginName = 'xrayEncryptionEnabled';
+        let defaultKeyDesc = 'Default master key that protects my X-Ray data when no other key is defined';
 
-        // find the location of the xray needing to be remediated
         var region = settings.region;
-
-        var err;
 
         // add the location of the xray to the config
         config.region = region;
@@ -85,8 +119,12 @@ module.exports = {
                 'KeyId': settings.input.kmsKeyIdforXray,
             };
         } else {
-            err = 'KmsKeyId is mandatory to enable encryption';
-            return callback(err, null);
+            let defaultKmsKeyId = helpers.getDefaultKeyId(cache, config.region, defaultKeyDesc);
+            if (!defaultKmsKeyId) return callback(`No default X-Ray key for the region ${config.region}`);
+            params = {
+                'Type': 'KMS',
+                'KeyId': defaultKmsKeyId
+            };
         }
 
         var remediation_file = settings.remediation_file;
