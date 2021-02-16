19c05b44d44bdd39f2a64ff7b1cdacdbf2c819e3
AkhtarAmir <makhtar.pucit@gmail.com>
2021-02-16 17:20:42 +0500

    Added Google 'Open Mongo' plugin

--- exports.js
+++ exports.js
@@ -579,6 +579,7 @@ module.exports = {
         'openHadoopNameNode'            : require(__dirname + '/plugins/google/vpcnetwork/openHadoopNameNode.js'),
         'openHadoopNameNodeWebUI'       : require(__dirname + '/plugins/google/vpcnetwork/openHadoopNameNodeWebUI.js'),
         'openKibana'                    : require(__dirname + '/plugins/google/vpcnetwork/openKibana.js'),
+        'openMongo'                     : require(__dirname + '/plugins/google/vpcnetwork/openMongo.js'),
         'openMySQL'                     : require(__dirname + '/plugins/google/vpcnetwork/openMySQL.js'),
         'openNetBIOS'                   : require(__dirname + '/plugins/google/vpcnetwork/openNetBIOS.js'),
         'openOracle'                    : require(__dirname + '/plugins/google/vpcnetwork/openOracle.js'),
