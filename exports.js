3158592ecf894853e5ee00c9d89fdf2556a2b771
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-03-26 21:34:58 +0500

    feature/AKAD-5: Added Azure PostgreSQL Server 'Active Directory Admin Enabled' plugin… (#570)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Added superlinter
    
    * Added scans ci
    
    * Updated Ci file
    
    * Updated Node version in CI file
    
    * removed spech check command
    
    * Delete scan_ci.yml
    
    * Added spellcheck
    
    * Added Azure PostgreSQL Server 'Active Directory Admin Enabled' plugin and test cases
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -417,6 +417,7 @@ module.exports = {
         'logConnectionsEnabled'         : require(__dirname + '/plugins/azure/postgresqlserver/logConnectionsEnabled.js'),
         'logCheckpointsEnabled'         : require(__dirname + '/plugins/azure/postgresqlserver/logCheckpointsEnabled.js'),
         'enforcePostgresSSLConnection'  : require(__dirname + '/plugins/azure/postgresqlserver/enforcePostgresSSLConnection.js'),
+        'activeDirectoryAdminEnabled'   : require(__dirname + '/plugins/azure/postgresqlserver/activeDirectoryAdminEnabled.js'),
         'geoRedundantBackupEnabled'     : require(__dirname + '/plugins/azure/postgresqlserver/geoRedundantBackupEnabled.js'),
 
         'openOracleAutoDataWarehouse'   : require(__dirname + '/plugins/azure/networksecuritygroups/openOracleAutoDataWarehouse.js'),
