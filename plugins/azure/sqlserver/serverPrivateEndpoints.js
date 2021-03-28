0b045ea8a9341c6e6f8d8180b3bf9e5329b0d579
Ali Imran <ali.imran7@outlook.com>
2021-03-28 21:26:31 +0500

    feature/AKD-108: Added Azure 'SQL Server Private Endpoints Configured' plugin and test cases

--- plugins/azure/sqlserver/serverPrivateEndpoints.js
+++ plugins/azure/sqlserver/serverPrivateEndpoints.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'SQL Server Private Endpoints Configured',
     category: 'SQL Server',
     description: 'Ensures that SQL Servers are accessible only through private endpoints',
-    more_info: 'Azure Private Link enables you to access Azure PaaS Services and Azure hosted customer-owned/partner services over a private endpoint in your virtual network',
+    more_info: 'Azure Private Endpoint is a network interface that connects you privately and securely to a service powered by Azure Private Link. Private Endpoint uses a private IP address from your VNet, effectively bringing the service such as Azure SQL Server into your VNet.',
     recommended_action: 'Ensure that Private Endpoints are configured properly and Public Network Access is disabled for SQL Server',
     link: 'https://docs.microsoft.com/en-us/azure/private-link/private-link-overview',
     apis: ['servers:listSql'],
@@ -36,12 +36,6 @@ module.exports = {
             for (const server of servers.data) {
                 if (server.privateEndpointConnections && server.privateEndpointConnections.length > 0) {
                     helpers.addResult(results, 0, 'Private Endpoints are configured for the SQL Server', location, server.id);
-
-                    if (server.publicNetworkAccess && server.publicNetworkAccess.toLowerCase() === 'disabled') {
-                        helpers.addResult(results, 0, 'Public Network Access is disabled for the SQL Server', location, server.id);
-                    } else {
-                        helpers.addResult(results, 2, 'Public Network Access is enabled for the SQL Server', location, server.id);
-                    }
                 } else {
                     helpers.addResult(results, 2, 'Private Endpoints are not configured for the SQL Server', location, server.id);
                 }
--- plugins/azure/sqlserver/serverPrivateEndpoints.spec.js
+++ plugins/azure/sqlserver/serverPrivateEndpoints.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var privateEndpointsConfigured = require('./privateEndpointsConfigured');
+var serverPrivateEndpoints = require('./serverPrivateEndpoints');
 
 const servers = [
     {
@@ -11,26 +11,12 @@ const servers = [
                 'provisioningState': 'Ready'
             }
         ],
-        'publicNetworkAccess': 'Disabled',
-        'location': 'eastus'
-    },
-    {
-        'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Sql/servers/test-server',
-        'name': 'test-server',
-        'privateEndpointConnections': [
-            {
-                'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Sql/servers/test-server/privateEndpointConnections/test-endpoint',
-                'provisioningState': 'Ready'
-            }
-        ],
-        'publicNetworkAccess': 'Enabled',
         'location': 'eastus'
     },
     {
         'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Sql/servers/test-server',
         'name': 'test-server',
         'privateEndpointConnections': [],
-        'publicNetworkAccess': 'Enabled',
         'location': 'eastus'
     }
 ];
@@ -50,11 +36,11 @@ const createCache = (servers) => {
     };
 };
 
-describe('privateEndpointsConfigured', function() {
+describe('serverPrivateEndpoints', function() {
     describe('run', function() {
         it('should give passing result if no SQL servers', function(done) {
             const cache = createCache([]);
-            privateEndpointsConfigured.run(cache, {}, (err, results) => {
+            serverPrivateEndpoints.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
                 expect(results[0].message).to.include('No SQL servers found');
@@ -65,7 +51,7 @@ describe('privateEndpointsConfigured', function() {
 
         it('should give unknown result if unable to query for SQL servers', function(done) {
             const cache = createCache(null);
-            privateEndpointsConfigured.run(cache, {}, (err, results) => {
+            serverPrivateEndpoints.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
                 expect(results[0].message).to.include('Unable to query for SQL servers:');
@@ -74,40 +60,20 @@ describe('privateEndpointsConfigured', function() {
             });
         });
 
-        it('should give passing results if private endpoints are configured and public network acess is disabled', function(done) {
+        it('should give passing result if private endpoints are configured', function(done) {
             const cache = createCache([servers[0]]);
-            privateEndpointsConfigured.run(cache, {}, (err, results) => {
-                expect(results.length).to.equal(2);
-
+            serverPrivateEndpoints.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
                 expect(results[0].message).to.include('Private Endpoints are configured for the SQL Server');
-
-                expect(results[1].status).to.equal(0);
-                expect(results[1].message).to.include('Public Network Access is disabled for the SQL Server');
-
-                expect(results[0].region).to.equal('eastus');
-                done();
-            });
-        });
-
-        it('should give a failing result if private endpoints are enabled and public network acess is enabled', function(done) {
-            const cache = createCache([servers[1]]);
-            privateEndpointsConfigured.run(cache, {}, (err, results) => {
-                expect(results.length).to.equal(2);
-
-                expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('Private Endpoints are configured for the SQL Server');
-
-                expect(results[1].status).to.equal(2);
-                expect(results[1].message).to.include('Public Network Access is enabled for the SQL Server');
                 expect(results[0].region).to.equal('eastus');
                 done();
             });
         });
 
         it('should give failing result if private endpoints are not configured', function(done) {
-            const cache = createCache([servers[2]]);
-            privateEndpointsConfigured.run(cache, {}, (err, results) => {
+            const cache = createCache([servers[1]]);
+            serverPrivateEndpoints.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
                 expect(results[0].message).to.include('Private Endpoints are not configured for the SQL Server');
