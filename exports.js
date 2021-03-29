cee5d41716eefab22d82c01a32f99355a4525d6a
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-03-29 22:12:17 +0500

    feature/AKD-114: Added Azure Cosmos DB 'Advanced Threat Protection' plugin and test cases (#577)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Added superlinter
    
    * Added scans ci
    
    * Updated Ci file
    
    * Updated Node version in CI file
    
    * removed spech check command
    
    * Delete scan_ci.yml
    
    * Added spellcheck
    
    * Added Azure CosmosDB 'Advanced Threat Protection' plugin and test cases
    
    * Update collector.js
    
    * Update plugins/azure/cosmosdb/advancedThreatProtection.js
    
    lint
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -504,8 +504,9 @@ module.exports = {
         'kvRecoveryEnabled'             : require(__dirname + '/plugins/azure/keyvaults/kvRecoveryEnabled.js'),
         'keyExpirationEnabled'          : require(__dirname + '/plugins/azure/keyvaults/keyExpirationEnabled.js'),
         'secretExpirationEnabled'       : require(__dirname + '/plugins/azure/keyvaults/secretExpirationEnabled.js'),
-        'cosmosPublicAccessDisabled'    : require(__dirname + '/plugins/azure/cosmosdb/cosmosPublicAccessDisabled.js'),
 
+        'advancedThreatProtection'      : require(__dirname + '/plugins/azure/cosmosdb/advancedThreatProtection.js'),
+        'cosmosPublicAccessDisabled'    : require(__dirname + '/plugins/azure/cosmosdb/cosmosPublicAccessDisabled.js'),
         'automaticFailoverEnabled'      : require(__dirname + '/plugins/azure/cosmosdb/automaticFailoverEnabled.js'),
     },
     github: {
