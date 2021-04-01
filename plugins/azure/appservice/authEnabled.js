0003e19906c45f695ee70b957b97ac5440b241fc
AkhtarAmir <makhtar.pucit@gmail.com>
2021-04-01 12:51:25 +0500

    Azure App Service Auth Enabled plugin remediated

--- plugins/azure/appservice/authEnabled.js
+++ plugins/azure/appservice/authEnabled.js
@@ -9,6 +9,12 @@ module.exports = {
     recommended_action: 'Enable App Service Authentication for all App Services.',
     link: 'https://docs.microsoft.com/en-us/azure/app-service/overview-authentication-authorization',
     apis: ['webApps:list', 'webApps:getAuthSettings'],
+    remediation_min_version: '202104011300',
+    remediation_description: 'The App Service Authentication option will be enabled for the web app',
+    apis_remediate: ['webApps:list'],
+    actions: {remediate:['webApps:updateAuthSettings'], rollback:['webApps:updateAuthSettings']},
+    permissions: {remediate: ['webApps:updateAuthSettings'], rollback: ['webApps:updateAuthSettings']},
+    realtime_triggers: ['microsoftweb:sites:write'],
     compliance: {
         hipaa: 'HIPAA requires all application access to be restricted to known users ' +
                'for auditing and security controls.',
@@ -44,7 +50,7 @@ module.exports = {
                 const authSettings = helpers.addSource(
                     cache, source, ['webApps', 'getAuthSettings', location, webApp.id]
                 );
-                
+
                 if (!authSettings || authSettings.err || !authSettings.data) {
                     helpers.addResult(results, 3,
                         'Unable to query App Service: ' + helpers.addError(authSettings),
@@ -63,5 +69,48 @@ module.exports = {
             // Global checking goes here
             callback(null, results, source);
         });
+    },
+    remediate: function(config, cache, settings, resource, callback) {
+        var remediation_file = settings.remediation_file;
+        var putCall = this.actions.remediate;
+
+        // inputs specific to the plugin
+        var pluginName = 'authEnabled';
+        var baseUrl = 'https://management.azure.com/{resource}}/config/authsettings?api-version=2019-08-01';
+        var method = 'PUT';
+
+        // for logging purposes
+        var webAppNameArr = resource.split('/');
+        var webAppName = webAppNameArr[webAppNameArr.length - 1];
+
+        // create the params necessary for the remediation
+        if (settings.region) {
+            var body = {
+                'location': settings.region,
+                'properties': {
+                    'enabled': true
+                }
+            };
+
+            // logging
+            remediation_file['pre_remediate']['actions'][pluginName][resource] = {
+                'AppAuthentication': 'Disabled',
+                'WebApp': webAppName
+            };
+
+            helpers.remediatePlugin(config, method, body, baseUrl, resource, remediation_file, putCall, pluginName, function(err, action) {
+                if (err) return callback(err);
+                if (action) action.action = putCall;
+
+                remediation_file['post_remediate']['actions'][pluginName][resource] = action;
+                remediation_file['remediate']['actions'][pluginName][resource] = {
+                    'Action': 'Enabled'
+                };
+
+                callback(null, action);
+            });
+        } else {
+            callback('No region found');
+        }
     }
 };
