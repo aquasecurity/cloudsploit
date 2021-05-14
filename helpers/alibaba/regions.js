0b1e84641f48c4dca8dba4a3afbfe1333c8b9d62
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-05-15 03:55:56 +0500

    feature: Added collector for Alibaba cloud (#683)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Added superlinter
    
    * Added scans ci
    
    * Updated Ci file
    
    * Updated Node version in CI file
    
    * removed spech check command
    
    * Delete scan_ci.yml
    
    * Added spellcheck
    
    * Azure MySQL Enforce SSL Connection remediated
    
    * Modified index and engine files to fix azure remediation invalid token issue
    
    * Update engine.js
    
    * Added new argument for cloud
    
    * Modified index for Alibaba cloud
    
    * Added logic for Alibaba collector
    
    * Modified collector

---
+++ helpers/alibaba/regions.js
@@ -0,0 +1,37 @@
+// Source: https://www.alibabacloud.com/global-locations
+
+var regions = [
+    'cn-hangzhou',            // China (Hangzhou)
+    'cn-shanghai',            // China (Shanghai)
+    'cn-qingdao',             // China (Qingdao)
+    'cn-beijing',             // China (Beijing)
+    'cn-zhangjiakou',         // China (Zhangjiakou)
+    'cn-huhehaote',           // China (Hohhot)
+    'cn-wulanchabu',          // China (Ulanqab)
+    'cn-shenzhen',            // China (Shenzhen)
+    'cn-heyuan',              // China (Heyuan)
+    'cn-chengdu',             // China (Chengdu)
+    'cn-hongkong',            // China(Hong Kong)
+    'cn-guangzhou',           // China (Guangzhou)
+    'ap-southeast-1',         // Singapore
+    'ap-southeast-2',         // Australia (Sydney)
+    'ap-southeast-3',         // Malaysia (Kuala Lumpur)
+    'ap-southeast-5',         // Indonesia (Jakarta)
+    'ap-northeast-1',         // Japan (Tokyo)
+    'ap-south-1',             // India (Mumbai)
+    'eu-central-1',           // Germany (Frankfurt)
+    'eu-west-1',              // UK(London)
+    'us-west-1',              // US (Silicon Valley)
+    'us-east-1',              // US (Virginia)
+    'me-east-1',              // UAE (Dubai)
+];
+
+module.exports = {
+    default: ['cn-hangzhou'],
+    all: regions,
+    ecs: regions,
+    polardb: regions,
+    ram: ['cn-hangzhou'],
+    vpc: regions,
+    rds: regions,
+};
