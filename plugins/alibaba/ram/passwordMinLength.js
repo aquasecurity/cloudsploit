f9556bb9ed58802eec2ab8f66c3456686a21de73
umermehmood9@gmail.com <umermehmood9@gmail.com>
2021-05-15 06:50:06 +0500

    Alibaba ram user password min length plugin added with test cases

---
+++ plugins/alibaba/ram/passwordMinLength.js
@@ -0,0 +1,41 @@
+var helpers = require('../../../helpers/alibaba');
+
+module.exports = {
+    title: 'Password Minimum Length',
+    category: 'RAM',
+    description: 'Ensure that RAM password security settings require minimum length of 14 or above.',
+    more_info: 'A strong password policy enforces minimum length, expiration, reuse, and symbol usage.',
+    link: 'https://www.alibabacloud.com/help/doc-detail/116413.htm',
+    recommended_action: 'Update the password security settings to require the minimum length of 14 or above.',
+    apis: ['RAM:GetPasswordPolicy'],
+    compliance: {
+        pci: 'PCI requires a strong password policy. Setting Identity password ' +
+             'requirements enforces this policy.'
+    },
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var region = helpers.defaultRegion(settings);
+
+        var getPasswordPolicy = helpers.addSource(cache, source,
+            ['ram', 'GetPasswordPolicy', region]);
+
+        if (!getPasswordPolicy) return callback(null, results, source);
+
+        if (getPasswordPolicy.err || !getPasswordPolicy.data || !Object.keys(getPasswordPolicy.data).length) {
+            helpers.addResult(results, 3,
+                'Unable to query RAM password policy: ' + helpers.addError(getPasswordPolicy), region);
+            return callback(null, results, source);
+        }
+
+        if (getPasswordPolicy.data.MinimumPasswordLength > 13) {
+            helpers.addResult(results, 0,
+                'RAM password security policy require minimum length greater than 13', region);
+        } else {
+            helpers.addResult(results, 2,
+                'RAM password security policy does not require minimum length greater than 13', region);
+        }
+
+        callback(null, results, source);
+    }
+};
