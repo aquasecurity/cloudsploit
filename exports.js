ffa8451ab5fe103797a0f7ab7a7662f8411d5faa
umermehmood9@gmail.com <umermehmood9@gmail.com>
2021-05-15 07:00:36 +0500

    Files removed as these exist in other PRs

--- exports.js
+++ exports.js
@@ -727,7 +727,6 @@ module.exports = {
         'logSinksEnabled'               : require(__dirname + '/plugins/google/logging/logSinksEnabled.js'),
     },
     alibaba: {
-        // 'passwordRequiresLowercase'     : require(__dirname + '/plugins/alibaba/ram/passwordRequiresLowercase.js'),
         'passwordMinLength'             : require(__dirname + '/plugins/alibaba/ram/passwordMinLength.js')
     }
 };
