9598df04b41f0c9d7eefa49d032f930d8b56bfcd
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-05-20 23:37:30 +0500

    feature/AKD-213: Added Alibaba OSS Bucket Logging Enabled plugin and test cases (#691)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Added superlinter
    
    * Added scans ci
    
    * Updated Ci file
    
    * Updated Node version in CI file
    
    * removed spech check command
    
    * Delete scan_ci.yml
    
    * Added spellcheck
    
    * Azure MySQL Enforce SSL Connection remediated
    
    * Modified index and engine files to fix azure remediation invalid token issue
    
    * Update engine.js
    
    * Added new argument for cloud
    
    * Modified index for Alibaba cloud
    
    * Added logic for Alibaba collector
    
    * Modified collector
    
    * Added Alibaba RAM Users MFA Enabled plugin and test cases
    
    * Modified collector
    
    * Added Alibaba RDS SSL Encryption Enabled plugin and test cases
    
    * Added Alibaba RDS Log Duration plugin and test cases
    
    * Added Alibaba OSS Bucket Private plugin and test cases
    
    * Added Alibaba OSS Bucket Logging Enabled plugin and test cases
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -727,6 +727,7 @@ module.exports = {
         'logSinksEnabled'               : require(__dirname + '/plugins/google/logging/logSinksEnabled.js'),
     },
     alibaba: {
+        'bucketLoggingEnabled'          : require(__dirname + '/plugins/alibaba/oss/bucketLoggingEnabled.js'),
         'ossBucketPrivate'              : require(__dirname + '/plugins/alibaba/oss/ossBucketPrivate.js'),
         'rdsLogDuration'                : require(__dirname + '/plugins/alibaba/rds/rdsLogDuration.js'),
         'dataDisksEncrypted'            : require(__dirname + '/plugins/alibaba/ecs/dataDisksEncrypted.js'),
