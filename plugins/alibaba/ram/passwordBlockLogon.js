d4cb34f224a003129002c6dbb9e0d13306318878
umermehmood9@gmail.com <umermehmood9@gmail.com>
2021-05-26 15:29:57 +0500

    PR comments updated

--- plugins/alibaba/ram/passwordBlockLogon.js
+++ plugins/alibaba/ram/passwordBlockLogon.js
@@ -3,10 +3,10 @@ var helpers = require('../../../helpers/alibaba');
 module.exports = {
     title: 'Password Block Logon',
     category: 'RAM',
-    description: 'Ensure that RAM password security settings require logon to be blocked after max login attempts.',
+    description: 'Ensure that RAM password security settings require logon to be blocked after maximum of 5 incorrect login attempts.',
     more_info: 'A strong password policy enforces minimum length, expiration, reuse, and symbol usage.',
     link: 'https://www.alibabacloud.com/help/doc-detail/116413.htm',
-    recommended_action: 'Update the password security settings to require logon to be blocked after max login attempts.',
+    recommended_action: 'Update the password security settings to require logon to be blocked after maximum of 5 or less incorrect login attempts.',
     apis: ['RAM:GetPasswordPolicy'],
     compliance: {
         pci: 'PCI requires a strong password policy. Setting Identity password ' +
@@ -28,7 +28,7 @@ module.exports = {
             return callback(null, results, source);
         }
 
-        if (getPasswordPolicy.data.MaxLoginAttemps > 0 && getPasswordPolicy.data.MaxLoginAttemps <=5) {
+        if (getPasswordPolicy.data.MaxLoginAttemps > 0 && getPasswordPolicy.data.MaxLoginAttemps <= 5) {
             helpers.addResult(results, 0,
                 'RAM password security policy requires logon to be blocked after 5 attempts', region);
         } else {
