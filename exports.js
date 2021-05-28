f9913fbd0f3db12da46a30e3e96d8e51b6db0839
umermehmood <umermehmood9@gmail.com>
2021-05-28 22:59:03 +0500

    Added GCP SQL plugins for database flags. (#664)
    * Added sql plugins for log_min_duration_statement and log_temp_files database flags for PostgreSQL
    
    * Messages changed as per the output
    
    * PR changes added
    
    * Update plugins/google/sql/postgresqlLogMinDuration.js
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>
    
    * PR changes added
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -669,6 +669,8 @@ module.exports = {
         'dbPubliclyAccessible'          : require(__dirname + '/plugins/google/sql/dbPubliclyAccessible.js'),
         'dbSSLEnabled'                  : require(__dirname + '/plugins/google/sql/dbSSLEnabled.js'),
         'anyHostRootAccess'             : require(__dirname + '/plugins/google/sql/anyHostRootAccess.js'),
+        'postgresqlLogTempFiles'        : require(__dirname + '/plugins/google/sql/postgresqlLogTempFiles.js'),
+        'postgresqlLogMinDuration'      : require(__dirname + '/plugins/google/sql/postgresqlLogMinDuration.js'),
         'postgresqlLogLockWaits'        : require(__dirname + '/plugins/google/sql/postgresqlLogLockWaits.js'),
         'mysqlLocalInfile'              : require(__dirname + '/plugins/google/sql/mysqlLocalInfile.js'),
         'postgresqlLogConnections'      : require(__dirname + '/plugins/google/sql/postgresqlLogConnections.js'),
