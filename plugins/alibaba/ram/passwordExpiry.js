3b1a255e1d6548b57343ae5aab4a853c6f85a568
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-05-28 22:49:46 +0500

    modification: Added settings for RAM plugins (#733)
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Added cloudformation in china and gov regions
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * SPLOIT-113: Added Plain Text Parameters plugin for CloudFormation
    
    * Added plugin and spec file for launch wizard security groups
    
    * Added vpcEndpointAcceptance plugin and spec file
    
    * Refactored code in plaintextParameters plugin and spec file
    
    * SPLOIT-113: Updated custom settings
    
    * Made PR requested changes
    
    * SPLOIT-113: Added regex to check if NoEcho is enabled
    
    * Accommodated PR changes
    
    * Fixed eslint issues
    
    * Update exports.js
    
    * Update index.js
    
    * Update index.js
    
    * Accomodated PR changes
    
    * Updated status in result of failure
    
    * Removed unnecesary rebase changes
    
    * Added superlinter
    
    * Added scans ci
    
    * Updated Ci file
    
    * Updated Node version in CI file
    
    * removed spech check command
    
    * Delete scan_ci.yml
    
    * Added spellcheck
    
    * Azure MySQL Enforce SSL Connection remediated
    
    * Modified index and engine files to fix azure remediation invalid token issue
    
    * Update engine.js
    
    * Added new argument for cloud
    
    * Modified index for Alibaba cloud
    
    * Added logic for Alibaba collector
    
    * Modified collector
    
    * Added Alibaba RAM Users MFA Enabled plugin and test cases
    
    * Modified collector
    
    * Alibaba inactive user plugin added with spec file.
    
    * Older files deleted
    
    * Indentation updated
    
    * Lint errors
    
    * RAM user password policy for lowercase character
    
    * Alibaba ram user password min length plugin added with test cases
    
    * Files removed as these exist in other PRs
    
    * Api version added in the api call
    
    * Added settings for RAM plugins
    
    Co-authored-by: umermehmood <umermehmood9@gmail.com>
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- plugins/alibaba/ram/passwordExpiry.js
+++ plugins/alibaba/ram/passwordExpiry.js
@@ -3,11 +3,19 @@ var helpers = require('../../../helpers/alibaba');
 module.exports = {
     title: 'Password Expiry',
     category: 'RAM',
-    description: 'Ensure that RAM password security settings require password to be expired after 90 days.',
+    description: 'Ensure that RAM password security settings require password to be expired after set number of days.',
     more_info: 'A strong password policy enforces minimum length, expiration, reuse, and symbol usage.',
     link: 'https://www.alibabacloud.com/help/doc-detail/116413.htm',
-    recommended_action: 'Update the password security settings to require password to be expired after 90 days.',
+    recommended_action: 'Update the password security settings to require password to be expired after set number of days.',
     apis: ['RAM:GetPasswordPolicy'],
+    settings: {
+        ram_password_expiry: {
+            name: 'RAM User Password Expiry',
+            description: 'Maximum number of days after which RAM user password should be expired',
+            regex: '^[1-9]{1}[0-9]{0,3}$',
+            default: '90'
+        }
+    },
 
     run: function(cache, settings, callback) {
         var results = [];
@@ -17,6 +25,8 @@ module.exports = {
         var getPasswordPolicy = helpers.addSource(cache, source,
             ['ram', 'GetPasswordPolicy', region]);
 
+        var ramPasswordExpiry = parseInt(settings.ram_password_expiry || this.settings.ram_password_expiry.default);
+
         if (!getPasswordPolicy) return callback(null, results, source);
 
         if (getPasswordPolicy.err || !getPasswordPolicy.data || !Object.keys(getPasswordPolicy.data).length) {
@@ -25,14 +35,12 @@ module.exports = {
             return callback(null, results, source);
         }
 
-        if (getPasswordPolicy.data.MaxPasswordAge && 
-            getPasswordPolicy.data.MaxPasswordAge > 0 &&
-            getPasswordPolicy.data.MaxPasswordAge <= 90) {
+        if (getPasswordPolicy.data.MaxPasswordAge <= ramPasswordExpiry) {
             helpers.addResult(results, 0,
-                'RAM password security policy requires password to be expired after 90 days', region);
+                `RAM password security policy requires password to be expired after ${getPasswordPolicy.data.MaxPasswordAge} days which is equal to or less than desired limit of ${ramPasswordExpiry}`, region);
         } else {
             helpers.addResult(results, 2,
-                'RAM password security policy does not require password to be expired after 90 days', region);
+                `RAM password security policy requires password to be expired after ${getPasswordPolicy.data.MaxPasswordAge} days which is greater than desired limit of ${ramPasswordExpiry}`, region);
         }
 
         callback(null, results, source);
