889ac4c4eb65616be3eeb36c2ae7abe62e8ea042
ali-imran7 <ali.imran7@outlook.com>
2021-05-28 19:58:32 +0500

    feature/networkPeering: Added Azure 'Managed NAT Gateway In Use' Plugin and test cases

--- exports.js
+++ exports.js
@@ -371,6 +371,7 @@ module.exports = {
 
         'multipleSubnets'               : require(__dirname + '/plugins/azure/virtualnetworks/multipleSubnets.js'),
         'ddosStandardProtectionEnabled' : require(__dirname + '/plugins/azure/virtualnetworks/ddosStandardProtectionEnabled.js'),
+        'managedNatGatway'              : require(__dirname + '/plugins/azure/virtualnetworks/managedNatGatway.js'),
 
         'vmInstanceLimit'               : require(__dirname + '/plugins/azure/virtualmachines/vmInstanceLimit.js'),
         'classicInstances'              : require(__dirname + '/plugins/azure/virtualmachines/classicInstances.js'),
