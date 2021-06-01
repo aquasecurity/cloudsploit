4e4027c4433528f62a91c07f5ab0985365bfef9e
Ali Imran <79709033+ali-imran7@users.noreply.github.com>
2021-06-01 22:10:09 +0500

    feature/AKD-132: Added Azure 'Premium SSD Disabled' plugin and test cases (#640)
    * feature/AKD-132: Added Azure 'Premium SSD Disabled' plugin and test cases
    
    * feature/AKD-132: Added Azure 'Premium SSD Disabled' plugin and test cases
    
    * Update plugins/azure/virtualmachines/premiumSsdDisabled.js
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>
    
    * feature/AKD-132: Added Azure 'Premium SSD Disabled' plugin and test cases
    
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -383,6 +383,7 @@ module.exports = {
         'scaleSetMultiAz'               : require(__dirname + '/plugins/azure/virtualmachines/scaleSetMultiAz.js'),
         'scaleSetAutoscaleEnabled'      : require(__dirname + '/plugins/azure/virtualmachines/scaleSetAutoscaleEnabled.js'),
         'vmAvailabilitySetLimit'        : require(__dirname + '/plugins/azure/virtualmachines/vmAvailabilitySetLimit.js'),
+        'premiumSsdDisabled'            : require(__dirname + '/plugins/azure/virtualmachines/premiumSsdDisabled.js'),
         'vmManagedDisks'                : require(__dirname + '/plugins/azure/virtualmachines/vmManagedDisks.js'),
         'autoInstanceRepairsEnabled'    : require(__dirname + '/plugins/azure/virtualmachines/autoInstanceRepairsEnabled.js'),
         'noEmptyScaleSets'              : require(__dirname + '/plugins/azure/virtualmachines/noEmptyScaleSets.js'),
