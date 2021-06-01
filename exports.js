9b168802577453947fdd733663c563049dd3e073
Ali Imran <79709033+ali-imran7@users.noreply.github.com>
2021-06-01 23:27:39 +0500

    feature/AKD-144: Added Azure 'Scale Sets Health Monitoring Enabled' plugin and test cases (#715)
    * feature/AKD-143: Added Azure 'Scale Sets Health Monitoring Enabled' plugin and test cases
    
    * feature/AKD-144: Added Azure 'Scale Sets Health Monitoring Enabled' plugin and test cases
    
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -395,6 +395,7 @@ module.exports = {
         'approvedVmImage'               : require(__dirname + '/plugins/azure/virtualmachines/approvedVmImage.js'),
         'autoOsUpgradesEnabled'         : require(__dirname + '/plugins/azure/virtualmachines/autoOsUpgradesEnabled.js'),
         'noUnattachedDisks'             : require(__dirname + '/plugins/azure/virtualmachines/noUnattachedDisks.js'),
+        'scaleSetHealthMonitoring'      : require(__dirname + '/plugins/azure/virtualmachines/scaleSetHealthMonitoring.js'),
         'diskByokEncryptionEnabled'     : require(__dirname + '/plugins/azure/virtualmachines/diskByokEncryptionEnabled.js'),
 
         'lbLogAnalyticsEnabled'         : require(__dirname + '/plugins/azure/monitor/lbLogAnalyticsEnabled.js'),
