78c6c73ca2cb01c655f3aef99154d8a1f4ab74cd
umermehmood <umermehmood9@gmail.com>
2021-06-03 21:54:20 +0500

    Alibaba rds public access disabled plugin with spec file (#747)
    * Alibaba rds public access disabled plugin with spec file
    
    * Update plugins/alibaba/rds/rdsPublicAccess.js
    
    * Update plugins/alibaba/rds/rdsPublicAccess.js
    
    Co-authored-by: AkhtarAmir <makhtar.pucit@gmail.com>
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- exports.js
+++ exports.js
@@ -791,7 +791,8 @@ module.exports = {
       
         'rdsLogDuration'                : require(__dirname + '/plugins/alibaba/rds/rdsLogDuration.js'),
         'rdsSslEncryptionEnabled'       : require(__dirname + '/plugins/alibaba/rds/rdsSslEncryptionEnabled.js'),
-      
+        'rdsPublicAccess'               : require(__dirname + '/plugins/alibaba/rds/rdsPublicAccess.js'),
+
         'actiontrailGlobalExportLogs'   : require(__dirname + '/plugins/alibaba/actiontrail/actiontrailGlobalExportLogs.js')
     }
 };
