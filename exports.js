b16e5b989294a70876a8ce7ac2345aa9d0cf1e46
AkhtarAmir <makhtar.pucit@gmail.com>
2021-06-13 19:17:49 +0500

    Added Google MySQL Slow Query Log Plugin and test cases

--- exports.js
+++ exports.js
@@ -701,6 +701,7 @@ module.exports = {
         'sqlNoPublicIps'                : require(__dirname + '/plugins/google/sql/sqlNoPublicIps.js'),
         'sqlCrossDbOwnership'           : require(__dirname + '/plugins/google/sql/sqlCrossDbOwnership.js'),
         'sqlContainedDatabaseAuth'      : require(__dirname + '/plugins/google/sql/sqlContainedDatabaseAuth.js'),
+        'mysqlSlowQueryLog'             : require(__dirname + '/plugins/google/sql/mysqlSlowQueryLog.js'),
 
         'bucketVersioning'              : require(__dirname + '/plugins/google/storage/bucketVersioning.js'),
         'bucketLogging'                 : require(__dirname + '/plugins/google/storage/bucketLogging.js'),
