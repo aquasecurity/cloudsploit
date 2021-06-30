3fb8c7748c8877703e1797013fba8d5b289df30a
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-06-30 23:00:21 +0500

    Added Google SQL PostgreSQL Max Connections flag plugin and test cases (#766)
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -702,6 +702,7 @@ module.exports = {
         'sqlNoPublicIps'                : require(__dirname + '/plugins/google/sql/sqlNoPublicIps.js'),
         'sqlCrossDbOwnership'           : require(__dirname + '/plugins/google/sql/sqlCrossDbOwnership.js'),
         'sqlContainedDatabaseAuth'      : require(__dirname + '/plugins/google/sql/sqlContainedDatabaseAuth.js'),
+        'postgresqlMaxConnections'      : require(__dirname + '/plugins/google/sql/postgresqlMaxConnections.js'),
         'mysqlSlowQueryLog'             : require(__dirname + '/plugins/google/sql/mysqlSlowQueryLog.js'),
         'storageAutoIncreaseEnabled'    : require(__dirname + '/plugins/google/sql/storageAutoIncreaseEnabled.js'),
         'serverCertificateRotation'     : require(__dirname + '/plugins/google/sql/serverCertificateRotation.js'),
