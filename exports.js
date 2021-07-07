13e9d275a56b2e38fb78ed79fd7076ffea6cd328
UmairAjmal <ajmal.umair@outlook.com>
2021-07-07 20:41:38 +0500

    Refactored Code

--- exports.js
+++ exports.js
@@ -796,7 +796,8 @@ module.exports = {
       
         'bucketLoggingEnabled'          : require(__dirname + '/plugins/alibaba/oss/bucketLoggingEnabled.js'),
         'ossBucketPrivate'              : require(__dirname + '/plugins/alibaba/oss/ossBucketPrivate.js'),
-        'bucketCrossRegionReplication'          : require(__dirname + '/plugins/alibaba/oss/bucketCrossRegionReplication.js'),
+        'ossBucketVersioning'           : require(__dirname + '/plugins/alibaba/oss/ossBucketVersioning.js'),
+        'bucketCrossRegionReplication'  : require(__dirname + '/plugins/alibaba/oss/bucketCrossRegionReplication.js'),
       
         'rdsLogDuration'                : require(__dirname + '/plugins/alibaba/rds/rdsLogDuration.js'),
         'rdsSslEncryptionEnabled'       : require(__dirname + '/plugins/alibaba/rds/rdsSslEncryptionEnabled.js'),
