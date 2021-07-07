9d24b774fc11c6328362da113b04c73d825c2d5d
Gio Rodriguez <gioroddev@gmail.com>
2021-07-07 11:57:31 -0700

    Update plugins/alibaba/oss/bucketCrossRegionReplication.js

--- plugins/alibaba/oss/bucketCrossRegionReplication.js
+++ plugins/alibaba/oss/bucketCrossRegionReplication.js
@@ -4,7 +4,7 @@ var helpers = require('../../../helpers/alibaba');
 module.exports = {
     title: 'Bucket Cross Region Replication Enabled',
     category: 'OSS',
-    description: 'Ensure that OSS buckets has cross region replication enabled.',
+    description: 'Ensure that OSS buckets have cross region replication enabled.',
     more_info: 'Enabling Cross region replication for OSS buckets provides automatic and asynchronous replication of objects across buckets in different Object Storage Service (OSS) regions.',
     recommended_action: 'Modify OSS buckets to enable cross region replication.',
     link: 'https://www.alibabacloud.com/help/doc-detail/181408.htm?',
