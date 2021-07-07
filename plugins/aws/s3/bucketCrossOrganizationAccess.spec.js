60480e863386b42f9989db88da4592cc1d5ad1f3
UmairAjmal <ajmal.umair@outlook.com>
2021-07-07 21:11:53 +0500

    Updated import in unit test

--- plugins/aws/s3/bucketCrossOrganizationAccess.spec.js
+++ plugins/aws/s3/bucketCrossOrganizationAccess.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-const bucketOrganizationAccountAccess = require('./bucketOrganizationAccountAccess');
+const bucketOrganizationAccountAccess = require('./bucketCrossOrganizationAccess');
 
 const listBuckets = [
     { 
