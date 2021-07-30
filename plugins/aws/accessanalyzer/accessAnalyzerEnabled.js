cbb53b3913100b87b2ac518f592f17385046e30d
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-07-30 17:50:36 +0500

    Update plugins/aws/accessanalyzer/accessAnalyzerEnabled.js

--- plugins/aws/accessanalyzer/accessAnalyzerEnabled.js
+++ plugins/aws/accessanalyzer/accessAnalyzerEnabled.js
@@ -4,7 +4,7 @@ var helpers = require('../../../helpers/aws');
 module.exports = {
     title: 'Access Analyzer Enabled',
     category: 'IAM',
-    description: 'Ensure that IAM Access analyzer is enabled for all regions',
+    description: 'Ensure that IAM Access analyzer is enabled for all regions.',
     more_info: 'Access Analyzer allow you to determine if an unintended user is allowed, making it easier for administrators to monitor least privileges access. It analyzes only policies that are applied to resources in the same AWS region.',
     link: 'https://docs.aws.amazon.com/IAM/latest/UserGuide/access-analyzer-getting-started.html',
     recommended_action: 'Enable Access Analyzer for all regions',
