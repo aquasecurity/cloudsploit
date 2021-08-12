7f1d5c8186931e1079945d76162517e55bfd0f6e
UmairAjmal <ajmal.umair@outlook.com>
2021-08-12 20:20:08 +0500

    Plugin to check AWS Elasticsearch domain has TLS version 1.2

--- exports.js
+++ exports.js
@@ -195,6 +195,7 @@ module.exports = {
         'esLoggingEnabled'              : require(__dirname + '/plugins/aws/es/esLoggingEnabled.js'),
         'esUpgradeAvailable'            : require(__dirname + '/plugins/aws/es/esUpgradeAvailable.js'),
         'esHttpsOnly'                   : require(__dirname + '/plugins/aws/es/esHttpsOnly.js'),
+        'esTlsVersion'                   : require(__dirname + '/plugins/aws/es/esTlsVersion.js'),
 
         'glueCloudwatchLogsEncrypted'   : require(__dirname + '/plugins/aws/glue/glueCloudwatchLogsEncrypted.js'),
         'glueS3EncryptionEnabled'       : require(__dirname + '/plugins/aws/glue/glueS3EncryptionEnabled.js'),
