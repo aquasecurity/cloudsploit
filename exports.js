d66116ee794e6b098506d6d9d8033ca8f519029f
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2021-08-20 21:06:47 +0500

    SAAS-2321: Application Layer Secrets  Encryption Level for GKE Clusters (#859)
    * Application Layer Secrets  Encryption Level for GKE Clusters
    
    * Fixed syntax issues
    
    * Update plugins/google/kubernetes/clusterEncryption.js
    
    Co-authored-by: AkhtarAmir <makhtar.pucit@gmail.com>
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- exports.js
+++ exports.js
@@ -764,6 +764,7 @@ module.exports = {
         'privateClusterEnabled'         : require(__dirname + '/plugins/google/kubernetes/privateClusterEnabled.js'),
         'basicAuthenticationDisabled'   : require(__dirname + '/plugins/google/kubernetes/basicAuthenticationDisabled.js'),
         'kubernetesAlphaDisabled'       : require(__dirname + '/plugins/google/kubernetes/kubernetesAlphaDisabled.js'),
+        'clusterEncryption'             : require(__dirname + '/plugins/google/kubernetes/clusterEncryption.js'),
 
         'dnsSecEnabled'                 : require(__dirname + '/plugins/google/dns/dnsSecEnabled.js'),
         'dnsSecSigningAlgorithm'        : require(__dirname + '/plugins/google/dns/dnsSecSigningAlgorithm.js'),
