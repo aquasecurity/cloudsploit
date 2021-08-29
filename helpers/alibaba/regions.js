b6641ba4acc438c0d9e33d8d4b2404c920661d8d
UmairAjmal <ajmal.umair@outlook.com>
2021-08-29 20:45:55 +0500

    Updated regions

--- helpers/alibaba/regions.js
+++ helpers/alibaba/regions.js
@@ -39,5 +39,7 @@ module.exports = {
     kms: regions,
     actiontrail: regions,
     ack: ['cn-hangzhou'],
-    apigateway: regions
+    apigateway: ['cn-hangzhou', 'cn-shanghai', 'cn-qingdao', 'cn-beijing', 'cn-zhangjiakou', 'cn-huhehaote', 'cn-shenzhen',
+        'cn-heyuan', 'cn-chengdu', 'cn-hongkong', 'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3', 'ap-southeast-5',
+        'ap-northeast-1', 'ap-south-1', 'eu-central-1', 'eu-west-1', 'us-west-1', 'us-east-1', 'me-east-1']
 };
