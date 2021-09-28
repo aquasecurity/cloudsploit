1d59b2d07b58015047ffe60373efe7e19cbc6433
Gio Rodriguez <gioroddev@gmail.com>
2021-09-28 11:07:09 -0700

    Update plugins/google/sql/sqlCMKEncryption.js

--- plugins/google/sql/sqlCMKEncryption.js
+++ plugins/google/sql/sqlCMKEncryption.js
@@ -4,7 +4,7 @@ var helpers = require('../../../helpers/google');
 module.exports = {
     title: 'SQL CMK Encryption',
     category: 'SQL',
-    description: 'Ensure that Cloud SQL instances are encrypted using Cutomer Managed Keys (CMKs).',
+    description: 'Ensure that Cloud SQL instances are encrypted using Customer Managed Keys (CMKs).',
     more_info: 'By default, your Google Cloud SQL instances are encrypted using Google-managed keys. To have a better control over the encryption process of your Cloud SQL instances you can use Customer-Managed Keys (CMKs).',
     link: 'https://cloud.google.com/sql/docs/sqlserver/cmek',
     recommended_action: 'Ensure that all Google Cloud SQL instances have desired encryption level.',
