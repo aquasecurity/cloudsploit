86a61011c02660a48464c717fec5c6baa01d54f3
Mehak Seedat <mehakseedat63@gmail.com>
2021-10-04 21:53:06 +0500

    GCP Compute Enable Usage Export

--- exports.js
+++ exports.js
@@ -727,6 +727,7 @@ module.exports = {
         'osLogin2FAEnabled'             : require(__dirname + '/plugins/google/compute/osLogin2FAEnabled.js'),
         'diskAutomaticBackupEnabled'    : require(__dirname + '/plugins/google/compute/diskAutomaticBackupEnabled.js'),
         'diskOldSnapshots'              : require(__dirname + '/plugins/google/compute/diskOldSnapshots.js'),
+        'enableUsageExport'             : require(__dirname + '/plugins/google/compute/enableUsageExport.js'),
 
         'keyRotation'                   : require(__dirname + '/plugins/google/cryptographickeys/keyRotation.js'),
         'keyProtectionLevel'            : require(__dirname + '/plugins/google/cryptographickeys/keyProtectionLevel.js'),
