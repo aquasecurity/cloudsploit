0cf10372905e7d32c99e38fff68f162e09a1b746
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-10-12 12:45:24 +0500

    Update plugins/aws/glacier/vaultPublicAccess.js

--- plugins/aws/glacier/vaultPublicAccess.js
+++ plugins/aws/glacier/vaultPublicAccess.js
@@ -3,7 +3,7 @@ var helpers = require('../../../helpers/aws');
 module.exports = {
     title: 'S3 Glacier Vault Public Access',
     category: 'S3',
-    description: 'Ensures S3 Glacier Vault public access block is enabled for the account',
+    description: 'Ensure that S3 Glacier Vault public access block is enabled for the account.',
     more_info: 'Blocking S3 Glacier Vault public access at the account level ensures objects are not accidentally exposed.',
     recommended_action: 'Add access policy for the S3 Glacier Vault to block public access for the AWS account.',
     link: 'https://docs.aws.amazon.com/amazonglacier/latest/dev/access-control-overview.html',
