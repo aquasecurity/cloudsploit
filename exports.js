28aa4a2689a7c1cd1f673b256b082f8d11d7899c
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2021-10-25 14:59:20 +0500

    Google Compute Engine VM Disks CMK Encrypted (#877)
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- exports.js
+++ exports.js
@@ -721,6 +721,7 @@ module.exports = {
         'instanceDesiredMachineTypes'   : require(__dirname + '/plugins/google/compute/instanceDesiredMachineTypes.js'),
         'automaticRestartEnabled'       : require(__dirname + '/plugins/google/compute/automaticRestartEnabled.js'),
         'instanceTemplateMachineTypes'  : require(__dirname + '/plugins/google/compute/instanceTemplateMachineTypes.js'),
+        'VMDisksCMKEncrypted'           : require(__dirname + '/plugins/google/compute/VMDisksCMKEncrypted.js'),
         'instanceMaintenanceBehavior'   : require(__dirname + '/plugins/google/compute/instanceMaintenanceBehavior.js'),
         'instancePreemptibility'        : require(__dirname + '/plugins/google/compute/instancePreemptibility.js'),
         'diskInUse'                     : require(__dirname + '/plugins/google/compute/diskInUse.js'),
