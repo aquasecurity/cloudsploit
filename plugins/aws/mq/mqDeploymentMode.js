0f8ac269ae00761833adcbabc06ec7b46653b5f1
Sadeed Rehman <sadeed1999@gmail.com>
2021-10-27 06:11:43 -0700

    Added correction

--- plugins/aws/mq/mqDeploymentMode.js
+++ plugins/aws/mq/mqDeploymentMode.js
@@ -4,7 +4,7 @@ var helpers = require('../../../helpers/aws');
 module.exports = {
     title: 'MQ Deployment Mode',
     category: 'MQ',
-    description: 'Ensure that for high availability, your AWS MQ brokers are using the active/standby deployment mode istead of single-instance ',
+    description: 'Ensure that for high availability, your AWS MQ brokers are using the active/standby deployment mode instead of single-instance ',
     more_info: 'With the active/standby deployment mode as opposed to the single-broker mode (enabled by default), you can achieve high availability for your Amazon MQ brokers as the service provides failure proof no risk.',
     recommended_action: 'Enabled Deployment Mode feature for MQ brokers',
     link: 'https://docs.aws.amazon.com/amazon-mq/latest/developer-guide/active-standby-broker-deployment.html',
