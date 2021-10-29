bb5fc3d6549e3336c53b628584899f19d3d07fe5
AkhtarAmir <makhtar.pucit@gmail.com>
2021-10-29 16:51:46 +0500

    Fixed issue in EC2 Instance VCPUs Limit plugin

--- plugins/aws/ec2/instanceLimit.js
+++ plugins/aws/ec2/instanceLimit.js
@@ -4,21 +4,21 @@ var helpers = require('../../../helpers/aws');
 module.exports = {
     title: 'Instance Limit',
     category: 'EC2',
-    description: 'Determine if the number of vCPUs used by running On-Demand EC2 instances is close to the AWS per-region limit.',
+    description: 'Determine if the number of EC2 instances is close to the AWS per-account limit',
     more_info: 'AWS limits accounts to certain numbers of resources. Exceeding those limits could prevent resources from launching.',
-    link: 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-on-demand-instances.html',
-    recommended_action: 'Contact AWS support to increase the number of EC2 instance vCPUs available',
-    apis: ['EC2:describeInstances', 'ServiceQuotas:listServiceQuotas'],
+    link: 'http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/elastic-ip-addresses-eip.html#using-instance-addressing-limit',
+    recommended_action: 'Contact AWS support to increase the number of instances available',
+    apis: ['EC2:describeAccountAttributes', 'EC2:describeInstances'],
     settings: {
         instance_limit_percentage_fail: {
             name: 'Instance Limit Percentage Fail',
-            description: 'Return a failing result when utilized EC2 instance vCPUs equals or exceeds this percentage',
+            description: 'Return a failing result when utilized instances equals or exceeds this percentage',
             regex: '^(100|[1-9][0-9]?)$',
             default: 90
         },
         instance_limit_percentage_warn: {
             name: 'Instance Limit Percentage Warn',
-            description: 'Return a warning result when utilized EC2 instance vCPUs equals or exceeds this percentage',
+            description: 'Return a warning result when utilized instances equals or exceeds this percentage',
             regex: '^(100|[1-9][0-9]?)$',
             default: 75
         }
@@ -36,20 +36,28 @@ module.exports = {
         var source = {};
         var regions = helpers.regions(settings);
 
-        async.each(regions.servicequotas, function(region, rcb){
-            var listServiceQuotas = helpers.addSource(cache, source,
-                ['servicequotas', 'listServiceQuotas', region]);
-                
-            if (!listServiceQuotas) return rcb();
+        async.each(regions.ec2, function(region, rcb){
+            var describeAccountAttributes = helpers.addSource(cache, source,
+                ['ec2', 'describeAccountAttributes', region]);
 
-            if (listServiceQuotas.err || !listServiceQuotas.data) {
+            if (!describeAccountAttributes) return rcb();
+
+            if (describeAccountAttributes.err || !describeAccountAttributes.data) {
                 helpers.addResult(results, 3,
-                    'Unable to list EC2 service quotas: ' + helpers.addError(listServiceQuotas), region);
+                    'Unable to query for account limits: ' + helpers.addError(describeAccountAttributes), region);
                 return rcb();
             }
 
-            let vCPUsQuota = listServiceQuotas.data.find(quota => quota.QuotaCode == 'L-1216C47A');
-            let vCPUsLimit = vCPUsQuota.Value ? vCPUsQuota.Value : 20;
+            var limits = {
+                'max-instances': 20
+            };
+
+            // Loop through response to assign custom limits
+            for (var i in describeAccountAttributes.data) {
+                if (limits[describeAccountAttributes.data[i].AttributeName]) {
+                    limits[describeAccountAttributes.data[i].AttributeName] = describeAccountAttributes.data[i].AttributeValues[0].AttributeValue;
+                }
+            }
 
             var describeInstances = helpers.addSource(cache, source,
                 ['ec2', 'describeInstances', region]);
@@ -62,25 +70,23 @@ module.exports = {
                 return rcb();
             }
 
-            var ec2vCPUs = 0;
+            var ec2Instances = 0;
 
             if (!describeInstances.data.length) {
                 helpers.addResult(results, 0, 'No instances found', region);
                 return rcb();
             } else {
                 for (var instances in describeInstances.data){
-                    for (var instance of describeInstances.data[instances].Instances){
-                        if (instance.State && instance.State.Name && instance.State.Name.toUpperCase() == 'RUNNING' &&
-                            instance.CpuOptions && instance.CpuOptions.CoreCount && instance.CpuOptions.ThreadsPerCore){
-                            let instancevCPUs = instance.CpuOptions.CoreCount * instance.CpuOptions.ThreadsPerCore;
-                            ec2vCPUs += instancevCPUs;
+                    for (var instance in describeInstances.data[instances].Instances){
+                        if (!describeInstances.data[instances].Instances[instance].SpotInstanceRequestId){
+                            ec2Instances += 1;
                         }
                     }
                 }
             }
 
-            var percentage = Math.ceil((ec2vCPUs / vCPUsLimit)*100);
-            var returnMsg = 'Account contains ' + ec2vCPUs + ' of ' + vCPUsLimit + ' (' + percentage + '%) available vCPUs';
+            var percentage = Math.ceil((ec2Instances / limits['max-instances'])*100);
+            var returnMsg = 'Account contains ' + ec2Instances + ' of ' + limits['max-instances'] + ' (' + percentage + '%) available instances';
 
             if (percentage >= config.instance_limit_percentage_fail) {
                 helpers.addResult(results, 2, returnMsg, region, null, custom);
