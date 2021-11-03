de1ba88a3c16b5f21156e02128fcebdc742c49c7
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2021-11-03 14:59:07 +0500

    GCP Modify Disk Plugins to use aggregated list (#921)
    Co-authored-by: AkhtarAmir <makhtar.pucit@gmail.com>

--- plugins/google/compute/diskAutomaticBackupEnabled.spec.js
+++ plugins/google/compute/diskAutomaticBackupEnabled.spec.js
@@ -4,8 +4,8 @@ var plugin = require('./diskAutomaticBackupEnabled');
 const createCache = (diskData, error) => {
     return {
         disks: {
-            list: {
-                'us-central1-a': {
+            aggregatedList: {
+                'global': {
                     data: diskData,
                     err: error
                 }
@@ -28,7 +28,7 @@ describe('diskAutomaticBackupEnabled', function () {
                 expect(results.length).to.be.above(0);
                 expect(results[0].status).to.equal(3);
                 expect(results[0].message).to.include('Unable to query compute disks');
-                expect(results[0].region).to.equal('us-central1');
+                expect(results[0].region).to.equal('global');
                 done()
             };
 
@@ -45,7 +45,7 @@ describe('diskAutomaticBackupEnabled', function () {
                 expect(results.length).to.be.above(0);
                 expect(results[0].status).to.equal(0);
                 expect(results[0].message).to.include('No compute disks found');
-                expect(results[0].region).to.equal('us-central1');
+                expect(results[0].region).to.equal('us-east1');
                 done()
             };
 
@@ -62,44 +62,30 @@ describe('diskAutomaticBackupEnabled', function () {
                 expect(results.length).to.be.above(0);
                 expect(results[0].status).to.equal(2);
                 expect(results[0].message).to.include('Snapshot schedule is not configured for');
-                expect(results[0].region).to.equal('us-central1');
+                expect(results[0].region).to.equal('us-east1');
                 done()
             };
 
             const cache = createCache(
-                [
-                    {
-                        "kind": "compute#disk",
-                        "id": "11111",
-                        "creationTimestamp": "2019-09-25T14:05:30.090-07:00",
-                        "name": "instance-2",
-                        "sizeGb": "10",
-                        "zone": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a",
-                        "status": "READY",
-                        "selfLink": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/disks/instance-2",
-                        "sourceImage": "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-9-stretch-v20190916",
-                        "sourceImageId": "2382294996946296915",
-                        "type": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/diskTypes/pd-standard",
-                        "licenses": [
-                            "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/licenses/debian-9-stretch"
-                        ],
-                        "guestOsFeatures": [
+                {
+                    "regions/us-east1": {
+                        "disks": [
                             {
-                                "type": "VIRTIO_SCSI_MULTIQUEUE"
+                                "id": "1111111",
+                                "creationTimestamp": "2021-09-23T12:58:54.065-07:00",
+                                "name": "disk-1",
+                                "sizeGb": "10",
+                                "status": "READY",
+                                "selfLink": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1/disks/disk-1",
+                                "type": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1/diskTypes/pd-balanced",
+                                "labelFingerprint": "42WmSpB8rSM=",
+                                "region": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1",
+                                "physicalBlockSizeBytes": "4096",
+                                "kind": "compute#disk"
                             }
-                        ],
-                        "lastAttachTimestamp": "2019-09-25T14:05:30.090-07:00",
-                        "users": [
-                            "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/instances/instance-2"
-                        ],
-                        "labelFingerprint": "42WmSpB8rSM=",
-                        "licenseCodes": [
-                            "1000205"
-                        ],
-                        "physicalBlockSizeBytes": "4096"
-                    }
-                ],
-                null
+                        ]
+                    },
+                }
             );
 
             plugin.run(cache, {}, callback);
@@ -110,51 +96,34 @@ describe('diskAutomaticBackupEnabled', function () {
                 expect(results.length).to.be.above(0);
                 expect(results[0].status).to.equal(0);
                 expect(results[0].message).to.include('Snapshot schedule is configured for');
-                expect(results[0].region).to.equal('us-central1');
+                expect(results[0].region).to.equal('us-east1');
                 done()
             };
 
             const cache = createCache(
-                [
-                    {
-                        "kind": "compute#disk",
-                        "id": "5472446060006107254",
-                        "creationTimestamp": "2019-09-25T14:05:30.090-07:00",
-                        "name": "instance-2",
-                        "sizeGb": "10",
-                        "zone": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a",
-                        "status": "READY",
-                        "selfLink": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/disks/instance-2",
-                        "sourceImage": "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-9-stretch-v20190916",
-                        "sourceImageId": "2382294996946296915",
-                        "type": "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/diskTypes/pd-standard",
-                        "licenses": [
-                            "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/licenses/debian-9-stretch"
-                        ],
-                        "guestOsFeatures": [
+                {
+                    "regions/us-east1": {
+                        "disks": [
                             {
-                                "type": "VIRTIO_SCSI_MULTIQUEUE"
+                                "id": "1111111",
+                                "creationTimestamp": "2021-09-23T12:58:54.065-07:00",
+                                "name": "disk-1",
+                                "sizeGb": "10",
+                                "status": "READY",
+                                "selfLink": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1/disks/disk-1",
+                                "type": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1/diskTypes/pd-balanced",
+                                "labelFingerprint": "42WmSpB8rSM=",
+                                "region": "https://www.googleapis.com/compute/v1/projects/my-test-project/regions/us-east1",
+                                "physicalBlockSizeBytes": "4096",
+                                "kind": "compute#disk",
+                                "resourcePolicies": [
+                                    'https://www.googleapis.com/compute/v1/projects/my-project/regions/us-east1/resourcePolicies/schedule-1'
+                                  ],
                             }
-                        ],
-                        "lastAttachTimestamp": "2019-09-25T14:05:30.090-07:00",
-                        "users": [
-                            "https://www.googleapis.com/compute/v1/projects/rosy-booth-253119/zones/us-central1-a/instances/instance-2"
-                        ],
-                        "labelFingerprint": "42WmSpB8rSM=",
-                        "licenseCodes": [
-                            "1000205"
-                        ],
-                        "resourcePolicies": [
-                            'https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/resourcePolicies/schedule-1'
-                          ],
-                        "physicalBlockSizeBytes": "4096",
-                        "diskEncryptionKey": {
-                            'key': 'true',
-                        }
-                    }
-                ]
+                        ]
+                    },
+                }        
             );
-
             plugin.run(cache, {}, callback);
         })
 
