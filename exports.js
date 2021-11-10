51728336ac396260616d75ba730ab7fe8a70b369
AkhtarAmir <makhtar.pucit@gmail.com>
2021-11-10 12:43:50 +0500

    Added AWS Elastic Trancoder Pipeline Data Encrypted plugin and tests

--- exports.js
+++ exports.js
@@ -377,7 +377,9 @@ module.exports = {
 
         'codebuildValidSourceProviders' : require(__dirname + '/plugins/aws/codebuild/codebuildValidSourceProviders.js'),
 
-        'codestarValidRepoProviders'    : require(__dirname + '/plugins/aws/codestar/codestarValidRepoProviders.js')
+        'codestarValidRepoProviders'    : require(__dirname + '/plugins/aws/codestar/codestarValidRepoProviders.js'),
+
+        'pipelineDataEncrypted'         : require(__dirname + '/plugins/aws/elastictranscoder/pipelineDataEncrypted.js')
     },
     azure : {
         'fileServiceEncryption'         : require(__dirname + '/plugins/azure/storageaccounts/fileServiceEncryption.js'),
