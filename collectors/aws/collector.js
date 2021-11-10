5573fda05790369c7d3955d0b9cb6749c2250764
AkhtarAmir <makhtar.pucit@gmail.com>
2021-11-10 17:33:39 +0500

    Added Elastic Transcoder Job Output Encrypted plugin and spec

--- collectors/aws/collector.js
+++ collectors/aws/collector.js
@@ -1085,6 +1085,14 @@ var postcalls = [
                 override: true
             }
         },
+        ElasticTranscoder: {
+            listJobsByPipeline : {
+                reliesOnService: 'elastictranscoder',
+                reliesOnCall: 'listPipelines',
+                filterKey: 'PipelineId',
+                filterValue: 'Id'
+            }
+        },
         ELB: {
             describeLoadBalancerPolicies: {
                 reliesOnService: 'elb',
