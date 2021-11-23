39f429bb01f8ef622549e847649cf67468a31291
dipsubha06 <68757892+dipsubha06@users.noreply.github.com>
2021-11-23 16:02:52 +0530

    Update plugins/aws/appflow/flowEncrypted.js

--- plugins/aws/appflow/flowEncrypted.js
+++ plugins/aws/appflow/flowEncrypted.js
@@ -82,7 +82,7 @@ module.exports = {
                     if (!describeKey || describeKey.err || !describeKey.data || !describeKey.data.KeyMetadata) {
                         helpers.addResult(results, 3,
                             `Unable to query KMS key: ${helpers.addError(describeKey)}`,
-                            region, kmsKeyId);
+                            region, describeFlow.data.kmsArn);
                         continue;
                     }
 
