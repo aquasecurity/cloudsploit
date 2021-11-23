2a1d01b8a823b6db7a8211de4c388950e83cddd5
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-11-23 15:31:15 +0500

    SAAS-3023: Added AWS Elastic Trancoder Pipeline Data Encrypted plugin and tests (#962)
    * Added AWS Elastic Trancoder Pipeline Data Encrypted plugin and tests
    
    * Update plugins/aws/elastictranscoder/pipelineDataEncrypted.js
    
    Co-authored-by: dipsubha06 <68757892+dipsubha06@users.noreply.github.com>

--- exports.js
+++ exports.js
@@ -380,6 +380,7 @@ module.exports = {
 
         'codestarValidRepoProviders'    : require(__dirname + '/plugins/aws/codestar/codestarValidRepoProviders.js'),
 
+        'pipelineDataEncrypted'         : require(__dirname + '/plugins/aws/elastictranscoder/pipelineDataEncrypted.js'),
         'jobOutputsEncrypted'           : require(__dirname + '/plugins/aws/elastictranscoder/jobOutputsEncrypted.js'),
       
         'translateJobOutputEncrypted'   : require(__dirname + '/plugins/aws/translate/translateJobOutputEncrypted.js'),
