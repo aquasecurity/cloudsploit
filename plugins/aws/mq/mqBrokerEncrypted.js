e62390b995e3069335ad0e26c8c6e6ab4230497e
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2021-11-24 13:41:00 +0500

    Update plugins/aws/mq/mqBrokerEncrypted.js

--- plugins/aws/mq/mqBrokerEncrypted.js
+++ plugins/aws/mq/mqBrokerEncrypted.js
@@ -92,7 +92,6 @@ module.exports = {
                             `Unable to query KMS key: ${helpers.addError(describeKey)}`,
                             region, KmsKeyId);
                         continue;
-
                     }
                 
                     currentEncryptionLevel = helpers.getEncryptionLevel(describeKey.data.KeyMetadata, helpers.ENCRYPTION_LEVELS);
