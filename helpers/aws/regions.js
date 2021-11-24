eebb4a17d118062f3c5a5c12d1c7176a45efab4f
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-11-24 17:58:13 +0500

    Update helpers/aws/regions.js

--- helpers/aws/regions.js
+++ helpers/aws/regions.js
@@ -113,7 +113,7 @@ module.exports = {
     mq: regions,
     glacier: regions,
     customerprofiles: ['us-east-1', 'us-west-2', 'eu-west-2', 'ca-central-1', 'eu-central-1',
-        'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2'],
+        'ap-southeast-1', 'ap-northeast-1', 'ap-southeast-2', 'ap-northeast-2'],
     finspace: ['us-east-1', 'us-east-2', 'us-west-2', 'ca-central-1','eu-west-1'],
     codepipeline: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ap-east-1', 'ap-south-1', 'ca-central-1', 'eu-central-1',
         'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'eu-south-1', 'ap-northeast-1',
