2b6219b04f3dd44764424e3d73ed291569d72e9c
Muhammad Nouman <m.nouman.suleman@gmail.com>
2021-11-25 07:58:17 -0800

    Refactored code and added spec

--- plugins/aws/waf/wafInUse.js
+++ plugins/aws/waf/wafInUse.js
@@ -9,26 +9,26 @@ module.exports = {
     more_info: 'Using WAF for your web application running in AWS environment against common web-based attacks, SQL injection attacks, DDOS attacks and more.',
     link: 'https://docs.aws.amazon.com/waf/latest/developerguide/what-is-aws-waf.html',
     recommended_action: 'Create one or more WAF ACLs with proper actions and rules',
-    apis: ['WAF:listWebACLs'],
+    apis: ['WAF:listWebACLs', 'WAFRegional:listWebACLs'],
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
 
-        async.each(regions.waf, function(region, rcb){
-            var listWebACLs = helpers.addSource(cache, source,
-                ['waf', 'listWebACLs', region]);
+        async.each(regions.wafregional, function(region, rcb){
+            var listRegionalWebACLs = helpers.addSource(cache, source,
+                ['wafregional', 'listWebACLs', region]);
 
-            if (!listWebACLs) return rcb();
+            if (!listRegionalWebACLs) return rcb();
 
-            if (listWebACLs.err || !listWebACLs.data) {
+            if (listRegionalWebACLs.err || !listRegionalWebACLs.data) {
                 helpers.addResult(results, 3,
-                    'Unable to query for WAF: ' + helpers.addError(listWebACLs), region);
+                    'Unable to query for WAF: ' + helpers.addError(listRegionalWebACLs), region);
                 return rcb();
             }
 
-            if (!listWebACLs.data.length) {
+            if (!listRegionalWebACLs.data.length) {
                 helpers.addResult(results, 2, 'WAF is not enabled', region);
             } else {
                 helpers.addResult(results, 0, 'WAF is enabled', region);
@@ -36,7 +36,26 @@ module.exports = {
 
             rcb();
         }, function(){
+            var listGlobalWebACLs = helpers.addSource(cache, source,
+                ['waf', 'listWebACLs', regions.waf]);
+
+            if (!listGlobalWebACLs) return callback(null, results, source);
+
+            if (listGlobalWebACLs.err || !listGlobalWebACLs.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for Global WAF: ' + helpers.addError(listGlobalWebACLs));
+                return callback(null, results, source);
+            }
+
+            if (!listGlobalWebACLs.data.length) {
+                helpers.addResult(results, 2, 'WAF is not enabled');
+            } else {
+                helpers.addResult(results, 0, 'WAF is enabled');
+            }
+
             callback(null, results, source);
         });
+
+
     }
 };
