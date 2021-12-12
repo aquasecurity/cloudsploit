add13f8978bb2a267880a391e77c5cf94166ef0f
Muhammad Nouman <m.nouman.suleman@gmail.com>
2021-12-12 12:03:07 -0800

    Incorporated feedback changes

--- plugins/aws/elasticbeanstalk/enhancedHealthReporting.js
+++ plugins/aws/elasticbeanstalk/enhancedHealthReporting.js
@@ -5,12 +5,11 @@ module.exports = {
     title: 'Enhanced Health Reporting',
     category: 'ElasticBeanstalk',
     domain: 'Application Integration',
-    description: 'Enhanced health reporting allow AWS Elastic Beanstalk to gather additional information about'
-                + 'resources in your environment and aid in the identification of issues that can cause your'
-                + 'application to become unavailable.',
-    more_info: 'Enhanced Health Reporting is the AWS Elastic Beanstalk feature that allows the service to gather'
-                + 'additional information about the resources available within your EB environments.',
-    recommended_action: 'Ensure that the Enhanced Health Reporting feature is enabled for all Amazon Elastic Beanstalk (EB) environments provisioned in your AWS account.',
+    description: 'Ensure that Amazon Elastic Beanstalk (EB) environments have enhanced health reporting feature enabled.',
+    more_info: 'Enhanced health reporting is a feature that you can enable on your environment to allow AWS Elastic Beanstalk to gather additional'
+        + 'information about resources in your environment. Elastic Beanstalk analyzes the information gathered to provide a better picture of overall'
+        + 'environment health and aid in the identification of issues that can cause your application to become unavailable.',
+    recommended_action: 'Modify Elastic Beanstalk environmentsand enable enhanced health reporting.',
     link: 'https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/health-enhanced.html',
     apis: ['ElasticBeanstalk:describeEnvironments'],
 
@@ -27,27 +26,25 @@ module.exports = {
 
             if (describeEnvironments.err || !describeEnvironments.data) {
                 helpers.addResult(results, 3,
-                    'Unable to query for application environments', region);
+                    'Unable to query for Elastic Beanstalk environments', region);
                 return rcb();
             }
 
             if (!describeEnvironments.data.length) {
                 helpers.addResult(results, 0,
-                    'No application environments found', region);
+                    'No Elastic Beanstalk environments found', region);
                 return rcb();
             }
 
-            async.each(describeEnvironments.data, function(environment, ecb){
+            for (let environment of describeEnvironments.data) {
                 var resource = environment.EnvironmentArn;
 
-                if (environment.Health === 'Green') {
-                    helpers.addResult(results, 0, `Enhanced Health Reporting feature is enabled for environment ${environment.EnvironmentName}. Environment health is ${environment.HealthStatus}`, region, resource);
+                if (environment.Health && environment.HealthStatus) {
+                    helpers.addResult(results, 0, `Enhanced Health Reporting feature is enabled for environment ${environment.EnvironmentName}.`, region, resource);
                 } else {
                     helpers.addResult(results, 2, `Enhanced Health Reporting feature is not enabled for environment: ${environment.EnvironmentName}`, region, resource);
                 }
-
-                ecb();
-            });
+            }
 
             rcb();
         }, function(){
