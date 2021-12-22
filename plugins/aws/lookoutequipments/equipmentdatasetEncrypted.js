2de25ad75e23a3ea0945337b7311240c27f57920
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2021-12-22 20:56:42 +0500

    Update plugins/aws/lookoutequipments/equipmentdatasetEncrypted.js

--- plugins/aws/lookoutequipments/equipmentdatasetEncrypted.js
+++ plugins/aws/lookoutequipments/equipmentdatasetEncrypted.js
@@ -97,7 +97,7 @@ module.exports = {
 
                 if (currentEncryptionLevel >= desiredEncryptionLevel) {
                     helpers.addResult(results, 0,
-                        `LookoutEquipment Datasets is encrypted with ${currentEncryptionLevelString} \
+                        `LookoutEquipment Datasets is using ${currentEncryptionLevelString} \
                         which is greater than or equal to the desired encryption level ${config.desiredEncryptionLevelString}`,
                         region, resource);
                 } else {
