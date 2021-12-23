72efa8e387dd23292b0faaa88496fd58a1a07da5
dipsubha06 <68757892+dipsubha06@users.noreply.github.com>
2021-12-23 21:20:24 +0530

    Update plugins/aws/kinesisvideo/videostreamDataEncrypted.spec.js

--- plugins/aws/kinesisvideo/videostreamDataEncrypted.spec.js
+++ plugins/aws/kinesisvideo/videostreamDataEncrypted.spec.js
@@ -140,7 +140,7 @@ describe('videostreamDataEncrypted', function () {
         });
 
         it('should UNKNOWN if unable to list KMS keys', function (done) {
-            const cache = createCache(null, null, null, { message: "Unable to list KMS keys" });
+            const cache = createCache(null, null, null, null, { message: "Unable to list KMS keys" });
             videostreamDataEncrypted.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
