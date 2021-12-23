c6e296711ae28497ea0d8ba7a6117f7392ad4dda
AkhtarAmir <makhtar.pucit@gmail.com>
2021-12-23 19:30:17 +0500

    Linted

--- collectors/aws/collector.js
+++ collectors/aws/collector.js
@@ -20,11 +20,11 @@ var async = require('async');
 var https = require('https');
 var helpers = require(__dirname + '/../../helpers/aws');
 var collectors = require(__dirname + '/../../collectors/aws');
- 
+
 // Override max sockets
 var agent = new https.Agent({maxSockets: 100});
 AWS.config.update({httpOptions: {agent: agent}});
- 
+
 var globalServices = [
     'S3',
     'IAM',
