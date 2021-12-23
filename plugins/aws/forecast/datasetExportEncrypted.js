123a22ae4adf79a6686c4579ab86ee08d8ef2da3
Muhammad Nouman <m.nouman.suleman@gmail.com>
2021-12-23 15:02:22 -0800

    Incorporated feedback changes

--- plugins/aws/forecast/datasetExportEncrypted.js
+++ plugins/aws/forecast/datasetExportEncrypted.js
@@ -10,12 +10,26 @@ module.exports = {
     recommended_action: 'Create Forecast exports with encryption enabled',
     link: 'https://docs.aws.amazon.com/forecast/latest/dg/howitworks-forecast.html',
     apis: ['ForecastService:listForecastExportJobs', 'KMS:listKeys', 'KMS:describeKey'],
-    settings: {},
+    settings: {
+        forecast_dataset_desired_encryption_level: {
+            name: 'Forecast Dataset Desired Encryption Level',
+            description: 'In order (lowest to highest) awskms=AWS managed KMS; awscmk=Customer managed KMS; externalcmk=Customer managed externally sourced KMS; cloudhsm=Customer managed CloudHSM sourced KMS',
+            regex: '^(awscmk|externalcmk|cloudhsm)$',
+            default: 'awscmk'
+        }
+    },
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
+
+        var config = {
+            desiredEncryptionLevelString: settings.forecast_dataset_desired_encryption_level || this.settings.forecast_dataset_desired_encryption_level.default
+        };
+
+        var desiredEncryptionLevel = helpers.ENCRYPTION_LEVELS.indexOf(config.desiredEncryptionLevelString);
+        var currentEncryptionLevel;
 
         async.each(regions.forecastservice, function(region, rcb){
             var listForecastExportJobs = helpers.addSource(cache, source,
@@ -30,20 +44,58 @@ module.exports = {
             }
 
             if (!listForecastExportJobs.data.length) {
-                helpers.addResult(results, 0, 'No forecast export found', region);
+                helpers.addResult(results, 0, 'No forecast exports found', region);
+                return rcb();
+            }
+
+            var listKeys = helpers.addSource(cache, source,
+                ['kms', 'listKeys', region]);
+
+            if (!listKeys || listKeys.err || !listKeys.data) {
+                helpers.addResult(results, 3,
+                    `Unable to list KMS keys: ${helpers.addError(listKeys)}`, region);
                 return rcb();
             }
 
             for (let forecastExportJob of listForecastExportJobs.data) {
+                if (!forecastExportJob && !forecastExportJob.Destination) {
+                    continue;
+                }
+
                 let { S3Config } = forecastExportJob.Destination;
                 let resource = forecastExportJob.ForecastExportJobArn;
 
-                if (S3Config.KMSKeyArn) {
-                    helpers.addResult(results, 0,
-                        `Forecast Dataset Export is with ${S3Config.KMSKeyArn}`, region, resource);
+                if (S3Config && S3Config.KMSKeyArn) {
+                    let encryptionKey = S3Config.KMSKeyArn;
+                    var keyId = encryptionKey.split('/')[1] ? encryptionKey.split('/')[1] : encryptionKey;
+
+                    var describeKey = helpers.addSource(cache, source,
+                        ['kms', 'describeKey', region, keyId]);
+
+                    if (!describeKey || describeKey.err || !describeKey.data || !describeKey.data.KeyMetadata) {
+                        helpers.addResult(results, 3,
+                            `Unable to query KMS key: ${helpers.addError(describeKey)}`,
+                            region, encryptionKey);
+                        continue;
+                    }
+
+                    currentEncryptionLevel = helpers.getEncryptionLevel(describeKey.data.KeyMetadata, helpers.ENCRYPTION_LEVELS);
+                    let currentEncryptionLevelString = helpers.ENCRYPTION_LEVELS[currentEncryptionLevel];
+
+                    if (currentEncryptionLevel >= desiredEncryptionLevel) {
+                        helpers.addResult(results, 0,
+                            `Forecast dataset export is encrypted with ${currentEncryptionLevelString} \
+                                which is greater than or equal to the desired encryption level ${config.desiredEncryptionLevelString}`,
+                            region, resource);
+                    } else {
+                        helpers.addResult(results, 2,
+                            `Forecast dataset export is encrypted with ${currentEncryptionLevelString} \
+                                which is less than the desired encryption level ${config.desiredEncryptionLevelString}`,
+                            region, resource);
+                    }
                 } else {
                     helpers.addResult(results, 2,
-                        'Forecast Dataset Export is not encrypted', region, resource);
+                        'Forecast dataset export does not have encryption enabled', region, resource);
                 }
             }
 
