63d27815192443248225308e71ed96a4a542156b
Muhammad Nouman <m.nouman.suleman@gmail.com>
2021-12-23 07:13:33 -0800

    Incorporated feedback changes

--- plugins/aws/waf/wafInUse.js
+++ plugins/aws/waf/wafInUse.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'WAF',
     domain: 'Availability',
     description: 'Ensure that AWS Web Application Firewall (WAF) is in use to achieve availability and security for AWS-powered web applications.',
-    more_info: 'Using WAF for your web application running in AWS environment against common web-based attacks, SQL injection attacks, DDOS attacks and more.',
+    more_info: 'Using WAF for your web application running in AWS environment can help against common web-based attacks, SQL injection attacks, DDOS attacks and more.',
     link: 'https://docs.aws.amazon.com/waf/latest/developerguide/what-is-aws-waf.html',
     recommended_action: 'Create one or more WAF ACLs with proper actions and rules',
     apis: ['WAF:listWebACLs', 'WAFRegional:listWebACLs'],
@@ -48,14 +48,12 @@ module.exports = {
             }
 
             if (!listGlobalWebACLs.data.length) {
-                helpers.addResult(results, 2, 'WAF is not enabled');
+                helpers.addResult(results, 2, 'WAF is not enabled', 'global');
             } else {
-                helpers.addResult(results, 0, 'WAF is enabled');
+                helpers.addResult(results, 0, 'WAF is enabled', 'global');
             }
 
             callback(null, results, source);
         });
-
-
     }
 };
