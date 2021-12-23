70758ce170e080a48200fd377cfb1c1a975c8d1c
dipsubha06 <68757892+dipsubha06@users.noreply.github.com>
2021-12-23 21:05:23 +0530

    Update plugins/aws/location/trackerDataEncrypted.spec.js

--- plugins/aws/location/trackerDataEncrypted.spec.js
+++ plugins/aws/location/trackerDataEncrypted.spec.js
@@ -170,7 +170,7 @@ describe('trackerDataEncrypted', function () {
         });
 
         it('should UNKNOWN if unable to list KMS keys', function (done) {
-            const cache = createCache(listTrackers, null, null, null, { message: "Unable to list KMS keys" });
+            const cache = createCache(listTrackers, null, null, null, null, { message: "Unable to list KMS keys" });
             trackerDataEncrypted.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
