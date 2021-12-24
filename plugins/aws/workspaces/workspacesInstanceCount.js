02e0c084ed945856ca0504181c046478b41fb653
Muhammad Nouman <m.nouman.suleman@gmail.com>
2021-12-23 18:10:40 -0800

    Incorporated feedback changes

--- plugins/aws/workspaces/workspacesInstanceCount.js
+++ plugins/aws/workspaces/workspacesInstanceCount.js
@@ -10,13 +10,20 @@ module.exports = {
     recommended_action: 'Ensure that number of WorkSpaces created within your AWS account is within set limit',
     link: 'https://docs.aws.amazon.com/workspaces/latest/adminguide/workspaces-limits.html',
     apis: ['WorkSpaces:describeWorkspaces'],
+    settings: {
+        workspace_instance_limit: {
+            name: 'Limit for the number of Workspace instances.',
+            description: 'Plugin results will become aggregated once this value is breached',
+            default: 50
+        }
+    },
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
+        var workspace_instance_limit = settings.workspace_instance_limit || this.settings.workspace_instance_limit.default;
         var instanceCount = 0;
-        const RECOMMENDED_THRESHOLD = 50;
 
         async.each(regions.workspaces, function(region, rcb){
             var listWorkspaces = helpers.addSource(cache, source, ['workspaces', 'describeWorkspaces', region]);
@@ -25,19 +32,22 @@ module.exports = {
                 return rcb();
             }
 
-            if (listWorkspaces.err || !listWorkspaces.data) {
-                return rcb();
+            if (!listWorkspaces || listWorkspaces.err || !listWorkspaces.data || !listWorkspaces.data.length) {
+                helpers.addResult(results, 3,
+                    'Unable to list Workspaces: ' + helpers.addError(listWorkspaces), region);
+                return callback(null, results, source);
             }
 
             instanceCount += listWorkspaces.data.length;
 
             return rcb();
         }, function(){
-            if (instanceCount > RECOMMENDED_THRESHOLD){
-                helpers.addResult(results, 2, `Workspaces Instance count is greater then the recommended threshold
-                                                i.e. ${RECOMMENDED_THRESHOLD} workspaces`);
+            if (instanceCount > workspace_instance_limit){
+                helpers.addResult(results, 2, `Workspaces Instance count is greater than the desired threshold
+                    i.e. ${workspace_instance_limit} workspaces`);
             } else {
-                helpers.addResult(results, 0, 'Workspaces Instance count is within the recommended threshold');
+                helpers.addResult(results, 0, `Workspaces Instance count is within the desired threshold
+                    i.e. ${workspace_instance_limit} workspaces`);
             }
 
             callback(null, results, source);
--- plugins/aws/workspaces/workspacesInstanceCount.spec.js
+++ plugins/aws/workspaces/workspacesInstanceCount.spec.js
@@ -56,20 +56,31 @@ const createCache = (data, err) => {
 
 describe('workspacesInstanceCount', function () {
     describe('run', function () {
-        it('should PASS if all Workspaces count is upto the recommended threshold i.e. 50', function (done) {
+        it('should PASS if all Workspaces count is upto the desired threshold i.e. 10', function (done) {
             const cache = createCache([describeWorkspaces[0]]);
-            workspacesInstanceCount.run(cache, {}, (err, results) => {
+            workspacesInstanceCount.run(cache, { workspace_instance_limit: 10 }, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Workspaces Instance count is within the desired threshold');
                 done();
             });
         });
 
-        it('should FAIL if Workspaces count is greater than the recommended threshold', function (done) {
-            const cache = createCache(Array(51).fill(describeWorkspaces[0]));
-            workspacesInstanceCount.run(cache, {}, (err, results) => {
+        it('should FAIL if Workspaces count is greater than the desired threshold', function (done) {
+            const cache = createCache(Array(3).fill(describeWorkspaces[0]));
+            workspacesInstanceCount.run(cache, { workspace_instance_limit: 2 }, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('Workspaces Instance count is greater than the desired threshold');
+                done();
+            });
+        });
+
+        it('should UNKNOWN if unable to list Workspaces', function (done) {
+            const cache = createCache([]);
+            workspacesInstanceCount.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
                 done();
             });
         });
