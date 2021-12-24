d8e3879fe8c24c6e8fa57fd925628044744e0764
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-12-24 18:20:14 +0500

    Update plugins/aws/documentDB/docdbClusterBackupRetention.js

--- plugins/aws/documentDB/docdbClusterBackupRetention.js
+++ plugins/aws/documentDB/docdbClusterBackupRetention.js
@@ -6,9 +6,7 @@ module.exports = {
     category: 'DocumentDB',
     domain: 'Databases',
     description: 'Ensure that your Amazon DocumentDB clusters have set a minimum backup retention period.',
-    more_info: `DocumentDB cluster provides feature to retain incremental backups between 1 and 35 allowing
-                you to quickly restore to any point within the backup retention period. Ensure that you have
-                sufficient backup retention period configured in order to restore your data in the event of failure.`,
+    more_info: 'DocumentDB cluster provides feature to retain incremental backups between 1 and 35 allowing you to quickly restore to any point within the backup retention period. Ensure that you have sufficient backup retention period configured in order to restore your data in the event of failure.',
     recommended_action: 'Modify DocumentDb cluster to configure sufficient backup retention period.',
     link: 'https://docs.aws.amazon.com/documentdb/latest/developerguide/db-cluster-modify.html',
     apis: ['DocDB:describeDBClusters'],
