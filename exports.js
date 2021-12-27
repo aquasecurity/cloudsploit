1172de5451c7f507497da6a1508761f3fb817c2e
Muhammad Nouman Suleman <m.nouman.suleman@gmail.com>
2021-12-27 02:12:57 -0800

    Added enhancedHealthReporting plugin and spec (#1018)
    * Added enhancedHealthReporting plugin and spec
    
    * Changed the info and error message
    
    * Incorporated feedback changes
    
    Co-authored-by: dipsubha06 <68757892+dipsubha06@users.noreply.github.com>

--- exports.js
+++ exports.js
@@ -160,6 +160,7 @@ module.exports = {
         'ecrRepositoryTagImmutability'  : require(__dirname + '/plugins/aws/ecr/ecrRepositoryTagImmutability.js'),
         
         'managedPlatformUpdates'        : require(__dirname + '/plugins/aws/elasticbeanstalk/managedPlatformUpdates.js'),
+        'enhancedHealthReporting'       : require(__dirname + '/plugins/aws/elasticbeanstalk/enhancedHealthReporting.js'),
         'environmentAccessLogs'         : require(__dirname + '/plugins/aws/elasticbeanstalk/environmentAccessLogs.js'),
         
         'eksKubernetesVersion'          : require(__dirname + '/plugins/aws/eks/eksKubernetesVersion.js'),
