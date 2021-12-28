a71a63ddc6f4ee7efbba7da4d2a49fd62eb4490b
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2021-12-28 14:02:51 +0500

    Update plugins/aws/connect/voiceIdDomainEncrypted.js

--- plugins/aws/connect/voiceIdDomainEncrypted.js
+++ plugins/aws/connect/voiceIdDomainEncrypted.js
@@ -87,7 +87,7 @@ module.exports = {
 
                 if (currentEncryptionLevel >= desiredEncryptionLevel) {
                     helpers.addResult(results, 0,
-                        `VoiceID domain is encrypted with ${currentEncryptionLevelString} \
+                        `Voice ID domain is encrypted with ${currentEncryptionLevelString} \
                         which is greater than or equal to the desired encryption level ${config.desiredEncryptionLevelString}`,
                         region, resource);
                 } else {
