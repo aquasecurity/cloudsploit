e58d335779e42933ce8fab3e09dba7e9780c3c5c
AkhtarAmir <makhtar.pucit@gmail.com>
2022-01-09 22:51:58 +0500

    Added Oracle User Customer Secret Keys Rotated plgin and spec

--- plugins/oracle/identity/userCustomerSecretKeysRotated.spec.js
+++ plugins/oracle/identity/userCustomerSecretKeysRotated.spec.js
@@ -1,28 +1,30 @@
 var expect = require('chai').expect;
-var plugin = require('./userAuthTokenRotated');
+var plugin = require('./userCustomerSecretKeysRotated');
 
-const authTokens = [
+const customerSecretKeys = [
     {
-        "id": "ocid1.credential.oc1..aaaaaaaa4f57ig72lh5gu6hicy5d5nl7wzb6q",
-        "compartmentId": "ocid1.tenancy.oc1..aaaaaaaaip3imsyctt3vhme6rnemweaxe4b6q2gcodq",
-        "description": "For CIS",
+        "id": "5f60514e3d78e8e14",
+        "userId": "ocid1.user.oc1..aaaaaaaarekfkxmha6u5cyvqjkmvo7kdxa",
+        "displayName": "For CIS",
         "timeCreated": new Date(),
-        "userId": "ocid1.user.oc1..aaaaaaaaddzaosqxqgeryy43emaptcnzzcrekfkxmha6u5cyvqjkmvo7kdxa",
         "lifecycleState": "ACTIVE",
-        "users": "ocid1.user.oc1..aaaaaaaaddzaosqxqgeryy43emaptcnzzcrekfkxmha6u5cyvqjkmvo7kdxa"
+        "compartmentId": "ocid1.tenancy.oc1..aaaaaaaaip36ginmudtormvwsv45imsyctt3vhme6rnemweaxe4b6q2gcodq",
+        "name": "5f60514e3d78e8e14",
+        "users": "ocid1.user.oc1..aaaaaaaarekfkxmha6u5cyvqjkmvo7kdxa"
     },
     {
-        "id": "ocid1.credential.oc1..aaaaaaaa4f57ig72lh5gu6hicy5d5nl7wzb6q",
-        "compartmentId": "ocid1.tenancy.oc1..aaaaaaaaip3imsyctt3vhme6rnemweaxe4b6q2gcodq",
-        "description": "For CIS",
-        "timeCreated": "2021-01-09T14:53:08.201Z",
-        "userId": "ocid1.user.oc1..aaaaaaaaddzaosqrekfkxmha6u5cyvqjkmvo7kdxa",
+        "id": "5f60514e3d78e8e14",
+        "userId": "ocid1.user.oc1..aaaaaaaarekfkxmha6u5cyvqjkmvo7kdxa",
+        "displayName": "For CIS",
+        "timeCreated": "2021-01-09T17:42:32.413Z",
         "lifecycleState": "ACTIVE",
-        "users": "ocid1.user.oc1..aaaaaaaaddzaosqrekfkxmha6u5cyvqjkmvo7kdxa"
+        "compartmentId": "ocid1.tenancy.oc1..aaaaaaaaip36ginmudtormvwsv45imsyctt3vhme6rnemweaxe4b6q2gcodq",
+        "name": "5f60514e3d78e8e14",
+        "users": "ocid1.user.oc1..aaaaaaaarekfkxmha6u5cyvqjkmvo7kdxa"
     }
 ];
 
-const createCache = (authData, userErr, authErr) => {
+const createCache = (cskData, userErr, cskErr) => {
     return {
         regionSubscription: {
             "list": {
@@ -89,18 +91,18 @@ const createCache = (authData, userErr, authErr) => {
                 }
             }
         }, 
-        authToken: {
+        customerSecretKey: {
             list: {
                 'us-ashburn-1': {
-                    data: authData,
-                    err: authErr
+                    data: cskData,
+                    err: cskErr
                 }
             }
         }
     }
 };
 
-describe('userAuthTokenRotated', function () {
+describe('userCustomerSecretKeysRotated', function () {
     describe('run', function () {
         it('should give unknown result if unable to query for users', function (done) {
             const callback = (err, results) => {
@@ -119,43 +121,43 @@ describe('userAuthTokenRotated', function () {
             plugin.run(cache, {}, callback);
         })
 
-        it('should give failing result Auth token has not been rotated within set days limit', function (done) {
+        it('should give failing result customer secret key has not been rotated within set days limit', function (done) {
             const callback = (err, results) => {
                 expect(results.length).to.be.above(0)
                 expect(results[0].status).to.equal(2)
-                expect(results[0].message).to.include('Auth token is')
+                expect(results[0].message).to.include('Customer secret key is')
                 expect(results[0].region).to.equal('global')
                 done()
             };
 
             const cache = createCache(
-                [authTokens[1]]
+                [customerSecretKeys[1]]
             );
 
             plugin.run(cache, {}, callback);
         })
 
-        it('should give passing result Auth token has been rotated within set days limit', function (done) {
+        it('should give passing result customer secret key has been rotated within set days limit', function (done) {
             const callback = (err, results) => {
                 expect(results.length).to.be.above(0)
                 expect(results[0].status).to.equal(0)
-                expect(results[0].message).to.include('Auth token is 0 days old')
+                expect(results[0].message).to.include('Customer secret key is 0 days old')
                 expect(results[0].region).to.equal('global')
                 done()
             };
 
             const cache = createCache(
-                [authTokens[0]]
+                [customerSecretKeys[0]]
             );
 
             plugin.run(cache, {}, callback);
         })
 
-        it('should give passing result if No user auth tokens found', function (done) {
+        it('should give passing result if No user customer secret keys found', function (done) {
             const callback = (err, results) => {
                 expect(results.length).to.be.above(0)
                 expect(results[0].status).to.equal(0)
-                expect(results[0].message).to.include('No user auth tokens found')
+                expect(results[0].message).to.include('No user customer secret keys found')
                 expect(results[0].region).to.equal('global')
                 done()
             };
@@ -167,11 +169,11 @@ describe('userAuthTokenRotated', function () {
             plugin.run(cache, {}, callback);
         });
 
-        it('should give unknown result if unable to query user auth tokens', function (done) {
+        it('should give unknown result if unable to query user customer secret keys', function (done) {
             const callback = (err, results) => {
                 expect(results.length).to.be.above(0)
                 expect(results[0].status).to.equal(3)
-                expect(results[0].message).to.include('Unable to query user auth tokens')
+                expect(results[0].message).to.include('Unable to query user customer secret keys')
                 expect(results[0].region).to.equal('global')
                 done()
             };
