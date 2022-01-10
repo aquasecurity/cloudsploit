c5ddf162c6e77ce83b9a1a78c2b65ec4877655f0
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-01-10 16:12:02 +0500

    Update plugins/aws/dms/dmsMultiAZFeatureEnabled.js

--- plugins/aws/dms/dmsMultiAZFeatureEnabled.js
+++ plugins/aws/dms/dmsMultiAZFeatureEnabled.js
@@ -2,7 +2,7 @@ var async = require('async');
 var helpers = require('../../../helpers/aws');
 
 module.exports = {
-    title: 'DMS Auto Minor Version Upgrade',
+    title: 'DMS Multi-AZ Feature Enabled',
     category: 'DMS',
     domain: 'Application Integration',
     description: 'Ensure that your Amazon Database Migration Service (DMS) replication instances are using Multi-AZ deployment configurations.',
