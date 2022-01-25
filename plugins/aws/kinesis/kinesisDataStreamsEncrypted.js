5b1876f4beb8cfce568324295ee12efcc2ea6eec
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-01-25 14:43:44 +0500

    Update plugins/aws/kinesis/kinesisDataStreamsEncrypted.js
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- plugins/aws/kinesis/kinesisDataStreamsEncrypted.js
+++ plugins/aws/kinesis/kinesisDataStreamsEncrypted.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'Kinesis',
     domain: 'Content Delivery',
     description: 'Ensures Kinesis Streams are encrypted using AWS KMS key of desired encryption level.',
-    more_info: 'Data sent to Kinesis Streams can be encrypted using KMS server-side encryption. Existing streams can be modified to add encryption with minimal overhead.'+ 
+    more_info: 'Data sent to Kinesis Streams can be encrypted using KMS server-side encryption. Existing streams can be modified to add encryption with minimal overhead. '+ 
         'Use customer-managed keys instead in order to gain more granular control over encryption/decryption process.',
     recommended_action: 'Enable encryption using desired level for all Kinesis Streams.',
     link: 'https://docs.aws.amazon.com/streams/latest/dev/server-side-encryption.html',
