d1634365603b23963ac315997c82ceaed7198479
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-01-26 14:39:50 +0500

    Update plugins/aws/elasticache/elasticacheClusterInVpc.js
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- plugins/aws/elasticache/elasticacheClusterInVpc.js
+++ plugins/aws/elasticache/elasticacheClusterInVpc.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'ElastiCache Cluster In VPC',
     category: 'ElastiCache',
     domain: 'Databases',
-    description: 'Ensure that your ElastiCache clusters are provisioned within the AWS EC2-VPC platform instead of EC2-Classic platform .',
+    description: 'Ensure that your ElastiCache clusters are provisioned within the AWS VPC platform.',
     more_info: 'Amazon ElastiCache clusters using EC2-VPC platform instead of EC2-Classic can bring multiple advantages such as better networking infrastructure, much more flexible control over access security .',
     link: 'https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/VPCs.EC.html',
     recommended_action: 'ElastiCache clusters Available in VPC',
