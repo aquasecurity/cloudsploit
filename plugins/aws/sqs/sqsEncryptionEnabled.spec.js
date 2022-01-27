3632bd990017d41355dd9e8992f3a25f947d7979
Sadeed Rehman <sadeed1999@gmail.com>
2022-01-27 15:45:04 +0500

    Added some changes

--- plugins/aws/sqs/sqsEncryptionEnabled.spec.js
+++ plugins/aws/sqs/sqsEncryptionEnabled.spec.js
@@ -236,6 +236,7 @@ describe('sqsEncryptionEnabled', function () {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
                 expect(results[0].region).to.equal('us-east-1');
+                expect(results[0].message).to.include('Unable to list KMS keys');
                 done();
             });
         });
