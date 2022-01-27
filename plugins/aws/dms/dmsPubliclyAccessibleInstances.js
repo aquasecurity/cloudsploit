821f93d314117a1e58388b7a5713bfb4c054c0f2
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-01-27 15:37:05 +0500

    Update plugins/aws/dms/dmsPubliclyAccessibleInstances.js

--- plugins/aws/dms/dmsPubliclyAccessibleInstances.js
+++ plugins/aws/dms/dmsPubliclyAccessibleInstances.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Application Integration',
     description: 'Ensure that Amazon Database Migration Service (DMS) instances are not publicly accessible.',
     more_info: 'An AWS DMS replication instance can have one public IP address and one private IP address. If you uncheck (disable) the box for Publicly accessible, then the replication instance has only a private IP address. that prevents from exposure of data to other users',
-    recommended_action: 'Disable Publicly Accessible feature in order to have private IP address and avoid exposing of private data.',
+    recommended_action: 'Ensure that DMS replication instances have only private IP address and not public IP address',
     link: 'https://docs.aws.amazon.com/dms/latest/userguide/CHAP_ReplicationInstance.PublicPrivate.html',
     apis: ['DMS:describeReplicationInstances'],
 
