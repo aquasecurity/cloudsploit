669a7bd7fedd26e273c5c16d6fd3b19ae78add70
Muhammad Nouman Suleman <m.nouman.suleman@gmail.com>
2022-02-02 01:58:02 -0800

    SAAS/3801: Added specs for Azure logAlerts plugins (#1100)

---
+++ plugins/azure/logalerts/securityPolicyAlertsEnabled.spec.js
@@ -0,0 +1,132 @@
+var expect = require("chai").expect;
+var securityPolicyAlertsEnabled = require("./securityPolicyAlertsEnabled");
+
+const activityLogAlerts = [
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Security"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Security/policies/delete"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
+    },
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Security"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Security/policies/write"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
+    }
+];
+
+const createCache = (err, data) => {
+  return {
+    activityLogAlerts: {
+      listBySubscriptionId: {
+        global: {
+          err: err,
+          data: data,
+        },
+      },
+    },
+  };
+};
+
+describe("securityPolicyAlertsEnabled", function () {
+  describe("run", function () {
+    it("should give failing result if no activity log alerts found", function (done) {
+        const cache = createCache(null, []);
+        securityPolicyAlertsEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(1);
+            expect(results[0].status).to.equal(2);
+            expect(results[0].message).to.include("No existing Activity Alerts found");
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give unknown result if unable to query for Activity alerts", function (done) {
+        const cache = createCache(null);
+        securityPolicyAlertsEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(1);
+            expect(results[0].status).to.equal(3);
+            expect(results[0].message).to.include("Unable to query for Activity Alerts");
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing result if Security Policy write/update is not enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[0]]);
+        securityPolicyAlertsEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(1);
+            expect(results[0].status).to.equal(2);
+            expect(results[0].message).to.include(
+                "Log Alert for Security Policy write/update is not enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing result if Security Policy write/update is enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[1]]);
+        securityPolicyAlertsEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(1);
+            expect(results[0].status).to.equal(0);
+            expect(results[0].message).to.include(
+                "Log Alert for Security Policy write/update is enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+  });
+});
