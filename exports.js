75179f4813cd56bbd5b1b33784c9fdb69cf1484f
Ron Cococcia <15912615+cococr@users.noreply.github.com>
2022-02-02 04:40:02 -0500

    SAAS-3397: Added SageMaker Notebook Instances in VPC plugin (#1070)
    * SAAS-3397: Added SageMaker Notebook Instances in VPC plugin
    
    * Naming change for plugin export
    
    * Title adjustment for plugin
    
    * Add newline to end of files

--- exports.js
+++ exports.js
@@ -333,6 +333,7 @@ module.exports = {
 
         'notebookDataEncrypted'         : require(__dirname + '/plugins/aws/sagemaker/notebookDataEncrypted.js'),
         'notebookDirectInternetAccess'  : require(__dirname + '/plugins/aws/sagemaker/notebookDirectInternetAccess.js'),
+        'notebookInstanceInVpc'         : require(__dirname + '/plugins/aws/sagemaker/notebookInstanceInVpc.js'),
 
         'dkimEnabled'                   : require(__dirname + '/plugins/aws/ses/dkimEnabled.js'),
         'emailMessagesEncrypted'        : require(__dirname + '/plugins/aws/ses/emailMessagesEncrypted.js'),
