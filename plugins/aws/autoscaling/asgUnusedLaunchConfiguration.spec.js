3b646621cfb22e60f9fefe44f92db635e552a6a0
Sadeed Rehman <sadeed1999@gmail.com>
2022-02-03 17:09:44 +0500

    Added some changes

--- plugins/aws/autoscaling/asgUnusedLaunchConfiguration.spec.js
+++ plugins/aws/autoscaling/asgUnusedLaunchConfiguration.spec.js
@@ -181,7 +181,7 @@ describe('asgUnusedLaunchConfiguration', function () {
             asgUnusedLaunchConfiguration.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
-                expect(results[0].message).to.include('Unable to query for AutoScaling groups');
+                expect(results[0].message).to.include('Unable to query for Auto Scaling groups');
                 done();
             });
         });
