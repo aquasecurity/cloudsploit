43e8c32e0722b76e98adf237248d6ecabe86779f
Mehak Seedat <mehakseedat63@gmail.com>
2022-02-21 03:41:06 +0500

    GCP Remediations for Cloud Resource Manager Plugins

--- plugins/google/cloudresourcemanager/disableIdentityClusterCreation.js
+++ plugins/google/cloudresourcemanager/disableIdentityClusterCreation.js
@@ -9,10 +9,26 @@ module.exports = {
     link: 'https://cloud.google.com/resource-manager/docs/organization-policy/org-policy-constraints',
     recommended_action: 'Ensure that "Disable Workload Identity Cluster Creation" constraint is enforced at the organization level.',
     apis: ['organizations:list', 'organizations:listOrgPolicies'],
+    remediation_min_version: '202202200432',
+    remediation_description: 'The "Disable Workload Identity Cluster Creation" constraint will be enforced at the organization level.',
+    apis_remediate: ['organizations:list', 'organizations:listOrgPolicies'],
+    actions: {remediate:['SetOrgPolicy'], rollback:['SetOrgPolicy']},
+    permissions: {remediate: ['orgpolicy.policy.set'], rollback: ['orgpolicy.policy.set']},
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
+
+        let organizations = helpers.addSource(cache, source,
+            ['organizations','list', 'global']);
+
+        if (!organizations || organizations.err || !organizations.data || !organizations.data.length) {
+            helpers.addResult(results, 3,
+                'Unable to query for organizations: ' + helpers.addError(organizations), 'global', null, null, (organizations) ? organizations.err : null);
+            return callback(null, results, source);
+        }
+
+        var organization = organizations.data[0].name;
 
         let listOrgPolicies = helpers.addSource(cache, source,
             ['organizations', 'listOrgPolicies', 'global']);
@@ -30,8 +46,29 @@ module.exports = {
         }
         let orgPolicies = listOrgPolicies.data[0];
 
-        helpers.checkOrgPolicy(orgPolicies, 'iam.disableWorkloadIdentityClusterCreation', 'booleanPolicy', true, false,  'Disable Workload Identity Cluster Creation', results);
+        helpers.checkOrgPolicy(orgPolicies, 'iam.disableWorkloadIdentityClusterCreation', 'booleanPolicy', true, false,  'Disable Workload Identity Cluster Creation', results, organization);
 
         return callback(null, results, source);
+    },
+    remediate: function(config, cache, settings, resource, callback) {
+        var remediation_file = settings.remediation_file;
+
+        // inputs specific to the plugin
+        var pluginName = 'disableIdentityClusterCreation';
+ 
+        var putCall = this.actions.remediate;
+
+        helpers.remediateOrgPolicy(config, 'constraints/iam.disableWorkloadIdentityClusterCreation', 'booleanPolicy', true, resource, remediation_file, putCall, pluginName, function(err, action) {
+            if (err) return callback(err);
+            if (action) action.action = putCall;
+
+
+            remediation_file['post_remediate']['actions'][pluginName][resource] = action;
+            remediation_file['remediate']['actions'][pluginName][resource] = {
+                'Action': 'Enabled'
+            };
+
+            callback(null, action);
+        });
     }
 };
--- plugins/google/cloudresourcemanager/disableSerialPortAccess.spec.js
+++ plugins/google/cloudresourcemanager/disableSerialPortAccess.spec.js
@@ -17,7 +17,6 @@ const createCache = (err, data) => {
                     ],
                     err: null
                 }
-
             },
             listOrgPolicies: {
                 'global': {
