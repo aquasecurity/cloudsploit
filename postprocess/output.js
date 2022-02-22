f784aef7c711be1a631770582f0146491ea993b2
Pasan Chamikara <gwpc92@gmail.com>
2022-02-22 22:55:45 +0530

    changes

--- postprocess/output.js
+++ postprocess/output.js
@@ -33,9 +33,11 @@ function generateTimeStamp(){
 
 function uploadToBucket(fileName, bucketName){
     const creds = {
-        'type': process.env.type,
         'project_id': process.env.GOOGLE_PROJECT_ID,
-        'private_key_id': process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        'credentials': {
+            'client_email': process.env.GOOGLE_CLIENT_EMAIL,
+            'private_key': process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        }
     };
     
     const storage = new Storage({
