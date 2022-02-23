fd0caed5a212ddfec96b26ef5b1e19a73a002bad
Pasan Chamikara <gwpc92@gmail.com>
2022-02-23 23:44:08 +0530

    fix

--- config.js
+++ config.js
@@ -38,6 +38,9 @@ module.exports = {
             // subscription_id: process.env.AZURE_SUBSCRIPTION_ID || ''
         },
         google: {
+            // OPTION 1: If using a credential JSON file, enter the path below
+            // credential_file: process.env.GOOGLE_APPLICATION_CREDENTIALS || '/path/to/file.json',
+            // OPTION 2: If using hard-coded credentials, enter them below
             project: process.env.GOOGLE_PROJECT_ID || 'my-project',
             client_email: process.env.GOOGLE_CLIENT_EMAIL || 'cloudsploit@your-project-name.iam.gserviceaccount.com',
             private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n') || '-----BEGIN PRIVATE KEY-----\nYOUR-PRIVATE-KEY-GOES-HERE\n-----END PRIVATE KEY-----\n'
@@ -60,9 +63,7 @@ module.exports = {
             // url: process.env.GITHUB_URL || 'https://api.github.com',
             // login: process.env.GITHUB_LOGIN || 'myusername',
             // organization: process.env.GITHUB_ORG || false
-        },
-        alibaba: {
-
         }
     }
 };
+
