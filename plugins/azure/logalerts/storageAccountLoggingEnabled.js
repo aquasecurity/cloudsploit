c073194471699c75f49494e5abfed9d6f42a4dd0
Muhammad Nouman <m.nouman.suleman@gmail.com>
2022-03-04 04:26:22 -0800

    Added test cases for delete and write separatedly

--- plugins/azure/logalerts/storageAccountLoggingEnabled.js
+++ plugins/azure/logalerts/storageAccountLoggingEnabled.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Storage Account Logging Enabled',
     category: 'Log Alerts',
     domain: 'Management and Governance',
-    description: 'Ensures Activity Log alerts for the create or update and delete Storage Account events are enabled',
-    more_info: 'Monitoring for create or update and delete Storage Account events gives insight into network access changes and may reduce the time it takes to detect suspicious activity.',
-    recommended_action: 'Add a new log alert to the Alerts service that monitors for Storage Account create or update and delete events.',
+    description: 'Ensure that Activity Log alerts for the create/update and delete Storage Account events are enabled.',
+    more_info: 'Monitoring for create/update and delete Storage Account events gives insight into network access changes and may reduce the time it takes to detect suspicious activity.',
+    recommended_action: 'Add a new log alert to the Alerts service that monitors for Storage Account create/update and delete events.',
     link: 'https://docs.microsoft.com/en-us/azure/azure-monitor/platform/activity-log-alerts',
     apis: ['activityLogAlerts:listBySubscriptionId'],
 
@@ -17,11 +17,8 @@ module.exports = {
         var locations = helpers.locations(settings.govcloud);
 
         async.each(locations.activityLogAlerts, function(location, rcb) {
-
             var conditionResource = 'microsoft.storage/storageaccounts';
-
             var text = 'Storage Accounts';
-
             var activityLogAlerts = helpers.addSource(cache, source,
                 ['activityLogAlerts', 'listBySubscriptionId', location]);
 
--- plugins/azure/logalerts/storageAccountLoggingEnabled.spec.js
+++ plugins/azure/logalerts/storageAccountLoggingEnabled.spec.js
@@ -67,6 +67,68 @@ const activityLogAlerts = [
                 }
             ]
         }
+    },
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Admninistrative"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Storage/storageAccounts/write"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
+    },
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Admninistrative"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Storage/storageAccounts/delete"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
     }
 ];
 
@@ -114,6 +176,42 @@ describe("storageAccountLoggingEnabled", function () {
             expect(results[0].status).to.equal(2);
             expect(results[0].message).to.include(
                 "Log Alert for Storage Accounts write and delete is not enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing and passing results if Storage Accounts delete is not enaled but write is enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[2]]);
+        storageAccountLoggingEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(2);
+            expect(results[0].status).to.equal(0);
+            expect(results[0].message).to.include(
+                "Log alert for Storage Accounts write is enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            expect(results[1].status).to.equal(2);
+            expect(results[1].message).to.include(
+                "Log Alert for Storage Accounts delete is not enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing and passing results if Storage Accounts write is not enaled but delete is enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[3]]);
+        storageAccountLoggingEnabled.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(2);
+            expect(results[0].status).to.equal(0);
+            expect(results[0].message).to.include(
+                "Log alert for Storage Accounts delete is enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            expect(results[1].status).to.equal(2);
+            expect(results[1].message).to.include(
+                "Log alert for Storage Accounts write is not enabled"
             );
             expect(results[0].region).to.equal("global");
             done();
