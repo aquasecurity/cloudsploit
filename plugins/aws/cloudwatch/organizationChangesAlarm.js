921a3e9896a65906b0db41791a9ffe79446e11d1
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-03-04 17:48:01 +0500

    Update plugins/aws/cloudwatch/organizationChangesAlarm.js

--- plugins/aws/cloudwatch/organizationChangesAlarm.js
+++ plugins/aws/cloudwatch/organizationChangesAlarm.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Organization Changes Alarm',
     category: 'CloudWatch',
     domain: 'Compliance',
-    description: 'Ensure that there is an Amazon CloudWatch alarm implemented within your AWS Master account that is triggered each time an administrator-specific action occurs within your AWS Organizations.',
+    description: 'Ensure that there is an Amazon CloudWatch alarm implemented within your AWS account to detect administrator-specific actions occurred within your AWS Organization.',
     more_info: 'Using Amazon CloudWatch alarms to detect administrator-specific changes such as create organization, delete organization, create new accounts within an organization or remove a member account from an organization is considered best practice and can help you prevent any unwanted, accidental or intentional modifications that may lead to unauthorized access or other security breaches.',
     recommended_action: 'Create an CloudWatch alarm to detect any administrator changes within you AWS organization.',
     link: 'https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html',
