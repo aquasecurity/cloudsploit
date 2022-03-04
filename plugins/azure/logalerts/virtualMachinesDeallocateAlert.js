44c9cf554c010f9622a3413e7c768d5a40dd7d1c
Muhammad Nouman <m.nouman.suleman@gmail.com>
2022-03-04 04:41:34 -0800

    Fixed indentation

--- plugins/azure/logalerts/virtualMachinesDeallocateAlert.js
+++ plugins/azure/logalerts/virtualMachinesDeallocateAlert.js
@@ -19,7 +19,6 @@ module.exports = {
         async.each(locations.activityLogAlerts, function(location, rcb) {
             var conditionResource = 'microsoft.compute/virtualmachines';
             var text = 'Virtual Machines';
-
             var activityLogAlerts = helpers.addSource(cache, source,
                 ['activityLogAlerts', 'listBySubscriptionId', location]);
 
@@ -49,7 +48,6 @@ module.exports = {
                 const allConditions = activityLogAlertResource.condition;
 
                 if (!allConditions || !allConditions.allOf || !allConditions.allOf.length) continue;
-
 
                 var conditionOperation = allConditions.allOf.filter((d) => {
                     return (d.equals && d.equals.toLowerCase().indexOf(conditionResource) > -1);
