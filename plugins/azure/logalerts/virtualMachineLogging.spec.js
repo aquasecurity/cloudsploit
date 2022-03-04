b6b25b27daf86404ba99c8e061078a0cf42d9994
Muhammad Nouman <m.nouman.suleman@gmail.com>
2022-03-04 04:15:24 -0800

    Added test cases for delete and write separatedly

--- plugins/azure/logalerts/virtualMachineLogging.spec.js
+++ plugins/azure/logalerts/virtualMachineLogging.spec.js
@@ -67,6 +67,68 @@ const activityLogAlerts = [
                 }
             ]
         }
+    },
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Admninistrative"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Compute/virtualMachines/write"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
+    },
+    {
+        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourceGroups/Default-ActivityLogAlerts/providers/microsoft.insights/activityLogAlerts/NSG2",
+        "name": "NSG2",
+        "type": "Microsoft.Insights/ActivityLogAlerts",
+        "location": "global",
+        "tags": {},
+        "scopes": [
+            "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a"
+        ],
+        "enabled": true,
+        "condition": {
+            "allOf": [
+                {
+                    "field": "category",
+                    "equals": "Admninistrative"
+                },
+                {
+                    "field": "operationName",
+                    "equals": "Microsoft.Compute/virtualMachines/delete"
+                }
+            ]
+        },
+        "actions": {
+            "actionGroups": [
+                {
+                    "actionGroupId": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/resourcegroups/default-activitylogalerts/providers/microsoft.insights/actiongroups/testactiong",
+                    "webhookProperties": {}
+                }
+            ]
+        }
     }
 ];
 
@@ -114,6 +176,42 @@ describe("virtualMachineLogging", function () {
             expect(results[0].status).to.equal(2);
             expect(results[0].message).to.include(
                 "Log Alert for Virtual Machines write and delete is not enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing and passing results if Virtual Machines delete is not enaled but write is enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[2]]);
+        virtualMachineLogging.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(2);
+            expect(results[0].status).to.equal(0);
+            expect(results[0].message).to.include(
+                "Log alert for Virtual Machines write is enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            expect(results[1].status).to.equal(2);
+            expect(results[1].message).to.include(
+                "Log Alert for Virtual Machines delete is not enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            done();
+        });
+    });
+
+    it("should give failing and passing results if Virtual Machines write is not enaled but delete is enabled", function (done) {
+        const cache = createCache(null, [activityLogAlerts[3]]);
+        virtualMachineLogging.run(cache, {}, (err, results) => {
+            expect(results.length).to.equal(2);
+            expect(results[0].status).to.equal(0);
+            expect(results[0].message).to.include(
+                "Log alert for Virtual Machines delete is enabled"
+            );
+            expect(results[0].region).to.equal("global");
+            expect(results[1].status).to.equal(2);
+            expect(results[1].message).to.include(
+                "Log alert for Virtual Machines write is not enabled"
             );
             expect(results[0].region).to.equal("global");
             done();
