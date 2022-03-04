1f0ed9e6df608b246144d814363b83f764d1aa6b
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-03-04 20:59:13 +0500

    Update plugins/aws/appmesh/appmeshTLSRequired.js
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- plugins/aws/appmesh/appmeshTLSRequired.js
+++ plugins/aws/appmesh/appmeshTLSRequired.js
@@ -66,7 +66,7 @@ module.exports = {
                         describeVirtualGateway.err ||
                         !describeVirtualGateway.data || describeVirtualGateway.data.virtualGateway) {
                         helpers.addResult(results, 3,
-                            'Unable to App Mesh virtual gateways: ' + helpers.addError(describeVirtualGateway), region, gatewayArn);
+                            'Unable to describe App Mesh virtual gateway: ' + helpers.addError(describeVirtualGateway), region, gatewayArn);
                         continue;
                     }
 
