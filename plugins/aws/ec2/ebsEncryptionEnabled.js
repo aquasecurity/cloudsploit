01a376d5143914836695d0a0a7774cf0361a1a2f
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-03-08 20:58:32 +0500

    Added Some Changes To Settings Of Plugins

--- plugins/aws/ec2/ebsEncryptionEnabled.js
+++ plugins/aws/ec2/ebsEncryptionEnabled.js
@@ -40,7 +40,7 @@ module.exports = {
     more_info: 'EBS volumes should have at-rest encryption enabled through AWS using KMS. If the volume is used for a root volume, the instance must be launched from an AMI that has been encrypted as well.',
     link: 'http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/EBSEncryption.html',
     recommended_action: 'Enable encryption for EBS volumes.',
-    apis: ['EC2:describeVolumes', 'KMS:describeKey', 'KMS:listKeys'],
+    apis: ['EC2:describeVolumes', 'KMS:describeKey', 'KMS:listKeys',  'STS:getCallerIdentity'],
     compliance: {
         hipaa: 'HIPAA requires that all data is encrypted, including data at rest. ' +
                 'EBS is a HIPAA-compliant solution that provides automated encryption ' +
@@ -54,22 +54,20 @@ module.exports = {
             name: 'EBS Minimum Encryption Level',
             description: 'In order (lowest to highest) awskms=AWS-managed KMS; awscmk=Customer managed KMS; externalcmk=Customer managed externally sourced KMS; cloudhsm=Customer managed CloudHSM sourced KMS',
             regex: '^(awskms|awscmk|externalcmk|cloudhsm)$',
-            default: 'awskms',
+            default: 'awscmk',
         },
-        ebs_result_limit: {
-            name: 'EBS Result Limit',
-            description: 'If the number of results is greater than this value, combine them into one result',
-            regex: '^[0-9]*$',
-            default: '20',
-        },
+       
     },
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
+
+        var acctRegion = helpers.defaultRegion(settings);
+        var awsOrGov = helpers.defaultPartition(settings);
+        var accountId = helpers.addSource(cache, source, ['sts', 'getCallerIdentity', acctRegion, 'data']);
         var targetEncryptionLevel = encryptionLevelMap[settings.ebs_encryption_level || this.settings.ebs_encryption_level.default];
-        var ebsResultLimit = parseInt(settings.ebs_result_limit || this.settings.ebs_result_limit.default);
 
         async.each(regions.ec2, function(region, rcb) {
             var describeVolumes = helpers.addSource(cache, source, ['ec2', 'describeVolumes', region]);
@@ -84,52 +82,34 @@ module.exports = {
                 return rcb();
             }
 
-            var unencryptedVolumes = [];
-            var poorlyEncryptedVolumes = [];
-            var kmsErrors = [];
-
             for (let volume of describeVolumes.data) {
+                var resource = 'arn:' + awsOrGov + ':ec2:' + region + ':' + accountId + ':volume/' + volume.VolumeId;
                 if (!volume.Encrypted || !volume.KmsKeyId){
-                    unencryptedVolumes.push(volume.VolumeId);
+                    helpers.addResult(results, 2, 'EBS volume is unencrypted', region);
                     continue;
                 }
+
                 var kmsKeyId = volume.KmsKeyId.split('/')[1];
                 var describeKey = helpers.addSource(cache, source, ['kms', 'describeKey', region, kmsKeyId]);
                 if (!describeKey || describeKey.err || !describeKey.data) {
-                    kmsErrors.push({ kmsKeyId, err: helpers.addError(describeKey) });
+                    helpers.addResult(results, 3, 'Could not describe KMS key', region, volume.KmsKeyId);
                     continue;
                 }
+
                 var encryptionLevel = getEncryptionLevel(describeKey.data.KeyMetadata);
+
                 if (encryptionLevel < targetEncryptionLevel) {
-                    poorlyEncryptedVolumes.push(volume.VolumeId);
+                    helpers.addResult(results, 1,
+                        `EBS volume is not encrypted to ${encryptionLevelMap[targetEncryptionLevel]}`,
+                        region, resource);
+                } else {
+                    helpers.addResult(results, 0,
+                        `EBS volume is encrypted to ${encryptionLevelMap[targetEncryptionLevel]}`,
+                        region, resource);
                 }
             }
 
-            if (unencryptedVolumes.length) {
-                if (unencryptedVolumes.length > ebsResultLimit) {
-                    helpers.addResult(results, 2, `More than ${ebsResultLimit} EBS volumes are unencrypted`, region);
-                } else {
-                    unencryptedVolumes.forEach(volume => helpers.addResult(results, 2, 'EBS volume is unencrypted', region, volume));
-                }
-            }
-            if (poorlyEncryptedVolumes.length) {
-                if (poorlyEncryptedVolumes.length > ebsResultLimit) {
-                    helpers.addResult(results, 1, `More than ${ebsResultLimit} EBS volumes are not encrypted to ${encryptionLevelMap[targetEncryptionLevel]}`, region);
-                } else {
-                    poorlyEncryptedVolumes.forEach(volume => helpers.addResult(results, 1, `EBS volume is not encrypted to ${encryptionLevelMap[targetEncryptionLevel]}`, region, volume));
-                }
-            }
-            if (kmsErrors.length) {
-                if (kmsErrors.length > ebsResultLimit) {
-                    helpers.addResult(results, 3, `More than ${ebsResultLimit} errors describing kms keys happened`, region);
-                } else {
-                    kmsErrors.forEach(({err, kmsKeyId}) => helpers.addResult(results, 3, `Could not describe KMS key: ${err}`, region, kmsKeyId));
-                }
-            }
-
-            if (!unencryptedVolumes.length && !poorlyEncryptedVolumes.length && !kmsErrors.length) {
-                helpers.addResult(results, 0, `All volumes encrypted to at least ${encryptionLevelMap[targetEncryptionLevel]}`, region);
-            }
+          
             rcb();
         }, function(){
             callback(null, results, source);
--- plugins/aws/ec2/ebsEncryptionEnabled.spec.js
+++ plugins/aws/ec2/ebsEncryptionEnabled.spec.js
@@ -1,18 +1,71 @@
 var expect = require('chai').expect;
-var ebsEncryptionEnabled = require('./ebsEncryptionEnabled')
+const ebsEncryptionEnabled = require('./ebsEncryptionEnabled');
+
+describeVolumes = [
+    {
+        "Encrypted": false,
+        "VolumeId": "vol-0ebea24b6b5ab89d5",
+        "Iops": 100,
+        "VolumeType": "gp2",
+        "MultiAttachEnabled": false
+    },
+    {
+        
+        "Encrypted": true,
+        "KmsKeyId": "arn:aws:kms:us-east-1:000011112222:key/mykmskey",
+        "VolumeId": "vol-0c3475c8999065481",
+        "Iops": 300,
+        "VolumeType": "gp2",
+        "MultiAttachEnabled": false
+    }
+];
+
+const describeKey = [
+    {
+        "KeyMetadata": {
+            "Origin": "AWS_KMS",
+            "KeyManager": "AWS",    
+        }
+    },
+    {
+        "KeyMetadata": {   
+            "Origin": "AWS_KMS",
+            "KeyManager": "CUSTOMER",
+        }
+    }
+];
 
 const createCache = (volumes, keys) => {
     return {
         ec2: {
             describeVolumes: {
                 'us-east-1': {
-                    data: volumes,
+                    data: volumes
                 },
-            },
+            }
         },
         kms: {
             describeKey: {
-                'us-east-1': keys,
+                'us-east-1': {
+                    'mykmskey': {
+                    data: keys
+                    }
+                }
+            }
+        },
+    };
+};
+
+const createNullCache = () => {
+    return {
+        ec2: {
+            describeVolumes: {
+                'us-east-1': null,
+            }
+        },
+        kms: {
+            describeKey: {
+                'us-east-1': null,
             },
         },
     };
@@ -20,107 +73,49 @@ const createCache = (volumes, keys) => {
 
 describe('ebsEncryptionEnabled', function () {
     describe('run', function () {
-        it('should FAIL if volume is not encrypted', function (done) {
-            const cache = createCache([{
-                VolumeId: 'abc123',
-                Encrypted: false,
-            }], {
-                'mykmskey': {
-                    data: {
-                        KeyMetadata: {
-                            Origin: 'AWS_KMS',
-                            KeyManager: 'AWS',
-                        },
-                    },
-                },
+        it('should PASS if EBS volume is encrypted', function (done) {
+            const cache = createCache([describeVolumes[1]], describeKey[1]);
+            ebsEncryptionEnabled.run(cache, { ebs_encryption_level: 'awscmk' }, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                done();
             });
-            const settings = {
-                ebs_encryption_level: 'awscmk',
-            };
+        });
 
-            ebsEncryptionEnabled.run(cache, settings, (err, results) => {
+        it('should FAIL if EBS volume is unencrypted', function (done) {
+            const cache = createCache([describeVolumes[0]], describeKey[0]);
+            ebsEncryptionEnabled.run(cache, {  ebs_encryption_level: 'awscmk' }, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
-                done()
+                done();
             });
         });
 
-        it('should WARN if encryption level is less than configured setting', function (done) {
-            const cache = createCache([{
-                VolumeId: 'abc123',
-                Encrypted: true,
-                KmsKeyId: 'arn:aws:kms:us-east-1:123412341234:key/mykmskey',
-            }], {
-                'mykmskey': {
-                    data: {
-                        KeyMetadata: {
-                            Origin: 'AWS_KMS',
-                            KeyManager: 'AWS',
-                        },
-                    },
-                },
-            });
-            const settings = {
-                ebs_encryption_level: 'awscmk',
-            };
-
-            ebsEncryptionEnabled.run(cache, settings, (err, results) => {
-                expect(results.length).to.equal(1);
-                expect(results[0].status).to.equal(1, 'bad status');
-                done()
-            });
-        });
-
-        it('should PASS if encryption level meets configured target setting', function (done) {
-            const cache = createCache([{
-                VolumeId: 'abc123',
-                Encrypted: true,
-                KmsKeyId: 'arn:aws:kms:us-east-1:123412341234:key/mykmskey',
-            }], {
-                'mykmskey': {
-                    data: {
-                        KeyMetadata: {
-                            Origin: 'AWS_KMS',
-                            KeyManager: 'AWS',
-                        },
-                    },
-                },
-            });
-            const settings = {
-                ebs_encryption_level: 'awskms',
-            };
-
-            ebsEncryptionEnabled.run(cache, settings, (err, results) => {
+        it('should PASS if No EBS volumes present', function (done) {
+            const cache = createCache([],[]);
+            ebsEncryptionEnabled.run(cache, {  ebs_encryption_level: 'awscmk' }, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
                 done();
             });
         });
-
-        it('should UNKNWON if encryption level meets configured target setting', function (done) {
-            const cache = createCache([{
-                VolumeId: 'abc123',
-                Encrypted: true,
-                KmsKeyId: 'arn:aws:kms:us-east-1:123412341234:key/mykmskey',
-            }], {
-                'mykmskey': {
-                    data: {
-                        KeyMetadata: {
-                            Origin: 'AWS_KMS',
-                            KeyManager: 'AWS',
-                        },
-                    },
-                },
-            });
-            const settings = {
-                ebs_encryption_level: 'awskms',
-            };
-
-            ebsEncryptionEnabled.run(cache, settings, (err, results) => {
-                expect(results.length).to.equal(1);
-                expect(results[0].status).to.equal(0);
+      
+        it('should not return any results if unable to fetch EBS volumes', function (done) {
+            const cache = createNullCache();
+            ebsEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(0);
                 done();
             });
         });
+
+        it('should UNKNOWN if EBS volume is not encrypted to target encryption level', function (done) {
+            const cache = createCache([describeVolumes[1]], describeKey[0]);
+            ebsEncryptionEnabled.run(cache, {  ebs_encryption_level: 'awscmk' }, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(1);
+                done();
+            });
+        });
+
     });
 });
