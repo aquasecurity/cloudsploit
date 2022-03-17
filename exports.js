fb77280c48d2bb521e4bb27770ae2984d6f661b9
Muhammad Nouman <m.nouman.suleman@gmail.com>
2022-03-17 03:06:42 -0700

    Added azureActiveDirectoryEnabled plugin and spec

--- exports.js
+++ exports.js
@@ -701,6 +701,7 @@ module.exports = {
         'certificateExpiry'             : require(__dirname + '/plugins/azure/appservice/certificateExpiry.js'),
         'scmSiteAccessRestriction'      : require(__dirname + '/plugins/azure/appservice/scmSiteAccessRestriction.js'),
         'appServiceAccessRestriction'   : require(__dirname + '/plugins/azure/appservice/appServiceAccessRestriction.js'),
+        'azureActiveDirectoryEnabled'   : require(__dirname + '/plugins/azure/appservice/azureActiveDirectoryEnabled.js'),
 
         'rbacEnabled'                   : require(__dirname + '/plugins/azure/kubernetesservice/rbacEnabled.js'),
         'aksLatestVersion'              : require(__dirname + '/plugins/azure/kubernetesservice/aksLatestVersion.js'),
