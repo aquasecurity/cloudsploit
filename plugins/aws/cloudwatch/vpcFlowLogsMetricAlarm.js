917beae58ba30699825d628c30fdfe6203c40d1f
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-03-24 15:12:30 +0500

    Update plugins/aws/cloudwatch/vpcFlowLogsMetricAlarm.js

--- plugins/aws/cloudwatch/vpcFlowLogsMetricAlarm.js
+++ plugins/aws/cloudwatch/vpcFlowLogsMetricAlarm.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'VPC Flow Logs Metric Alarm',
     category: 'CloudWatch',
     domain: 'Compliance',
-    severity: 'Low',
+    severity: 'LOW',
     description: 'Ensure that CloudWatch group and alarm exist to detect any changes within VPC flow logs.',
     more_info: 'A metric alarm watches a single CloudWatch metric or the result of a math expression based on CloudWatch metrics. The alarm performs one or more actions based on the value of the metric or expression relative to a threshold over a number of time periods. The action can be sending a notification to an Amazon SNS topic.',
     recommended_action: '1. Create a log group in CloudWatch logs; 2. Create a metric filter for log group with Namespace=LogMetrics, metric Threshold=1 and check for logGroupName; 3.Create CloudWatch alarm for the metric filter',
