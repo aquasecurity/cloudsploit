3e3ecee180086cbf597d5d2b34b4d38344541c84
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-03-24 15:19:54 +0500

    Update plugins/aws/cloudformation/cloudformationInUse.js

--- plugins/aws/cloudformation/cloudformationInUse.js
+++ plugins/aws/cloudformation/cloudformationInUse.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'AWS CloudFormation In Use',
     category: 'CloudFormation',
     domain: 'Application Integration',
-    severity: 'Low',
+    severity: 'LOW',
     description: 'Ensure that Amazon CloudFormation is in use within your AWS account of selected region to automate your infrastructure management and deployment.',
     more_info: 'AWS CloudFormation is a service that helps you model and set up your AWS resources so that you can spend less time managing those resources and more time focusing on your applications that run in AWS. '+
         'A stack is a collection of AWS resources that you can manage as a single unit. In other words, you can create, update, or delete a collection of resources by creating, updating, or deleting stacks.',
