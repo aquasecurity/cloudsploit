2ec70069a113188da9c231c16d5bb1a44adf33f7
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-04-13 18:04:08 +0500

    Update helpers/alibaba/regions.js

--- helpers/alibaba/regions.js
+++ helpers/alibaba/regions.js
@@ -38,7 +38,7 @@ module.exports = {
     apigateway: [ 'cn-hangzhou', 'cn-shanghai', 'cn-qingdao', 'cn-beijing', 'cn-zhangjiakou', 'cn-huhehaote', 'ap-southeast-6',
         'cn-shenzhen', 'cn-heyuan', 'cn-chengdu', 'cn-hongkong', 'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3',
         'ap-southeast-5', 'ap-northeast-1', 'ap-south-1', 'eu-central-1', 'eu-west-1', 'us-west-1', 'us-east-1', 'me-east-1'
-]   ,
+],
     polardb: regions,
     ram: ['cn-hangzhou'],
     vpc: regions,
