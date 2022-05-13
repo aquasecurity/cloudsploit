208492f7e48976e32f420d2313e0f79cf5a9d4cc
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-05-13 18:18:09 +0500

    Apply suggestions from code review

--- plugins/azure/appservice/appInsightsEnabled.js
+++ plugins/azure/appservice/appInsightsEnabled.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Web Apps Insights Enabled',
     category: 'App Service',
     domain: 'Application Integration',
-    description: 'Ensures that Application Insights feature is enabled.',
+    description: 'Ensures that Application Insights feature is enabled for Azure web apps.',
     more_info: 'Application insights provide advanced application monitoring. Application Insights is an extensible Application Performance Management (APM) service for developers and DevOps professionals available as monitoring feature within Azure cloud.',
     recommended_action: 'Enable Application insights for Azure Web Apps',
     link: 'https://docs.microsoft.com/en-us/azure/azure-monitor/app/app-insights-overview',
