6af2c621b8ba6b9b4050f49236f1a2489dc1a169
Muhammad Nouman <m.nouman.suleman@gmail.com>
2022-05-15 03:16:35 +0500

    Added enableDefenderForStorage plugin and spec

--- exports.js
+++ exports.js
@@ -760,6 +760,8 @@ module.exports = {
         'advancedThreatProtection'      : require(__dirname + '/plugins/azure/cosmosdb/advancedThreatProtection.js'),
         'cosmosPublicAccessDisabled'    : require(__dirname + '/plugins/azure/cosmosdb/cosmosPublicAccessDisabled.js'),
         'automaticFailoverEnabled'      : require(__dirname + '/plugins/azure/cosmosdb/automaticFailoverEnabled.js'),
+
+        'enableDefenderForStorage'      : require(__dirname + '/plugins/azure/defender/enableDefenderForStorage.js'),
     },
     github: {
         'publicKeysRotated'             : require(__dirname + '/plugins/github/users/publicKeysRotated.js'),
