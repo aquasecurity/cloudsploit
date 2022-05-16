56a7a2231faf6ba5a78feb480307d7276d56d5f7
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-05-16 18:45:56 +0500

    Added Remediation for StackTerminationProtection Plugin

--- plugins/aws/cloudformation/stackTerminationProtection.js
+++ plugins/aws/cloudformation/stackTerminationProtection.js
@@ -10,6 +10,18 @@ module.exports = {
     link: 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-protect-stacks.html',
     recommended_action: 'Enable termination protection for CloudFormation stack',
     apis: ['CloudFormation:listStacks', 'CloudFormation:describeStacks'],
+    remediation_description: 'Stack Termination protection will be enabled.',
+    remediation_min_version: '202205161341',
+    apis_remediate: ['CloudFormation:listStacks', 'CloudFormation:describeStacks'],
+    actions: {
+        remediate: ['CloudFormation:updateTerminationProtection'],
+        rollback: ['CloudFormation:updateTerminationProtection']
+    },
+    permissions: {
+        remediate: ['cloudformation:UpdateTerminationProtection'],
+        rollback: ['cloudformation:UpdateTerminationProtection']
+    },
+    realtime_triggers: ['cloudformation:UpdateTerminationProtection'],
 
     run: function(cache, settings, callback) {
         var results = [];
@@ -66,6 +78,54 @@ module.exports = {
             });
         }, function(){
             callback(null, results, source);
+        });
+    },
+
+    remediate: function(config, cache, settings, resource, callback) {
+        var putCall = this.actions.remediate;
+        var pluginName = 'stackTerminationProtection';
+        var stackNameArr = resource.split(':');
+        var stacks = stackNameArr[stackNameArr.length - 1];
+        var stackName = stacks.split('/');
+        stackName = stackName[1];
+
+        var stackLocation = stackNameArr[3];
+
+        // add the location of the parameter group to the config
+        config.region = stackLocation;
+        var params = {};
+
+        // create the params necessary for the remediation
+        
+        params = { 
+            EnableTerminationProtection: true,
+            StackName: stackName  
+        };
+
+        var remediation_file = settings.remediation_file;
+        remediation_file['pre_remediate']['actions'][pluginName][resource] = {
+            'TerminationProtection': 'Disabled',
+            'StackName': stackName
+        };
+
+        // passes the config, put call, and params to the remediate helper function
+        helpers.remediatePlugin(config, putCall[0], params, function(err) {
+            if (err) {
+                remediation_file['remediate']['actions'][pluginName]['error'] = err;
+                return callback(err, null);
+            }
+
+            let action = params;
+            action.action = putCall;
+
+            remediation_file['post_remediate']['actions'][pluginName][resource] = action;
+            remediation_file['remediate']['actions'][pluginName][resource] = {
+                'TerminationProtection': 'Enabled',
+                'StackName': stackName
+            };
+            
+            settings.remediation_file = remediation_file;
+            return callback(null, action);
         });
     }
 };
