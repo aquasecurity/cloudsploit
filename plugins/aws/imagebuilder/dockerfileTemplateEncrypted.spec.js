9592aa7f9a264d9c8a100c213fd6fb22ed1d69c4
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-05-17 17:37:52 +0500

    Update plugins/aws/imagebuilder/dockerfileTemplateEncrypted.spec.js

--- plugins/aws/imagebuilder/dockerfileTemplateEncrypted.spec.js
+++ plugins/aws/imagebuilder/dockerfileTemplateEncrypted.spec.js
@@ -210,7 +210,7 @@ describe('dockerfileTemplateEncrypted', function () {
             dockerfileTemplateEncrypted.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('No container recipe list found');
+                expect(results[0].message).to.include('No container recipes found');
                 expect(results[0].region).to.equal('us-east-1');
                 done();
             });
