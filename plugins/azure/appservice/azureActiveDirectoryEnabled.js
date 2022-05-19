adfa1c854946e29861eda46b245cbda770055c4a
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-05-19 20:48:04 +0500

    Update plugins/azure/appservice/azureActiveDirectoryEnabled.js

--- plugins/azure/appservice/azureActiveDirectoryEnabled.js
+++ plugins/azure/appservice/azureActiveDirectoryEnabled.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Web Apps Azure Active Directory Enabled',
     category: 'App Service',
     domain: 'Application Integration',
-    description: 'Ensures that Azure Web Apps have Registration with Azure Active Directory is Enabled.',
+    description: 'Ensures that Azure Web Apps have registration with Azure Active Directory.',
     more_info: 'Registration with Azure Active Directory (AAD) enables App Service web applications to connect to other Azure cloud services securely without the need of access credentials such as user names and passwords.',
     recommended_action: 'Enable Registration with Azurre Active Directory for Azure Web Apps',
     link: 'https://docs.microsoft.com/en-us/azure/app-service/overview-managed-identity?tabs=portal%2Chttp#add-a-system-assigned-identity',
