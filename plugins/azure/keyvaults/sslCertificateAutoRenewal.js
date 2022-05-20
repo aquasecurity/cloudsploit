2a1dbf53b5554545c90dc6a9caac6f0d1dcfe8c3
AkhtarAmir <makhtar.pucit@gmail.com>
2022-05-20 14:48:02 +0500

    PR changes fix

--- plugins/azure/keyvaults/sslCertificateAutoRenewal.js
+++ plugins/azure/keyvaults/sslCertificateAutoRenewal.js
@@ -4,9 +4,9 @@ var helpers = require('../../../helpers/azure');
 module.exports = {
     title: 'SSL Certificate Auto Renewal',
     category: 'Key Vaults',
-    domain: 'Identity and Access Management',
-    description: 'Ensures that Microsoft Azure Key Vault SSL certificates auto renewal is enabled.',
-    more_info: 'Configure SSL certificates for auto renewal in order to prevent any application or service outage, credential leak, or process violation that can disrupt your business.',
+    domain: 'Application Integration',
+    description: 'Ensures that Microsoft Azure Key Vault SSL certificates have auto renewal enabled.',
+    more_info: 'Configure auto renewal for SSL certificates in order to prevent any application or service outage, credential leak, or process violation that can disrupt your business.',
     recommended_action: 'Ensure that Key Vault SSL certificates are configured to have auto renewal enabled.',
     link: 'https://docs.microsoft.com/en-us/azure/key-vault/certificates/overview-renew-certificate',
     apis: ['vaults:list', 'vaults:getCertificates', 'getCertificatePolicy:get'],
@@ -48,14 +48,11 @@ module.exports = {
                         if (!certificatePolicy || certificatePolicy.err || !certificatePolicy.data) {
                             helpers.addResult(results, 3, 'Unable to query for Certificate Policy: ' + helpers.addError(certificatePolicy), location, certificate.id);
                         } else {
-                            let autoRenew = false;
                             const lifetimeActions = certificatePolicy.data.lifetime_actions;
 
-                            lifetimeActions.forEach(({ action }) => {
-                                if (action.action_type.toLowerCase() === 'autorenew') {
-                                    autoRenew = true;
-                                }
-                            });
+                            let autoRenew = lifetimeActions && lifetimeActions.find(lifetimeAction =>
+                                lifetimeAction.action && lifetimeAction.action.action_type &&
+                                lifetimeAction.action.action_type.toLowerCase() == 'autorenew');
 
                             if (autoRenew) {
                                 helpers.addResult(results, 0, 'SSL Certificate auto renewal is enabled', location, certificate.id);
