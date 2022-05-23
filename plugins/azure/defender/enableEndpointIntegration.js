83e13c729592166a9324c32922ceccd74657c20f
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-05-23 17:30:52 +0500

    Update plugins/azure/defender/enableEndpointIntegration.js

--- plugins/azure/defender/enableEndpointIntegration.js
+++ plugins/azure/defender/enableEndpointIntegration.js
@@ -4,7 +4,7 @@ var helpers = require('../../../helpers/azure');
 module.exports = {
     title: 'Enable Defender Endpoint Integration',
     category: 'Defender',
-    domain: 'Security',
+    domain: 'Management and Governance',
     description: 'Ensures that Microsoft Defender for Endpoint integration is enabled.',
     more_info: 'WDATP integration brings comprehensive Endpoint Detection and Response (EDR) capabilities within Microsoft Defender for Cloud. This integration helps to spot abnormalities, detect and respond to advanced attacks on Windows server endpoints monitored by Microsoft Defender for Cloud. Windows Defender ATP in Microsoft Defender for Cloud supports detection on Windows Server 2016, 2012 R2, and 2008 R2 SP1 operating systems in a Standard service subscription.',
     recommended_action: 'Enable "Allow Microsoft Defender for Endpoint to access my data setting" in Defender environment settings.',
