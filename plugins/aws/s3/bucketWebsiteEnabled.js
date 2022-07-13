731f85198dc759a4f686811f5091dd82975b3ba1
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-07-13 22:55:01 +0500

    Added Setting for whitelisted empty buckets for S3 Bucket Website enabled

--- plugins/aws/s3/bucketWebsiteEnabled.js
+++ plugins/aws/s3/bucketWebsiteEnabled.js
@@ -8,9 +8,21 @@ module.exports = {
     more_info: 'S3 buckets should not be configured with static website hosting with public objects. Instead, a CloudFront distribution should be configured with an origin access identity.',
     recommended_action: 'Disable S3 bucket static website hosting in favor or CloudFront distributions.',
     link: 'https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-https-requests-s3/',
-    apis: ['S3:listBuckets', 'S3:getBucketWebsite', 'S3:getBucketLocation'],
+    apis: ['S3:listBuckets', 'S3:listObjects', 'S3:getBucketWebsite', 'S3:getBucketLocation'],
+    settings: {
+        s3_whitelist_empty_buckets: {
+            name: 'S3 Whitelist Empty Buckets Allow Pattern',
+            description: 'When set to true, whitelist empty buckets matching the given pattern. Useful for overriding buckets outside the account control.',
+               regex: '^(true|false)$',
+            default: 'false'
+        }
+    },
 
     run: function(cache, settings, callback) {
+        var config = {
+            s3_whitelist_empty_buckets: settings.s3_whitelist_empty_buckets || this.settings.s3_whitelist_empty_buckets.default,
+        };
+
         var results = [];
         var source = {};
 
@@ -32,9 +44,29 @@ module.exports = {
             return callback(null, results, source);
         }
 
+
         listBuckets.data.forEach(function(bucket){
             var bucketLocation = helpers.getS3BucketLocation(cache, region, bucket.Name);
 
+            var bucketResource = 'arn:aws:s3:::' + bucket.Name;
+
+            if (!config.s3_whitelist_empty_buckets){
+                var listObjects = helpers.addSource(cache, source,
+                    ['s3', 'listObjects', region, bucket.Name]);
+
+                if (listObjects.err || !listObjects.data) {
+                    helpers.addResult(results, 3,
+                        'Unable to query for S3 buckets object: ' + helpers.addError(listObjects), bucketResource);
+                    return;
+                }
+                
+                if (!listObjects.data.Contents || !listObjects.data.Contents.length){
+                    helpers.addResult(results, 2,
+                        'S3 Buckets are empty buckets', bucketLocation, bucketResource);
+                    return;
+                }
+            }
+                
             var getBucketWebsite = helpers.addSource(cache, source,
                 ['s3', 'getBucketWebsite', region, bucket.Name]);
 
@@ -58,6 +90,8 @@ module.exports = {
                     bucketLocation, 'arn:aws:s3:::' + bucket.Name);
             }
         });
+
+        
         callback(null, results, source);
     }
 };
