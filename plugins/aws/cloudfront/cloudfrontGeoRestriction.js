baf8e8029926dffa8c003ef4865d7d504bbfbdf4
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-07-28 18:34:23 +0500

    Added Settings for CloudFront Geo Restrictions WhiteListed Regions

--- plugins/aws/cloudfront/cloudfrontGeoRestriction.js
+++ plugins/aws/cloudfront/cloudfrontGeoRestriction.js
@@ -10,13 +10,28 @@ module.exports = {
     link: 'https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/georestrictions.html',
     recommended_action: 'Enable CloudFront geo restriction to whitelist or block location-based access.',
     apis: ['CloudFront:listDistributions'],
+    settings: {
+        cloudfront_whitelisted_regions: {
+            name: 'CloudFront WhiteListed Regions',
+            description: 'A comma-delimited list of CloudFront regions which should be allowed, for example AR=Argentine, EG=Egypt etc ',
+            regex: '[a-zA-Z0-9-,]',
+            default: 'AF,EG'
+        }
+    },
 
     run: function(cache, settings, callback) {
-
         var results = [];
         var source = {};
         var region = helpers.defaultRegion(settings);
 
+        var config = {
+            cloudfront_whitelisted_regions: settings.cloudfront_whitelisted_regions || this.settings.cloudfront_whitelisted_regions.default, 
+        };
+
+        if (!config.cloudfront_whitelisted_regions.length) return callback();
+        config.cloudfront_whitelisted_regions = config.cloudfront_whitelisted_regions.split(',');
+        config.cloudfront_whitelisted_regions = JSON.stringify(config.cloudfront_whitelisted_regions);
+        
         var listDistributions = helpers.addSource(cache, source,
             ['cloudfront', 'listDistributions', region]);
 
@@ -32,9 +47,35 @@ module.exports = {
             helpers.addResult(results, 0, 'No CloudFront distributions found', 'global');
             return callback(null, results, source);
         }
+   
+        listDistributions.data.forEach(distribution => {     
+            if (distribution.Restrictions && distribution.Restrictions.GeoRestriction 
+                && distribution.Restrictions.GeoRestriction.RestrictionType 
+                && distribution.Restrictions.GeoRestriction.RestrictionType.toLowerCase() === 'whitelist'){
+                    let item = distribution.Restrictions.GeoRestriction.Items;
+                    let whitelistItems = item.filter(element => config.cloudfront_whitelisted_regions.includes(element));
+                if (whitelistItems.length) {
+                    helpers.addResult(results, 0,
+                        `CloudFront distribution has ${whitelistItems} available for its content distribution.`, 'global', distribution.ARN);
+                } else {
+                    helpers.addResult(results, 2,
+                        'CloudFront distribution does not have region available for its content distribution', 'global', distribution.ARN);
+                }
+            
+            } else if (distribution.Restrictions && distribution.Restrictions.GeoRestriction 
+                && distribution.Restrictions.GeoRestriction.RestrictionType 
+                && distribution.Restrictions.GeoRestriction.RestrictionType.toLowerCase() === 'blacklist'){
+                    let items = distribution.Restrictions.GeoRestriction.Items
+                    let blacklistItems = items.filter(element => config.cloudfront_whitelisted_regions.includes(element));
+                    if (blacklistItems.length) {
+                        helpers.addResult(results, 2,
+                            `CloudFront distribution has ${blacklistItems} region blacklisted for its content distribution.`, 'global', distribution.ARN);
+                    } else {
+                        helpers.addResult(results, 0,
+                            'CloudFront distribution does not have Blacklisted regions for its content distribution', 'global', distribution.ARN);
+                    }    
+                }
 
-        // loop through Instances for every reservation
-        listDistributions.data.forEach(distribution => {
             if (distribution.Restrictions && distribution.Restrictions.GeoRestriction 
                 && distribution.Restrictions.GeoRestriction.RestrictionType 
                 && distribution.Restrictions.GeoRestriction.RestrictionType.toLowerCase() != 'none') {
@@ -44,6 +85,7 @@ module.exports = {
                 helpers.addResult(results, 2,
                     'Geo restriction feature is not enabled within CloudFront distribution.', 'global', distribution.ARN);
             }
+            
         });
 
         return callback(null, results, source);
