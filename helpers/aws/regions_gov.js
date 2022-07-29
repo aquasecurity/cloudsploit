162c64525259e995cfc981346bd1d5ac3c249a84
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-07-29 17:16:42 +0500

    Apply suggestions from code review

--- helpers/aws/regions_gov.js
+++ helpers/aws/regions_gov.js
@@ -116,7 +116,7 @@ module.exports = {
     voiceid: regions,
     appmesh: regions,
     frauddetector: regions,
-    imagebuilder: regions
+    imagebuilder: regions,
     computeoptimizer: regions,
     appconfig: regions
 
