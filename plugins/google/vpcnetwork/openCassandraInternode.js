70bee507f0072ba309313abf16e8da3b1b91210b
mehakseedat63 <mehakseedat63@gmail.com>
2022-09-02 06:56:32 +0500

    GCP - Open Cassnadra Internode Port Plugin

---
+++ plugins/google/vpcnetwork/openCassandraInternode.js
@@ -0,0 +1,49 @@
+var async = require('async');
+var helpers = require('../../../helpers/google');
+
+module.exports = {
+    title: 'Open Cassandra Internode',
+    category: 'VPC Network',
+    domain: 'Network Access Control',
+    description: 'Determines if TCP port 7000 for Cassandra Internode is open to the public',
+    more_info: 'While some ports such as HTTP and HTTPS are required to be open to the public to function properly, more sensitive services such as Cassandra Internode should be restricted to known IP addresses.',
+    link: 'https://cloud.google.com/vpc/docs/using-firewalls',
+    recommended_action: 'Restrict TCP port 7000 to known IP addresses.',
+    apis: ['firewalls:list'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var regions = helpers.regions();
+
+        async.each(regions.firewalls, function(region, rcb){
+            let firewalls = helpers.addSource(
+                cache, source, ['firewalls', 'list', region]);
+
+            if (!firewalls) return rcb();
+
+            if (firewalls.err || !firewalls.data) {
+                helpers.addResult(results, 3, 'Unable to query firewall rules', region, null, null, firewalls.err);
+                return rcb();
+            }
+
+            if (!firewalls.data.length) {
+                helpers.addResult(results, 0, 'No firewall rules found', region);
+                return rcb();
+            }
+
+            let ports = {
+                'tcp': [7000]
+            };
+
+            let service = 'Cassandra Internode';
+
+            helpers.findOpenPorts(firewalls.data, ports, service, region, results, cache, source);
+
+            rcb();
+        }, function(){
+            // Global checking goes here
+            callback(null, results, source);
+        });
+    }
+};
