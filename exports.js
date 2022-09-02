e3f23d853885a63c2ee9086dc6d036cda5a2d11e
Sadeed Rehman <45767890+sadeed12345@users.noreply.github.com>
2022-09-02 19:42:56 +0500

    Added linting issues

--- exports.js
+++ exports.js
@@ -154,6 +154,7 @@ module.exports = {
         'openCassandraMonitoring'       : require(__dirname + '/plugins/aws/ec2/openCassandraMonitoring.js'),
         'openCassandraThrift'           : require(__dirname + '/plugins/aws/ec2/openCassandraThrift.js'),
         'openLDAP'                      : require(__dirname + '/plugins/aws/ec2/openLDAP.js'),
+        'openLDAPSSL'                   : require(__dirname + '/plugins/aws/ec2/openLDAPSSL.js'),
         'openMemcached'                 : require(__dirname + '/plugins/aws/ec2/openMemcached.js'),
         'openRedis'                     : require(__dirname + '/plugins/aws/ec2/openRedis.js'),
         'vpcElasticIpLimit'             : require(__dirname + '/plugins/aws/ec2/vpcElasticIpLimit.js'),
