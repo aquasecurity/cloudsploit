604ce131fd5843dc909def714b8fa42e86bb3233
mehakseedat63 <mehakseedat63@gmail.com>
2022-09-02 20:04:02 +0500

    GCP - Modified some open ports plugins

--- plugins/google/vpcnetwork/openMongo.js
+++ plugins/google/vpcnetwork/openMongo.js
@@ -5,10 +5,10 @@ module.exports = {
     title: 'Open MongoDB',
     category: 'VPC Network',
     domain: 'Network Access Control',
-    description: 'Determines if TCP port 27017 for MongoDB is open to the public',
+    description: 'Determines if TCP port 27017, 27018 or 27019 for MongoDB is open to the public',
     more_info: 'While some ports such as HTTP and HTTPS are required to be open to the public to function properly, more sensitive services such as Mongo should be restricted to known IP addresses.',
     link: 'https://cloud.google.com/vpc/docs/using-firewalls',
-    recommended_action: 'Restrict TCP ports 27017 to known IP addresses.',
+    recommended_action: 'Restrict TCP ports 27017, 27018 and 27019 to known IP addresses.',
     apis: ['firewalls:list'],
 
     run: function(cache, settings, callback) {
@@ -33,7 +33,7 @@ module.exports = {
             }
 
             let ports = {
-                'tcp': [27017]
+                'tcp': [27017,27018,27019]
             };
 
             let service = 'Mongo';
