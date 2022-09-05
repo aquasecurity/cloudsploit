0ce2dd7209e2d9ca3eec6a62e62d8affb557a888
AkhtarAmir <makhtar.pucit@gmail.com>
2022-09-05 14:33:14 +0500

    lint fix

--- plugins/aws/ec2/vpnGatewayInVpc.js
+++ plugins/aws/ec2/vpnGatewayInVpc.js
@@ -48,16 +48,16 @@ module.exports = {
                     
                     if (attached) {
                         helpers.addResult(results, 0,
-                            `Virtual Private Gateway is associated with VPC`,
+                            'Virtual Private Gateway is associated with VPC',
                             region, resource);
                     } else {
                         helpers.addResult(results, 2,
-                            `Virtual Private Gateway is not associated with VPC`,
+                            'Virtual Private Gateway is not associated with VPC',
                             region, resource);
                     }
                 } else {
                     helpers.addResult(results, 2,
-                        `Virtual Private Gateway is not associated with VPC`,
+                        'Virtual Private Gateway is not associated with VPC',
                         region, resource);
                 }
 
