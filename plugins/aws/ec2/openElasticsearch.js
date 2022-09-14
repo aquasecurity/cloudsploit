afbe1558ee83b8c359c4480adcb6242b2d9cc2da
AkhtarAmir <makhtar.pucit@gmail.com>
2022-09-14 18:33:22 +0500

    updated remediation

--- plugins/aws/ec2/openElasticsearch.js
+++ plugins/aws/ec2/openElasticsearch.js
@@ -92,16 +92,28 @@ module.exports = {
         var pluginName = 'openElasticsearch';
         var protocol = 'tcp';
         var ports = [9200,9300];
+        var actions = [];
+        var errors = [];
 
-        helpers.remediateOpenPorts(putCall, pluginName, protocol, ports, config, cache, settings, resource, remediation_file, function(error, action) {
-            if (error && (error.length || Object.keys(error).length)) {
-                remediation_file['post_remediate']['actions'][pluginName]['error'] = error;
+        async.each(ports,function(port, cb) {
+            helpers.remediateOpenPorts(putCall, pluginName, protocol, port, config, cache, settings, resource, remediation_file, function(error, action) {
+                if (error && (error.length || Object.keys(error).length)) {
+                    errors.push(error);
+                } else if (action && (action.length || Object.keys(action).length)){
+                    actions.push(action);
+                }
+
+                cb();
+            });
+        }, function() {
+            if (errors && errors.length) {
+                remediation_file['post_remediate']['actions'][pluginName]['error'] = errors.join(', ');
                 settings.remediation_file = remediation_file;
-                return callback(error, null);
+                return callback(errors, null);
             } else {
-                remediation_file['post_remediate']['actions'][pluginName][resource] = action;
+                remediation_file['post_remediate']['actions'][pluginName][resource] = actions;
                 settings.remediation_file = remediation_file;
-                return callback(null, action);
+                return callback(null, actions);
             }
         });
     }
