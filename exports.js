c548e5ffe1add73b3b73600198267124376f4746
abdullahaslam306 <f178135@nu.edu.pk>
2022-09-28 13:05:50 +0500

    olugin VPC has tags

--- exports.js
+++ exports.js
@@ -573,6 +573,7 @@ module.exports = {
         'wafInUse'                      : require(__dirname + '/plugins/aws/waf/wafInUse.js'),
 
         'fraudDetectorDataEncrypted'    : require(__dirname + '/plugins/aws/frauddetector/fraudDetectorDataEncrypted.js'),
+        'vpcHasTags'                    : require(__dirname + '/plugins/aws/ec2/vpcHasTags')
     },
     azure : {
         'fileServiceEncryption'         : require(__dirname + '/plugins/azure/storageaccounts/fileServiceEncryption.js'),
