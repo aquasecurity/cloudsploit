4be27a7492eac0b47a09d1e23cac816b435b74ee
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-10-03 14:41:47 +0500

    Apply suggestions from code review

--- plugins/aws/ec2/ec2HasTags.js
+++ plugins/aws/ec2/ec2HasTags.js
@@ -5,10 +5,10 @@ module.exports = {
     title: 'EC2 has Tags',
     category: 'EC2',
     domain: 'Compute',
-    description: 'Ensure that AWS EC2 Instance have tags associated.',
+    description: 'Ensure that AWS EC2 Instances have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     link: 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Using_Tags.html',
-    recommended_action: 'Add tags for ec2 instance',
+    recommended_action: 'Modify EC2 instances and add tags.',
     apis: ['EC2:describeInstances'],
 
     run: function(cache, settings, callback) {
@@ -25,20 +25,22 @@ module.exports = {
                 helpers.addResult(results, 3, `Unable to query for instances: ${helpers.addError(describeInstances)}`, region);
                 return rcb();
             }
+
             if (!describeInstances.data.length) {
                 helpers.addResult(results, 0, 'No EC2 instances found', region);
                 return rcb();
             }
+
             for (var instances of describeInstances.data){
                 const { OwnerId } = instances;
-                for (var instance of instances.Instances){
-                
+
+                for (var instance of instances.Instances) {
                     const { Tags, InstanceId } = instance;
-                    const ARN = `arn:aws:ec2:${region}:${OwnerId}:instance/${InstanceId}`;
-                    if (Tags.length === 0){
-                        helpers.addResult(results, 2, 'EC2 Instance has no tags', region, ARN);
+                    const arn = `arn:aws:ec2:${region}:${OwnerId}:instance/${InstanceId}`;
+                    if (!Tags || !Tags.length){
+                        helpers.addResult(results, 2, 'EC2 Instance has no tags', region, arn);
                     } else {
-                        helpers.addResult(results, 0, 'EC2 Instance has tags specified', region, ARN);
+                        helpers.addResult(results, 0, 'EC2 Instance has tags specified', region, arn);
                     }
                 }
             }
