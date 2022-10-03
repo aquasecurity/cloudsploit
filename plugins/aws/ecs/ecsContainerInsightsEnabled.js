3f85244ef9f1bc7d8085012dd61a210c790fe7c5
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2022-10-03 15:59:06 +0500

    Update plugins/aws/ecs/ecsContainerInsightsEnabled.js

--- plugins/aws/ecs/ecsContainerInsightsEnabled.js
+++ plugins/aws/ecs/ecsContainerInsightsEnabled.js
@@ -12,7 +12,6 @@ module.exports = {
     apis: ['ECS:listClusters', 'ECS:describeCluster'],
 
     run: function(cache, settings, callback){
-
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
