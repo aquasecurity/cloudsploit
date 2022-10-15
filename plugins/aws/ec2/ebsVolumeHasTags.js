c463891c697874089771329339b0e81d655a9876
abdullahaslam306 <f178135@nu.edu.pk>
2022-10-15 19:54:23 +0500

    plugin dev ebs volume has tags

---
+++ plugins/aws/ec2/ebsVolumeHasTags.js
@@ -0,0 +1,54 @@
+var async = require('async');
+var helpers = require('../../../helpers/aws');
+
+module.exports = {
+    title: 'EBS Volumes have tags',
+    category: 'EC2',
+    domain: 'Compute',
+    description: 'Ensure that EBS volumes have tags',
+    more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
+    recommended_action: 'Modify EBS volumes and add tags',
+    link: 'https://aws.amazon.com/blogs/aws/new-tag-ec2-instances-ebs-volumes-on-creation/',
+    apis: ['EC2:describeVolumes', 'STS:getCallerIdentity'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var regions = helpers.regions(settings);
+
+        var acctRegion = helpers.defaultRegion(settings);
+        var awsOrGov = helpers.defaultPartition(settings);
+        var accountId = helpers.addSource(cache, source, ['sts', 'getCallerIdentity', acctRegion, 'data']);
+
+        async.each(regions.ec2, function(region, rcb) {
+            var describeVolumes = helpers.addSource(cache, source,
+                ['ec2', 'describeVolumes', region]);
+
+            if (!describeVolumes) return rcb();
+
+            if (describeVolumes.err || !describeVolumes.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for EBS Volumes: ' + helpers.addError(describeVolumes), region);
+                return rcb();
+            }
+
+            if (!describeVolumes.data.length) {
+                helpers.addResult(results, 0, 'No EBS Volumes found', region);
+                return rcb();
+            }
+            for (let volume of describeVolumes.data) {
+                var volumeArn = 'arn:' + awsOrGov + ':ec2:' + region + ':' + accountId + ':volume/' + volume.VolumeId;
+
+                if (!volume.Tags || !volume.Tags.length) {
+                    helpers.addResult(results, 2, 'EBS volume have no tags associated.', region, volumeArn);
+                } else {
+                    helpers.addResult(results, 0, 'EBS volume have tags.', region, volumeArn);
+                }
+            }
+
+            rcb();
+        }, function(){
+            callback(null, results, source);
+        });
+    }
+};
