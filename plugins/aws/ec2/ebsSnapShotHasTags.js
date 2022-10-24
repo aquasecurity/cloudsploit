df1d0081392417f2c8acb6158de9c0d5c3b2e3b0
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-10-24 13:43:54 +0500

    Apply suggestions from code review

--- plugins/aws/ec2/ebsSnapShotHasTags.js
+++ plugins/aws/ec2/ebsSnapShotHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'EBS Snapshot Has Tags',
     category: 'EC2',
     domain: 'Compute',
-    description: 'Ensure that EBS snapshots have tags',
+    description: 'Ensure that EBS snapshots have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     link: 'https://aws.amazon.com/blogs/compute/tag-amazon-ebs-snapshots-on-creation-and-implement-stronger-security-policies/',
     recommended_action: 'Modify EBS snapshots and add tags.',
@@ -30,16 +30,16 @@ module.exports = {
             }
 
             if (!describeSnapshots.data.length) {
-                helpers.addResult(results, 0, 'No EBS snapshots present', region);
+                helpers.addResult(results, 0, 'No EBS snapshots found', region);
                 return rcb();
             }
             for (let snapshot of describeSnapshots.data){
                 var resourceARN = `arn:${awsOrGov}:${region}:${snapshot.OwnerId}:snapshot/${snapshot.SnapshotId}`;
 
                 if (!snapshot.Tags || !snapshot.Tags.length) {
-                    helpers.addResult(results, 2, 'EBS Snapshot has no Tags associated.', region, resourceARN);
+                    helpers.addResult(results, 2, 'EBS Snapshot does not have tags', region, resourceARN);
                 } else {
-                    helpers.addResult(results, 0, 'EBS Snapshot have tags.', region, resourceARN);
+                    helpers.addResult(results, 0, 'EBS Snapshot has tags', region, resourceARN);
                 }
             }
             rcb();
