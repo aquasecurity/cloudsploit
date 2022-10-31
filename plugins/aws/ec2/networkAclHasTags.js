ad069348fb9e188b311d5c790a1685ebc2eaaaf4
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-10-31 13:11:17 +0500

    Update plugins/aws/ec2/networkAclHasTags.js

--- plugins/aws/ec2/networkAclHasTags.js
+++ plugins/aws/ec2/networkAclHasTags.js
@@ -37,7 +37,7 @@ module.exports = {
                 return rcb();
             }
             for (let nAcl of describeNetworkAcls.data) {
-
+            if (!nAcl.NetworkAclId) continue;
                 var resourceARN = `arn:${awsOrGov}:ec2:${region}:${accountId}:network-acl/${nAcl.NetworkAclId}`;
 
                 if (!nAcl.Tags || !nAcl.Tags.length) {
