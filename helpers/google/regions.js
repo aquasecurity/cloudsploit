4ea7708a264176d4b20cf66892f851b10e516ce1
mehakseedat63 <mehakseedat63@gmail.com>
2022-10-31 19:44:20 +0500

    Added New GCP regions

--- helpers/google/regions.js
+++ helpers/google/regions.js
@@ -7,6 +7,8 @@ var regions = [
     'us-west2',                     // Los Angeles
     'us-west3',                     // Salt Lake City
     'us-west4',                     // Las Vegas
+    'us-east5',                     // Columbus
+    'us-south1',                    // Dallas
     'us-central1',                  // Iowa
     'northamerica-northeast1',      // Montreal
     'northamerica-northeast2',      // Toronto
@@ -19,6 +21,9 @@ var regions = [
     'europe-west6',                 // Zurich
     'europe-north1',                // Finland
     'europe-central2',              // Warsaw
+    'europe-west8',                 // Milan
+    'europe-southwest1',            // Madrid
+    'europe-west9',                 // Paris
     'asia-south1',                  // Mumbai
     'asia-south2',                  // Delhi
     'asia-southeast1',              // Singapore
@@ -30,12 +35,15 @@ var regions = [
     'asia-northeast3',              // Seoul
     'australia-southeast1',         // Sydney
     'australia-southeast2',         // Melbourne
+    'me-west1',                     // Tel Aviv
 ];
 
 
 var zones = {
     'us-east1'                      : ['us-east1-b', 'us-east1-c', 'us-east1-d'],
     'us-east4'                      : ['us-east4-a', 'us-east4-b', 'us-east4-c'],
+    'us-east5'                      : ['us-east5-a', 'us-east5-b', 'us-east5-c'],
+    'us-south1'                      : ['us-south1-a', 'us-south1-b', 'us-south1-c'],
     'us-west1'                      : ['us-west1-a', 'us-west1-b', 'us-west1-c'],
     'us-west2'                      : ['us-west2-a', 'us-west2-b', 'us-west2-c'],
     'us-west3'                      : ['us-west3-a', 'us-west3-b', 'us-west3-c'],
@@ -53,6 +61,9 @@ var zones = {
     'europe-west6'                  : ['europe-west6-a', 'europe-west6-b', 'europe-west6-c'],
     'europe-north1'                 : ['europe-north1-a', 'europe-north1-b', 'europe-north1-c'],
     'europe-central2'               : ['europe-central2-a', 'europe-central2-b', 'europe-central2-c'],
+    'europe-southwest1'             : ['europe-southwest1-a', 'europe-southwest1-b', 'europe-southwest1-c'],
+    'europe-west8'                  : ['europe-west8-a', 'europe-west8-b', 'europe-west8-c'],
+    'europe-west9'                  : ['europe-west9-a', 'europe-west9-b', 'europe-west9-c'],
     'asia-south1'                   : ['asia-south1-a', 'asia-south1-b', 'asia-south1-c'],
     'asia-south2'                   : ['asia-south2-a', 'asia-south2-b', 'asia-south2-c'],
     'asia-southeast1'               : ['asia-southeast1-a', 'asia-southeast1-b', 'asia-southeast1-c'],
@@ -63,7 +74,8 @@ var zones = {
     'asia-northeast2'               : ['asia-northeast2-a', 'asia-northeast2-b', 'asia-northeast2-c'],
     'asia-northeast3'               : ['asia-northeast3-a', 'asia-northeast3-b', 'asia-northeast3-c'],
     'australia-southeast1'          : ['australia-southeast1-a', 'australia-southeast1-b', 'australia-southeast1-c'],
-    'australia-southeast2'          : ['australia-southeast2-a', 'australia-southeast2-b', 'australia-southeast2-c']
+    'australia-southeast2'          : ['australia-southeast2-a', 'australia-southeast2-b', 'australia-southeast2-c'],
+    'me-west1'                      : ['me-west1-a', 'me-west1-b', 'me-west1-c'],
 };
 
 module.exports = {
