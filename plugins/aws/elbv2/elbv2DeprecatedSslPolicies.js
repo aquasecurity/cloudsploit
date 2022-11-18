9de1d1310670b6cc430f583b2178ecd6b702e6f2
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-11-18 19:37:02 +0500

    Update plugins/aws/elbv2/elbv2DeprecatedSslPolicies.js

--- plugins/aws/elbv2/elbv2DeprecatedSslPolicies.js
+++ plugins/aws/elbv2/elbv2DeprecatedSslPolicies.js
@@ -82,7 +82,7 @@ module.exports = {
                 if (!SslPolicy){
                     helpers.addResult(results, 0,'No SSL policies found for load balancer', region, alb.LoadBalancerArn);
                 } else if (depPolicies && depPolicies.length){
-                    helpers.addResult(results, 2, `Load balancer listeners are using these deprecated policies : ` + depPolicies.join(', '), region, alb.LoadBalancerArn);
+                    helpers.addResult(results, 2, 'Load balancer listeners are using these deprecated policies : ' + depPolicies.join(', '), region, alb.LoadBalancerArn);
                 } else {
                     helpers.addResult(results, 0, 'Load balancer listeners are using current SSL policies', region, alb.LoadBalancerArn);
                 }
