3947e29f80291849e777c97d17f25d28ace2a672
mehakseedat63 <mehakseedat63@gmail.com>
2022-12-11 17:48:49 +0500

    GCP - PostgreSQL Pg Audit Flag Enabled

--- exports.js
+++ exports.js
@@ -1095,6 +1095,7 @@ module.exports = {
         'mysqlLatestVersion'            : require(__dirname + '/plugins/google/sql/mysqlLatestVersion.js'),
         'postgresqlLatestVersion'       : require(__dirname + '/plugins/google/sql/postgresqlLatestVersion.js'),
         'sqlInstanceLabelsAdded'        : require(__dirname + '/plugins/google/sql/sqlInstanceLabelsAdded.js'),
+        'postgresqlPgAuditEnabled'      : require(__dirname + '/plugins/google/sql/postgresqlPgAuditEnabled.js'),
 
         'bucketVersioning'              : require(__dirname + '/plugins/google/storage/bucketVersioning.js'),
         'bucketLogging'                 : require(__dirname + '/plugins/google/storage/bucketLogging.js'),
