758384209c137196c0a81cab800e7f65b6b184ad
abdullahaslam306 <f178135@nu.edu.pk>
2022-12-12 00:19:18 +0500

    description added

--- plugins/azure/mysqlserver/mysqlServerHasTags.js
+++ plugins/azure/mysqlserver/mysqlServerHasTags.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Databases',
     description: 'Ensures that Azure MySQL Server Has Tags',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
-    recommended_action: 'Modify MySQL server and tags',
+    recommended_action: 'Modify MySQL server and add tags',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
     apis: ['servers:listMysql'],
 
