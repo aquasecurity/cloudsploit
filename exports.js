3f1ee4e625bbe7d955cbc1d8ed48af215578c077
mehakseedat63 <mehakseedat63@gmail.com>
2022-12-15 00:05:24 +0500

    GCP - SQL Server External Scripts Flag Disabled

--- exports.js
+++ exports.js
@@ -1095,6 +1095,7 @@ module.exports = {
         'mysqlLatestVersion'            : require(__dirname + '/plugins/google/sql/mysqlLatestVersion.js'),
         'postgresqlLatestVersion'       : require(__dirname + '/plugins/google/sql/postgresqlLatestVersion.js'),
         'sqlInstanceLabelsAdded'        : require(__dirname + '/plugins/google/sql/sqlInstanceLabelsAdded.js'),
+        'sqlServerExternalScripts'      : require(__dirname + '/plugins/google/sql/sqlServerExternalScripts.js'),
 
         'bucketVersioning'              : require(__dirname + '/plugins/google/storage/bucketVersioning.js'),
         'bucketLogging'                 : require(__dirname + '/plugins/google/storage/bucketLogging.js'),
