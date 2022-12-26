7b743c2a8940ad70b17a231737b20fcd15863c41
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-12-26 19:27:05 +0500

    Update plugins/azure/redisCache/redisCacheHasTags.js

--- plugins/azure/redisCache/redisCacheHasTags.js
+++ plugins/azure/redisCache/redisCacheHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Redis Cache Has Tags',
     category: 'Redis Cache',
     domain: 'Databases',
-    description: 'Ensures that Azure Cache for Redis has tags.',
+    description: 'Ensures that Azure Cache for Redis have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     recommended_action: 'Modify Azure Cache for Redis and add tags.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
