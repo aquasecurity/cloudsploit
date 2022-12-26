22f07a4aa62bb1f389c511841de92006fdcc2377
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-12-26 19:31:19 +0500

    Update plugins/azure/virtualmachines/snapshotHasTags.js

--- plugins/azure/virtualmachines/snapshotHasTags.js
+++ plugins/azure/virtualmachines/snapshotHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Snapshot Has Tags',
     category: 'Virtual Machines',
     domain: 'Compute',
-    description: 'Ensures that Azure Snapshot Has Tags.',
+    description: 'Ensures that Azure Snapshot have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     recommended_action: 'Modify Snapshot and add tags',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
