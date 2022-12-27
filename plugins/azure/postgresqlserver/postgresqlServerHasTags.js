86f33ce27e25067aa4384f90ea3205412f031798
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-12-27 16:10:07 +0500

    Apply suggestions from code review

--- plugins/azure/postgresqlserver/postgresqlServerHasTags.js
+++ plugins/azure/postgresqlserver/postgresqlServerHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'PostgreSQL Server Has Tags',
     category: 'PostgreSQL Server',
     domain: 'Databases',
-    description: 'Ensures that PostgreSQL Server Has Tags.',
+    description: 'Ensure that Azure PostgreSQL servers have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     recommended_action: 'Modify PostgreSQL servers and add tags.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
@@ -34,7 +34,6 @@ module.exports = {
                 return rcb();
             }
             for (let postgresServer of listPostgres.data) {
-                
                 if (!postgresServer.id) continue;
 
                 if (postgresServer.tags && Object.entries(postgresServer.tags).length > 0){
