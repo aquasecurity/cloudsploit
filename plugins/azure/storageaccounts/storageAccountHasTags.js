a196cfef0aa8bb2937aea54867cac8baa93dcfa8
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-12-27 18:36:41 +0500

    Update plugins/azure/storageaccounts/storageAccountHasTags.js

--- plugins/azure/storageaccounts/storageAccountHasTags.js
+++ plugins/azure/storageaccounts/storageAccountHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Storage Account Has Tags',
     category: 'Storage Accounts',
     domain: 'Storage',
-    description: 'Ensures that Azure Storage Account Has Tags associated',
+    description: 'Ensure that Azure Storage accounts have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     recommended_action: 'Modify storage account and add tags',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
