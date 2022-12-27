f76aa8b4cf6547481d5e747d517c46ae535c65b2
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2022-12-27 18:10:20 +0500

    Update plugins/azure/sqlserver/sqlServerHasTags.js

--- plugins/azure/sqlserver/sqlServerHasTags.js
+++ plugins/azure/sqlserver/sqlServerHasTags.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'SQL Server Has Tags',
     category: 'SQL Server',
     domain: 'Databases',
-    description: 'Ensures that Azure SQL Server have tags associated.',
+    description: 'Ensure that Azure SQL servers have tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
     recommended_action: 'Modify SQL Server and add tags.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
