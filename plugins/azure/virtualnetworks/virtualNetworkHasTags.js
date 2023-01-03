4eb873d1a895a8813eac9197b5e5234d80f79d6d
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2023-01-03 14:30:41 +0500

    Update plugins/azure/virtualnetworks/virtualNetworkHasTags.js

--- plugins/azure/virtualnetworks/virtualNetworkHasTags.js
+++ plugins/azure/virtualnetworks/virtualNetworkHasTags.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Network Access Control',
     description: 'Ensures that Microsoft Azure Virtual Network has tags associated.',
     more_info: 'Tags help you to group resources together that are related to or associated with each other. It is a best practice to tag cloud resources to better organize and gain visibility into their usage.',
-    recommended_action: 'Modify virtual network and add tags',
+    recommended_action: 'Modify virtual networks and add tags.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/tag-resources',
     apis: ['virtualNetworks:listAll'],
 
