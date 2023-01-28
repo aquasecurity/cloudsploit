464e054b1c9b5cb367839e11a2b5b52c65b3da31
mehakseedat63 <mehakseedat63@gmail.com>
2023-01-28 05:34:50 +0500

    GCP - API Key API Restriction Plugin

--- exports.js
+++ exports.js
@@ -1171,6 +1171,7 @@ module.exports = {
 
         'apiKeyRotation'                : require(__dirname + '/plugins/google/api/apiKeyRotation.js'),
         'apiKeyApplicationRestriction'  : require(__dirname + '/plugins/google/api/apiKeyApplicationRestriction.js'),
+        'apiKeyAPIRestriction'          : require(__dirname + '/plugins/google/api/apiKeyAPIRestriction.js'),
       
         'privateEndpoint'               : require(__dirname + '/plugins/google/kubernetes/privateEndpoint.js'),
         'monitoringEnabled'             : require(__dirname + '/plugins/google/kubernetes/monitoringEnabled.js'),
