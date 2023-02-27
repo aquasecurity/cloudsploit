39e3ca1a10fc4b9f3ca40e10c526f99a0b3d4f81
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2023-02-27 16:09:04 +0500

    Apply suggestions from code review

--- plugins/aws/ec2/openHTTPS.js
+++ plugins/aws/ec2/openHTTPS.js
@@ -8,11 +8,10 @@ module.exports = {
     description: 'Determine if TCP port 443 for HTTPS is open to the public',
     more_info: 'While some ports are required to be open to the public to function properly, more sensitive services such as HTTPS should be restricted to known IP addresses.',
     link: 'http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/authorizing-access-to-an-instance.html',
-    recommended_action: 'Restrict TCP port 443 to known IP addresses',
+    recommended_action: 'Restrict TCP port 443 to known IP addresses.',
     apis: ['EC2:describeSecurityGroups'],
 
     run: function(cache, settings, callback) {
-
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
