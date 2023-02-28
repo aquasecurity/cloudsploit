55ac9ac9066f9e7de4b59a1d8ff624feab492732
mehakseedat63 <mehakseedat63@gmail.com>
2023-02-28 13:16:14 +0500

    GCP - Trigger Has Tags

--- exports.js
+++ exports.js
@@ -1294,6 +1294,7 @@ module.exports = {
         'accessApprovalEnabled'         : require(__dirname + '/plugins/google/security/accessApprovalEnabled.js'),
 
         'commentControlEnabled'         : require(__dirname + '/plugins/google/cloudbuild/commentControlEnabled.js'),
+        'triggerHasTags'                : require(__dirname + '/plugins/google/cloudbuild/triggerHasTags.js'),
     },
     alibaba: {
         'passwordMinLength'             : require(__dirname + '/plugins/alibaba/ram/passwordMinLength.js'),
