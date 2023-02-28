ba51ceadae0f3ba7ff83928c30278e8683f9af9d
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2023-03-01 01:37:10 +0500

    Apply suggestions from code review

--- plugins/azure/appservice/javaVersion.spec.js
+++ plugins/azure/appservice/javaVersion.spec.js
@@ -119,7 +119,7 @@ describe('javaVersion', function() {
         it('should give unknown result if app has no configs', function(done) {
             const cache = createErrorCache('configs');
             javaVersion.run(cache, {}, (err, results) => {
-                expect(results.length).to.equal(1);
+                expect(results.length).to.equal(2);
 
                 expect(results[0].status).to.equal(3);
                 expect(results[0].message).to.include('Unable to query App Service');
