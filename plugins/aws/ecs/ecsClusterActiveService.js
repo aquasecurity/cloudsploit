4311edda6d54b9fdd8fbe73fd1a01aa458f4419d
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2023-03-06 16:33:05 +0500

    Update plugins/aws/ecs/ecsClusterActiveService.js

--- plugins/aws/ecs/ecsClusterActiveService.js
+++ plugins/aws/ecs/ecsClusterActiveService.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'ECS Cluster Active Services',
     category: 'ECS',
     domain: 'Containers',
-    description: 'Ensure that ECS Cluster have active services',
+    description: 'Ensure that AWS ECS clusters have active services.',
     more_info: 'Amazon ECS service allows you to run and maintain a specified number of instances of a task definition simultaneously in an Amazon ECS cluster. It is recommended to have clusters with the active services to avoid any container attack surface.',
     recommended_action: 'Modify Cluster and create new service',
     link: 'https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs_services.html',
