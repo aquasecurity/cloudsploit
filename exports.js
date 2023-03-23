e750d51430bf5ace20703ba06ad01c69c7df0ae6
abdullahaslam306 <f178135@nu.edu.pk>
2023-03-23 13:55:42 +0500

    backup vault policies

--- exports.js
+++ exports.js
@@ -592,6 +592,7 @@ module.exports = {
         'backupDeletionProtection'      : require(__dirname + '/plugins/aws/backup/backupDeletionProtection.js'),
         'compliantLifecyleConfigured'   : require(__dirname + '/plugins/aws/backup/compliantLifecyleConfigured.js'),
         'backupVaultHasTags'            : require(__dirname + '/plugins/aws/backup/backupVaultHasTags.js'),
+        'backupVaultPolicies'           : require(__dirname + '/plugins/aws/backup/backupVaultPolicies.js'),
 
         'equipmentdatasetEncrypted'     : require(__dirname + '/plugins/aws/lookout/equipmentdatasetEncrypted.js'),
 
