6a7a50f54341516e86671e21f7c49022080a5a68
abdullahaslam306 <f178135@nu.edu.pk>
2023-03-31 19:46:15 +0500

    description update

--- plugins/aws/dynamodb/dynamoUnusedTable.js
+++ plugins/aws/dynamodb/dynamoUnusedTable.js
@@ -8,7 +8,7 @@ module.exports = {
     description: 'Ensures that Amazon DynamoDB unused tables are removed to optimise costs.',
     more_info: 'A DynamoDB table is considered unused if its item count is zero. As a best practice, delete unused tables for operational efficiency and better resource management. This will also prevent resource wastage and unnecessary costs. This plugin might produce false positives or false negatives as AWS updates table count every 6 hours.',
     link: 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/WorkingWithTables.Basics.html',
-    recommended_action: 'Identify unused tables and remove them',
+    recommended_action: 'Remove unused tables if you no longer need them.',
     apis: ['DynamoDB:listTables', 'DynamoDB:describeTable', 'STS:getCallerIdentity'],
 
     run: function(cache, settings, callback) {
