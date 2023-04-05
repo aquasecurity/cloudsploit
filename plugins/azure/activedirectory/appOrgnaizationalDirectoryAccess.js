56a1c4023dae50ef03bb1b7a4a6be7d4836a7025
abdullahaslam306 <f178135@nu.edu.pk>
2023-04-05 15:50:38 +0500

    pr changes and lambda runtime changed

--- plugins/azure/activedirectory/appOrgnaizationalDirectoryAccess.js
+++ plugins/azure/activedirectory/appOrgnaizationalDirectoryAccess.js
@@ -2,13 +2,13 @@ const async = require('async');
 const helpers = require('../../../helpers/azure');
 
 module.exports = {
-    title: 'Azure App Organizational Directory Access',
+    title: 'Azure AD App Organisational Directory Access',
     category: 'Active Directory',
     domain: 'Identity and Access Management',
-    description: 'Ensures that Azure Apps are accessible to accounts in organizational directory only.',
-    more_info: 'AAD provides different types of account access. By using single-tenant authentication, the impact is limited to the application’s tenant – all users from the same tenant could connect to the application and save app from unauthorized access.',
+    description: 'Ensures that Azure Active Directory applications are accessible to accounts in organisational directory only.',
+    more_info: 'AAD provides different types of account access. By using single-tenant authentication, the impact gets limited to the application’s tenant i.e. all users from the same tenant could connect to the application and save app from unauthorised access.',
     link: 'https://learn.microsoft.com/en-us/azure/active-directory/develop/single-and-multi-tenant-apps',
-    recommended_action: 'Modify the Azure app authentication setting and provide access to accounts in  organizational directory only',
+    recommended_action: 'Modify the Azure app authentication setting and provide access to accounts in organisational directory only',
     apis: ['applications:list'],
 
     run: function(cache, settings, callback) {
@@ -23,21 +23,20 @@ module.exports = {
             if (!applications) return rcb();
 
             if (applications.err || !applications.data) {
-                helpers.addResult(results, 3, 'Unable to query for applications: ' + helpers.addError(applications), location);
+                helpers.addResult(results, 3, 'Unable to query for AAD applications: ' + helpers.addError(applications), location);
                 return rcb();
             }
             if (!applications.data.length) {
-                helpers.addResult(results, 0, 'No existing application found', location);
+                helpers.addResult(results, 0, 'No existing AAD application found', location);
                 return rcb();
             }
-            console.log(applications)
             for (let app of applications.data) {
                 if (!app.id) continue;
 
                 if (app.signInAudience && app.signInAudience === 'AzureADMultipleOrgs' || app.signInAudience === 'AzureADandPersonalMicrosoftAccount'){
-                    helpers.addResult(results, 2, 'Multi tenant access enabled for the application.', location, app.displayName);
+                    helpers.addResult(results, 2, 'Multi tenant access enabled for the application.', location, app.appId);
                 } else {
-                    helpers.addResult(results, 0, 'Single tenant access enabled for the application.', location, app.displayName);
+                    helpers.addResult(results, 0, 'Single tenant access enabled for the application.', location, app.appId);
                 }
             }
             rcb();
