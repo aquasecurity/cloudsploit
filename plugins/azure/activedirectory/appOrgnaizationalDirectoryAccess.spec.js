d4a25d7044260cb02d86b1df101eb7e53f9ea0b7
abdullahaslam306 <f178135@nu.edu.pk>
2023-04-05 15:53:36 +0500

    unit test message change

--- plugins/azure/activedirectory/appOrgnaizationalDirectoryAccess.spec.js
+++ plugins/azure/activedirectory/appOrgnaizationalDirectoryAccess.spec.js
@@ -48,7 +48,7 @@ describe('appOrgnaizationalDirectoryAccess', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('No existing application found');
+                expect(results[0].message).to.include('No existing AAD application found');
                 expect(results[0].region).to.equal('global');
                 done()
             };
@@ -64,7 +64,7 @@ describe('appOrgnaizationalDirectoryAccess', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
-                expect(results[0].message).to.include('Unable to query for applications');
+                expect(results[0].message).to.include('Unable to query for AAD applications:');
                 expect(results[0].region).to.equal('global');
                 done()
             };
