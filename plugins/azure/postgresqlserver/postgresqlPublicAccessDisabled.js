a632d5a54822f47f7f4e691ca12e2144f31a7fe7
abdullahaslam306 <f178135@nu.edu.pk>
2023-04-26 01:38:08 +0500

    public network access disabled dev completed

---
+++ plugins/azure/postgresqlserver/postgresqlPublicAccessDisabled.js
@@ -0,0 +1,55 @@
+const async = require('async');
+const helpers = require('../../../helpers/azure');
+
+module.exports = {
+    title: 'PostgreSQL Server Public Network Access Disabled',
+    category: 'PostgreSQL Server',
+    domain: 'Databases',
+    description: 'Ensure that Microsoft Azure PostgreSQL database servers have public network access disabled.',
+    more_info: 'Disabling network access to public for PostgreSQL database servers improves security by preventing access from public endpoints and allowing access only from trusted sources.',
+    recommended_action: 'Disable network access to the public for PostgreSQL database servers.',
+    link: 'https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-firewall-rules',
+    apis: ['servers:listPostgres'],
+
+    run: function(cache, settings, callback) {
+        const results = [];
+        const source = {};
+        const locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.servers, (location, rcb) => {
+
+            const servers = helpers.addSource(cache, source,
+                ['servers', 'listPostgres', location]);
+
+            if (!servers) return rcb();
+
+            if (servers.err || !servers.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for PostgreSQL Servers: ' + helpers.addError(servers), location);
+                return rcb();
+            }
+
+            if (!servers.data.length) {
+                helpers.addResult(results, 0, 'No existing PostgreSQL Servers found', location);
+                return rcb();
+            }
+
+            for (let postgresServer of servers.data) {
+                if (postgresServer.properties &&
+                    postgresServer.properties.publicNetworkAccess &&
+                    postgresServer.properties.publicNetworkAccess.toUpperCase() === 'DISABLED') {
+                    helpers.addResult(results, 0,
+                        'The PostgreSQL Server has public network access disabled', location, postgresServer.id);
+                } else {
+                    helpers.addResult(results, 2,
+                        'The PostgreSQL Server does not have public network access disabled', location, postgresServer.id);
+                }
+            }
+
+            rcb();
+        }, function() {
+            // Global checking goes here
+            callback(null, results, source);
+        });
+    }
+};
---
+++ plugins/azure/postgresqlserver/postgresqlPublicAccessDisabled.spec.js
@@ -0,0 +1,114 @@
+var expect = require('chai').expect;
+var postgresqlPublicAccessDisabled = require('./postgresqlPublicAccessDisabled');
+
+const listPostgres = [
+    {
+        "id": "/subscriptions/jk34n234k-dwef/resourceGroups/akhtar-rg/providers/Microsoft.DBforPostgreSQL/servers/geo-redundant",
+        "type": "Microsoft.DBforPostgreSQL/servers",
+        "storageProfile": {
+            "storageMB": 5120,
+            "backupRetentionDays": 7,
+            "geoRedundantBackup": "Disabled",
+            "storageAutogrow": "Disabled"
+        },
+        "properties": {
+            "publicNetworkAccess": "Enabled"
+        }
+    },
+    {
+        "id": "/subscriptions/jk34n234k-dwef/resourceGroups/akhtar-rg/providers/Microsoft.DBforPostgreSQL/servers/geo-redundant",
+        "type": "Microsoft.DBforPostgreSQL/servers",
+        "storageProfile": {
+            "storageMB": 5120,
+            "backupRetentionDays": 7,
+            "geoRedundantBackup": "Enabled",
+            "storageAutogrow": "Disabled"
+        },
+        "properties": {
+            "publicNetworkAccess": "Disabled"
+        }
+    }
+];
+const createCache = (err, list) => {
+    return {
+        servers: {
+            listPostgres: {
+                'eastus': {
+                    err: err,
+                    data: list
+                }
+            }
+        }
+    }
+};
+
+describe('postgresqlPublicAccessDisabled', function() {
+    describe('run', function() {
+        it('should give passing result if no servers', function(done) {
+            const callback = (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('No existing PostgreSQL Servers found');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            };
+
+            const cache = createCache(
+                null,
+                []
+            );
+
+            postgresqlPublicAccessDisabled.run(cache, {}, callback);
+        })
+
+        it('should give failing result if postgresql server does not have public network access disabled', function(done) {
+            const callback = (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('The PostgreSQL Server does not have public network access disabled');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            };
+
+            const cache = createCache(
+                null,
+                [listPostgres[0]]
+            );
+
+            postgresqlPublicAccessDisabled.run(cache, {}, callback);
+        });
+
+        it('should give passing result if postgresql server public network access disabled', function(done) {
+            const callback = (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('The PostgreSQL Server has public network access disabled');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            };
+
+            const cache = createCache(
+                null,
+                [listPostgres[1]]
+            );
+
+            postgresqlPublicAccessDisabled.run(cache, {}, callback);
+        });
+
+        it('should give unknown result if unable to query for PostgreSQL Servers', function(done) {
+            const callback = (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                expect(results[0].message).to.include('Unable to query for PostgreSQL Servers');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            };
+
+            const cache = createCache(
+                { message: "Unable to list servers" },
+            );
+
+            postgresqlPublicAccessDisabled.run(cache, {}, callback);
+        });
+    })
+})
