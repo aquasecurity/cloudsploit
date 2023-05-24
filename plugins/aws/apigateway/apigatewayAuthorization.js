8acaa05ec4bb96b72187a72e4dc135d5741f192b
fatima99s <fatimasarfraz15@gmail.com>
2023-05-24 18:48:06 +0500

    plugin to check apigateway is not using authorizer

---
+++ plugins/aws/apigateway/apigatewayAuthorization.js
@@ -0,0 +1,77 @@
+var async = require('async');
+var helpers = require('../../../helpers/aws');
+
+module.exports = {
+    title: 'API Gateway Authorization',
+    category: 'API Gateway',
+    domain: 'Availability',
+    description: 'Ensures that Amazon API Gateway APIs are using authorizer.',
+    more_info: 'API Gateway API should be using authorizer to enforce security measures and control access to API resources.',
+    recommended_action: 'Please review the API Gateway configuration and ensure that appropriate authorizers are set up for each API. Authorizers can be used to authenticate and authorize API requests.',
+    link: 'https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-use-lambda-authorizer.html',
+    apis: ['APIGateway:getRestApis','APIGateway:getAuthorizers'],
+    
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var regions = helpers.regions(settings);
+        var awsOrGov = helpers.defaultPartition(settings);
+
+        async.each(regions.apigateway, function(region, rcb){
+            var getRestApis = helpers.addSource(cache, source,
+                ['apigateway', 'getRestApis', region]);
+
+            if (!getRestApis) return rcb();
+
+            if (getRestApis.err || !getRestApis.data) {
+                helpers.addResult(results, 3,
+                    `Unable to query for API Gateway Rest APIs: ${helpers.addError(getRestApis)}`, region);
+                return rcb();
+            }
+
+            if (!getRestApis.data.length) {
+                helpers.addResult(results, 0, 'No API Gateway Rest APIs found', region);
+                return rcb();
+            }
+
+            async.each(getRestApis.data, function(api, cb) {
+                if (!api.id) return cb();
+
+                var apiArn = `arn:${awsOrGov}:apigateway:${region}::/restapis/${api.id}`;
+
+                var getAuthorizers = helpers.addSource(cache, source,
+                    ['apigateway', 'getAuthorizers', region, api.id]);
+          
+                    if (!getAuthorizers || getAuthorizers.err || !getAuthorizers.data || !getAuthorizers.data.items) {
+                        helpers.addResult(results, 3,
+                            `Unable to query for API Gateway Authorizers: ${helpers.addError(getAuthorizers)}`,
+                            region, apiArn);
+                        return cb();
+                    }
+
+                if (getAuthorizers.data.items.length === 0) {
+                    helpers.addResult(results, 2,
+                      'No authorizers found for API Gateway Rest API ' + api.name,region );
+                  } else {
+                    // Authorizers found
+                    for (var i = 0; i < getAuthorizers.data.items.length; i++) {
+                      var authorizer = getAuthorizers.data.items[i];
+                      helpers.addResult(results,0,
+                        'Authorizer found for API Gateway Rest API ' + api.name + ': ' + authorizer.name,
+                        region
+                      );
+                    }
+                  }
+          
+                   cb();
+
+                }, function(){
+                    rcb();
+                });
+            },function() {
+                callback(null, results, source);
+         });
+    }
+};
+
+        
