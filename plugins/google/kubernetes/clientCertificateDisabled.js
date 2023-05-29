b946992238f892c31e34d82182993e4aebb1f670
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2023-05-29 13:58:04 +0500

    Update plugins/google/kubernetes/clientCertificateDisabled.js

--- plugins/google/kubernetes/clientCertificateDisabled.js
+++ plugins/google/kubernetes/clientCertificateDisabled.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Clieent Certificate Disabled',
     category: 'Kubernetes',
     domain: 'Containers',
-    description: 'Ensure client certificate authentication to Kubernetes clusters is disabled',
+    description: 'Ensure client certificate authentication to Kubernetes clusters is disabled.',
     more_info: 'In authentication using client certificates, the client presents a certificate signed by cluster root certificate authority which is only base64 encoded and not encrypted. The client certificate authentication method is considered legacy and cause potential security risks. It is recommended to use the default GKE OAuth method for authentication.',
     link: 'https://cloud.google.com/kubernetes-engine/docs/how-to/hardening-your-cluster#restrict_authn_methods',
     recommended_action: 'Ensure no kubernetes clusters are using client certificates for authentication',
