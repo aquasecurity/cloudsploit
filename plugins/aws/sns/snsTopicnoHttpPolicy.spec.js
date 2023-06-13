12d3356fe7250169133c14e656c9465cde476017
fatima99s <fatimasarfraz15@gmail.com>
2023-06-14 02:46:07 +0500

    fixed

--- plugins/aws/sns/snsTopicnoHttpPolicy.spec.js
+++ plugins/aws/sns/snsTopicnoHttpPolicy.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var snsTopicnoHttpPolicy = require('./snsTopicnoHttpPolicy');
+var snsTopicnoHttpPolicy = require('./snsTopicNoHttpPolicy');
 
 
 const listTopics = [
