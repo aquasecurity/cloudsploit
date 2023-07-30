924465762ffce0db968ea820ebd476a1c57c0096
abdullahaslam306 <f178135@nu.edu.pk>
2023-07-31 00:32:58 +0500

    Anonymous pull enabled

---
+++ plugins/azure/containerregistry/acrAnonymousPullAccessEnabled.js
@@ -0,0 +1,52 @@
+var async = require('async');
+var helpers = require('../../../helpers/azure');
+
+module.exports = {
+    title: 'ACR Anonymous Pull Access Enabled',
+    category: 'Container Registry',
+    domain: 'Containers',
+    description: 'Ensures that anonymous pull access is enabled for Azure Container registries.',
+    more_info: 'Anonymous pull access to a container registry can pose security risks and lead to unauthorized access to images.',
+    recommended_action: 'Enable anonymous pull access for each container registry.',
+    link: 'https://learn.microsoft.com/en-us/azure/container-registry/anonymous-pull-access',
+    apis: ['registries:list'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.registries, function(location, rcb) {
+            var registries = helpers.addSource(cache, source,
+                ['registries', 'list', location]);
+
+            if (!registries) return rcb();
+
+            if (registries.err || !registries.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for container registries: ' + helpers.addError(registries), location);
+                return rcb();
+            }
+
+            if (!registries.data.length) {
+                helpers.addResult(results, 0, 'No existing Container registries found', location);
+                return rcb();
+            }
+            for (let registry of registries.data){
+                if (!registry.id) continue;
+
+            console.log(registry)
+                if (registry.anonymousPullEnabled) {
+                    helpers.addResult(results, 0, 'Anonymous pull access is enabled for the container registry', location, registry.id);
+                } else {
+                    helpers.addResult(results, 2, 'Anonymous pull access is not enabled for the container registry', location, registry.id);
+                }
+            }
+
+            rcb();
+        }, function() {
+            // Global checking goes here
+            callback(null, results, source);
+        });
+    }
+};
