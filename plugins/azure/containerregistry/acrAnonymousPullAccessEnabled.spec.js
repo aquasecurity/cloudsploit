c694f242dd946b132688ca264da869493d07267c
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2023-08-03 18:27:10 +0500

    Update plugins/azure/containerregistry/acrAnonymousPullAccessEnabled.spec.js

--- plugins/azure/containerregistry/acrAnonymousPullAccessEnabled.spec.js
+++ plugins/azure/containerregistry/acrAnonymousPullAccessEnabled.spec.js
@@ -56,7 +56,7 @@ describe('acrAnonymousPullAccessEnabled', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('No existing Container registries found');
+                expect(results[0].message).to.include('No existing container registries found');
                 expect(results[0].region).to.equal('eastus');
                 done()
             };
