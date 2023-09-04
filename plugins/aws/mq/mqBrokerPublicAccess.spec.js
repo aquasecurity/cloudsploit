ae26c790e554eb993818a1eac79f071ab3d8065e
muzzamilinovaqo <140418718+muzzamilinovaqo@users.noreply.github.com>
2023-09-04 06:30:02 -0700

    Apply suggestions from code review
    Co-authored-by: AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>

--- plugins/aws/mq/mqBrokerPublicAccess.spec.js
+++ plugins/aws/mq/mqBrokerPublicAccess.spec.js
@@ -80,6 +80,7 @@ describe('mqBrokerPublicAccessibility', function () {
             mqBrokerPublicAccessibility.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('MQ Broker is not publicly accessible');
                 expect(results[0].region).to.equal('us-east-1');
                 done();
             });
