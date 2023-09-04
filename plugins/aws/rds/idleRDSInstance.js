9ffc915afc03b193e3f71b09f614fdda66027c55
--global <fatimasarfraz15@gmail.com>
2023-09-04 18:22:00 +0500

    fixed

--- plugins/aws/rds/idleRDSInstance.js
+++ plugins/aws/rds/idleRDSInstance.js
@@ -5,27 +5,27 @@ module.exports = {
     title: 'RDS Idle Instance Status',
     category: 'RDS',
     domain: 'Databases',
-    description: 'Identify RDS instance having CPU utilization below defined threshold within last 7 days (idle instance).',
-    more_info: 'Idle Amazon RDS instance represent a good candidate to reduce your monthly AWS costs and avoid accumulating unnecessary usage charges.',
-    link: 'https://aws.amazon.com/rds/features/',
+    description: 'Ensure there are no RDS instances with CPU utilization below defined threshold within last 7 days.',
+    more_info: 'Idle Amazon RDS instance is a prime candidate for reducing monthly AWS expenses and preventing unnecessary usage charges from accumulating.',
+    link: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/amazon-rds-monitoring-alerting/db-instance-cloudwatch-metrics.html',
     recommended_action: 'Identify and remove idle RDS instance',
     apis: ['RDS:describeDBInstances', 'CloudWatch:getRdsMetricStatistics', 'CloudWatch:getRdsWriteIOPSMetricStatistics', 'CloudWatch:getRdsReadIOPSMetricStatistics'],
     settings: {
         rds_idle_instance_cpu_percentage: {
             name: 'RDS Idle Instance Average CPU Usage Percentage',
-            description: 'Return a failing result when consumed RDS insatnce cpu threshold equals or less this percentage',
+            description: 'Return a failing result when consumed RDS instance cpu threshold equals or less this percentage',
             regex: '^(100|[1-9][0-9]?)$', 
             default: '1.0'
         },
         rds_idle_instance_readIOPS_percentage: {
             name: 'RDS Idle Instance Average Read IOPS Percentage',
-            description: 'Return a failing result when consumed RDS insatnce read IOPS threshold equals or less this percentage',
+            description: 'Return a failing result when consumed RDS instance read IOPS threshold equals or less this percentage',
             regex: '^(100|[1-9][0-9]?)$', 
             default: '20'
         },
         rds_idle_instance_writeIOPS_percentage: {
             name: 'RDS Idle Instance Average Write IOPS Percentage',
-            description: 'Return a failing result when consumed RDS insatnce write IOPS threshold equals or less this percentage',
+            description: 'Return a failing result when consumed RDS instance write IOPS threshold equals or less this percentage',
             regex: '^(100|[1-9][0-9]?)$', 
             default: '20'
         }
