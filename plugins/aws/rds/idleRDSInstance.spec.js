e421b0bbbd56f9ee22c4d69b1e681866d69a8634
Fatima <66124862+fatima99s@users.noreply.github.com>
2023-09-05 18:54:45 +0500

    Update idleRDSInstance.spec.js

--- plugins/aws/rds/idleRDSInstance.spec.js
+++ plugins/aws/rds/idleRDSInstance.spec.js
@@ -344,15 +344,15 @@ const createNullCache = () => {
 
 describe('idleRDSInstance', function () {
     describe('run', function () {
-        // it('should PASS if the RDS Instance cpu utilization is more than 1.0 percent or more than 20 Read or Write IOPS', function (done) {
-        //     const cache = createCache([describeDBInstances[0]], rdsMetricStatistics[1], rdsReadMetricStatistics[0], rdsWriteMetricStatistics[0]);
-        //     idleRDSInstance.run(cache, {}, (err, results) => {
-        //         expect(results.length).to.equal(1);
-        //         expect(results[0].status).to.equal(0);
-        //         expect(results[0].region).to.equal('us-east-1');
-        //         done();
-        //     });
-        // });
+        it('should PASS if the RDS Instance cpu utilization is more than 1.0 percent or more than 20 Read or Write IOPS', function (done) {
+            const cache = createCache([describeDBInstances[0]], rdsMetricStatistics[1], rdsReadMetricStatistics[0], rdsWriteMetricStatistics[0]);
+            idleRDSInstance.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].region).to.equal('us-east-1');
+                done();
+            });
+        });
 
         it('should FAIL if the RDS Instance cpu utilization is less than or equal to 1.0 percent', function (done) {
             const cache = createCache([describeDBInstances[1]], rdsMetricStatistics[0], rdsReadMetricStatistics[1], rdsWriteMetricStatistics[1]);
@@ -364,34 +364,34 @@ describe('idleRDSInstance', function () {
             });
         });
 
-        // it('should PASS if no RDS Instance found', function (done) {
-        //     const cache = createCache([]);
-        //     idleRDSInstance.run(cache, {}, (err, results) => {
-        //         expect(results.length).to.equal(1);
-        //         expect(results[0].status).to.equal(0);
-        //         expect(results[0].region).to.equal('us-east-1');
-        //         expect(results[0].message).to.include('No RDS instance found');
-        //         done();
-        //     });
-        // });
+        it('should PASS if no RDS Instance found', function (done) {
+            const cache = createCache([]);
+            idleRDSInstance.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].region).to.equal('us-east-1');
+                expect(results[0].message).to.include('No RDS instance found');
+                done();
+            });
+        });
 
-        // it('should UNKNOWN if unable to describe RDS Instance', function (done) {
-        //     const cache = createErrorCache();
-        //     idleRDSInstance.run(cache, {}, (err, results) => {
-        //         expect(results.length).to.equal(1);
-        //         expect(results[0].status).to.equal(3);
-        //         expect(results[0].region).to.equal('us-east-1');
-        //         expect(results[0].message).to.include('Unable to query for RDS instance: ');
-        //         done();
-        //     });
-        // });
+        it('should UNKNOWN if unable to describe RDS Instance', function (done) {
+            const cache = createErrorCache();
+            idleRDSInstance.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                expect(results[0].region).to.equal('us-east-1');
+                expect(results[0].message).to.include('Unable to query for RDS instance: ');
+                done();
+            });
+        });
 
-        // it('should not return any results if describe RDS Instance response not found', function (done) {
-        //     const cache = createNullCache();
-        //     idleRDSInstance.run(cache, {}, (err, results) => {
-        //         expect(results.length).to.equal(0);
-        //         done();
-        //     });
-        // }); 
+        it('should not return any results if describe RDS Instance response not found', function (done) {
+            const cache = createNullCache();
+            idleRDSInstance.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(0);
+                done();
+            });
+        }); 
     });
 });
