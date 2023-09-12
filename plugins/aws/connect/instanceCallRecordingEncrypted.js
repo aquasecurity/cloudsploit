01c5431938f01e98cee090cc7fb832abd90e670e
--global <fatimasarfraz15@gmail.com>
2023-09-12 12:30:08 +0500

    Map-Events-to-AWS-Plugins

--- plugins/aws/connect/instanceCallRecordingEncrypted.js
+++ plugins/aws/connect/instanceCallRecordingEncrypted.js
@@ -19,6 +19,7 @@ module.exports = {
             default: 'awscmk'
         }
     },
+    realtime_triggers: ['connect:CreateInstance', 'connect:AssociateInstanceStorageConfig', 'connect:UpdateInstanceStorageConfig'],
 
     run: function(cache, settings, callback) {
         var results = [];
