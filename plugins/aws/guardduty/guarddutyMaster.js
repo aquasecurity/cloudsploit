90a92162384fcd42de39a57ff7a5e54edfaee6f3
--global <fatimasarfraz15@gmail.com>
2023-09-12 12:06:25 +0500

    Map-Events-to-AWS-Plugins

--- plugins/aws/guardduty/guarddutyMaster.js
+++ plugins/aws/guardduty/guarddutyMaster.js
@@ -18,7 +18,7 @@ module.exports = {
             default: '',
         },
     },
-    realtime_triggers: ['guardduty:CreateDetector'],
+    realtime_triggers: ['guardduty:CreateDetector', 'guardduty:CreateMembers'],
 
     run: function(cache, settings, callback) {
         var results = [];
