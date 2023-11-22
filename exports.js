a6cf4373098d83f4ed2386dd732aa017ececbc95
alphadev4 <devalpha4@gmail.com>
2023-11-22 23:31:07 +0500

    SAAS-20313/Front-Door-Request-Body-Inspection

--- exports.js
+++ exports.js
@@ -989,7 +989,9 @@ module.exports = {
         'eventHubMinimumTLSversion'     : require(__dirname + '/plugins/azure/eventhub/eventHubMinimumTLSversion.js'),
 
         'accessLogsEnabled'             : require(__dirname + '/plugins/azure/frontdoor/accessLogsEnabled.js'),
-        'frontDoorMinimumTlsVersion'    : require(__dirname + '/plugins/azure/frontdoor/frontDoorMinimumTlsVersion.js')
+        'frontDoorMinimumTlsVersion'    : require(__dirname + '/plugins/azure/frontdoor/frontDoorMinimumTlsVersion.js'),
+        'frontDoorRequestBodyInspection': require(__dirname + '/plugins/azure/frontdoor/frontDoorRequestBodyInspection.js'),
+
     },
     github: {
         'publicKeysRotated'             : require(__dirname + '/plugins/github/users/publicKeysRotated.js'),
---
+++ plugins/azure/frontdoor/frontDoorRequestBodyInspection.js
@@ -0,0 +1,58 @@
+const async = require('async');
+const helpers = require('../../../helpers/azure');
+
+module.exports = {
+    title: 'Front Door Request Body Inspection',
+    category: 'Front Door',
+    domain: 'Content Delivery',
+    description: 'Ensures that request body inspection is enabled for Azure Front Door premium WAF policy.',
+    more_info: 'Web Application Firewalls associated to Azure Front Doors premium that have request body inspection enabled, allows to inspect properties within the HTTP body that may not be evaluated in the HTTP headers, cookies, or URI.',
+    recommended_action: 'Ensure that request body inspection in policy settings for Azure Front Door WAF policy is enabled.',
+    link: 'https://learn.microsoft.com/en-us/azure/web-application-firewall/ag/application-gateway-waf-request-size-limits#request-body-inspection',
+    apis: ['afdWafPolicies:listAll'],
+
+    run: function(cache, settings, callback) {
+        const results = [];
+        const source = {};
+        const locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.afdWafPolicies, (location, rcb) => {
+
+            var afdWafPolicies = helpers.addSource(cache, source,
+                ['afdWafPolicies', 'listAll', location]);
+
+            if (!afdWafPolicies) return rcb();
+
+            if (afdWafPolicies.err || !afdWafPolicies.data) {
+                helpers.addResult(results, 3, 'Unable to query for Front Door WAF policies: ' + helpers.addError(afdWafPolicies), location);
+                return rcb();
+            }
+            if (!afdWafPolicies.data.length) {
+                helpers.addResult(results, 0, 'No existing Front Door WAF policies found', location);
+                return rcb();
+            }
+
+            var frontDoorWafPolicies = false;
+            for (let policy of afdWafPolicies.data) {
+                if (!policy.id || !policy.sku || policy.sku.name.toLowerCase() != 'premium_azurefrontdoor') continue;
+
+                frontDoorWafPolicies = true;
+                if (policy.policySettings && 
+                    policy.policySettings.requestBodyCheck && 
+                    policy.policySettings.requestBodyCheck.toLowerCase() == 'enabled') {
+                    helpers.addResult(results, 0, 'Front Door WAF policy has request body inspection enabled', location, policy.id);
+                } else {
+                    helpers.addResult(results, 2, 'Front Door WAF policy does not have request body inspection enabled', location, policy.id);
+                }
+            }
+
+            if (!frontDoorWafPolicies) {
+                helpers.addResult(results, 0, 'No existing Front Door WAF policies found', location);
+            }
+
+            rcb();
+        }, function() {
+            callback(null, results, source);
+        });
+    }
+};
