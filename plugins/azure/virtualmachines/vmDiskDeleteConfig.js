cb7d0b7f953d623580fe3370f469951239aabade
Muzzammil <muzzamil.hussain@inovaqo.com>
2023-11-30 19:08:13 +0500

    added azure vm plugin diskDeleteConfig

---
+++ plugins/azure/virtualmachines/vmDiskDeleteConfig.js
@@ -0,0 +1,47 @@
+var async = require('async');
+var helpers = require('../../../helpers/azure');
+
+module.exports = {
+    title: 'Automatically Delete Disks on VM Termination',
+    category: 'Virtual Machines',
+    domain: 'Compute',
+    description: 'Enable the option to automatically delete disks when the associated VM is terminated to ensure all confidential information is wiped.',
+    more_info: 'Disks persist independently from VMs. Enabling this option ensures that all disks associated with a VM are deleted automatically when the VM is terminated, enhancing security.',
+    recommended_action: 'Configure VMs to automatically delete disks when the VM is terminated to enhance security and prevent lingering confidential information.',
+    link: 'https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/disk-delete',
+    apis: ['virtualMachines:listAll'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.virtualMachines, function(location, rcb) {
+            var virtualMachines = helpers.addSource(cache, source,
+                ['virtualMachines', 'listAll', location]);
+
+            if (!virtualMachines) return rcb();
+
+            if (virtualMachines.err || !virtualMachines.data) {
+                helpers.addResult(results, 3, 'Unable to query for virtual machines: ' + helpers.addError(virtualMachines), location);
+                return rcb();
+            }
+
+            if (!virtualMachines.data.length) {
+                helpers.addResult(results, 0, 'No existing Virtual Machines found', location);
+                return rcb();
+            }
+
+            virtualMachines.data.forEach(virtualMachine => {
+                if (virtualMachine.storageProfile && virtualMachine.storageProfile.osDisk && virtualMachine.storageProfile.osDisk.deleteOption=='Delete') {
+                    helpers.addResult(results, 0, 'Automatically delete disks with VM is configured', location, virtualMachine.id);
+                } else {
+                    helpers.addResult(results, 2, 'Automatically delete disks with VM is not configured', location, virtualMachine.id);
+                }
+            });
+            rcb();
+        }, function() {
+            callback(null, results, source);
+        });
+    }
+};
