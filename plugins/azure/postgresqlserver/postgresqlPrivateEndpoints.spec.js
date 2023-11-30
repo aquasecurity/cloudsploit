1ad0ab8f2858ce2d3cf13a93e205f1221d8e1e42
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2023-12-01 03:32:03 +0500

    Update plugins/azure/postgresqlserver/postgresqlPrivateEndpoints.spec.js

--- plugins/azure/postgresqlserver/postgresqlPrivateEndpoints.spec.js
+++ plugins/azure/postgresqlserver/postgresqlPrivateEndpoints.spec.js
@@ -14,7 +14,7 @@ const listPostgres = [
         'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.DBforPostgreSQL/servers/server1',
         'name': 'server1',
         'type': 'Microsoft.DBforPostgreSQL/servers',
-        'administratorLogin': 'Aquaadmin',
+        'administratorLogin': 'test',
         'storageProfile': {
             'storageMB': 5120,
             'backupRetentionDays': 7,
