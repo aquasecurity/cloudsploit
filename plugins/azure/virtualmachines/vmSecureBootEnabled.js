188573ffe68cbbb91ef2f798bc8f7947f47e8093
Muzzammil <muzzamil.hussain@inovaqo.com>
2023-11-30 15:53:54 +0500

    added azure vm plugin secureBootEnabled

---
+++ plugins/azure/virtualmachines/vmSecureBootEnabled.js
@@ -0,0 +1,49 @@
+var async = require('async');
+var helpers = require('../../../helpers/azure');
+
+module.exports = {
+    title: 'Select Secure Boot for Azure VMs',
+    category: 'Virtual Machines',
+    domain: 'Compute',
+    description: 'Ensure Secure Boot is enabled for Azure virtual machines (VM) to protect against boot kits, rootkits, and kernel-level malware.',
+    more_info: 'Secure Boot helps protect VMs by ensuring that only signed and trusted components are allowed to execute during the boot process.',
+    recommended_action: 'Enable Secure Boot for Azure virtual machines to enhance security and protect against advanced threats during the boot process.',
+    link: 'https://docs.microsoft.com/en-us/azure/virtual-machines/security-secure-boot',
+    apis: ['virtualMachines:listAll'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.virtualMachines, function(location, rcb) {
+            var virtualMachines = helpers.addSource(cache, source,
+                ['virtualMachines', 'listAll', location]);
+
+            if (!virtualMachines) return rcb();
+
+            if (virtualMachines.err || !virtualMachines.data) {
+                helpers.addResult(results, 3, 'Unable to query for virtual machines: ' + helpers.addError(virtualMachines), location);
+                return rcb();
+            }
+
+            if (!virtualMachines.data.length) {
+                helpers.addResult(results, 0, 'No existing Virtual Machines found', location);
+                return rcb();
+            }
+
+            virtualMachines.data.forEach(virtualMachine => {
+                if (virtualMachine.securityProfile && virtualMachine.securityProfile.uefiSettings.secureBootEnabled) {
+                    helpers.addResult(results, 0, 'Secure Boot is selected for Azure Virtual Machine', location, virtualMachine.id);
+                } else {
+                    helpers.addResult(results, 2, 'Secure Boot is not selected for Azure Virtual Machine', location, virtualMachine.id);
+
+                }
+            });
+
+            rcb();
+        }, function() {
+            callback(null, results, source);
+        });
+    }
+};
