f4ede63dd0f655c5a6190f13f424f5c0df3344ab
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2023-12-01 15:48:31 +0500

    Update plugins/azure/applicationGateway/agSslPolicy.js
    Co-authored-by: mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>

--- plugins/azure/applicationGateway/agSslPolicy.js
+++ plugins/azure/applicationGateway/agSslPolicy.js
@@ -37,8 +37,8 @@ module.exports = {
                 if (!appGateway.id) continue;
 
                 const sslPolicy = appGateway.sslPolicy? appGateway.sslPolicy : '';
-                if ((sslPolicy.policyType == 'Predefined' && sslPolicy.policyName && recommendedSSLPolicies.indexOf(sslPolicy.policyName) > -1) ||
-                    (sslPolicy.policyType == 'Custom' && sslPolicy.minProtocolVersion && sslPolicy.minProtocolVersion.toLowerCase() == 'tlsv1_2')) {
+                if (sslPolicy.policyType && ((sslPolicy.policyType == 'Predefined' && sslPolicy.policyName && recommendedSSLPolicies.indexOf(sslPolicy.policyName) > -1) ||
+                    (sslPolicy.policyType == 'Custom' && sslPolicy.minProtocolVersion && sslPolicy.minProtocolVersion.toLowerCase() == 'tlsv1_2'))) {
                     helpers.addResult(results, 0, 'Application Gateway is using SSL policy which supports TLSV1_2', location, appGateway.id);
                 } else {
                     helpers.addResult(results, 2, 'Application Gateway is using SSL policy which does not support TLSV1_2', location, appGateway.id);
