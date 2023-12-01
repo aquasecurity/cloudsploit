20ffd4920d368d62174c62e139c8b3dc3d4254a6
fatima99s <fatimasarfraz15@gmail.com>
2023-12-01 12:10:09 +0500

    Plugin/AWS Bedrock Model in VPC

--- exports.js
+++ exports.js
@@ -31,6 +31,7 @@ module.exports = {
         'appmeshVGHealthChecks'         : require(__dirname + '/plugins/aws/appmesh/appmeshVGHealthChecks.js'),
 
         'customModelEncrypted'          :require(__dirname + '/plugins/aws/bedrock/customModelEncryptionEnabled.js'),
+        'customModelInVpc'              :require(__dirname + '/plugins/aws/bedrock/customModelInVpc.js'),
         
 
         'asgMultiAz'                    : require(__dirname + '/plugins/aws/autoscaling/asgMultiAz.js'),
