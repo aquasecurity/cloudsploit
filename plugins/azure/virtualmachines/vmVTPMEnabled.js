1abe3beb12ad59db39a241ef7e7eea16bd2ed461
muzzamil <140418718+muzzamilinovaqo@users.noreply.github.com>
2023-12-04 22:46:09 +0500

    Update vmVTPMEnabled.js

--- plugins/azure/virtualmachines/vmVTPMEnabled.js
+++ plugins/azure/virtualmachines/vmVTPMEnabled.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Compute',
     description: 'Ensure Virtual Trusted Platform Module (vTPM) is enabled for Azure virtual machines (VM).',
     more_info: 'vTPM is TPM2.0 compliant and enhances security by validating VM boot integrity and providing a secure storage mechanism for keys and secrets.',
-    recommended_action: 'Enable vTPM for Azure virtual machines to leverage advanced security features and support Guest Attestation in Azure Security Center.',
+    recommended_action: 'Enable vTPM for Azure virtual machines.',
     link: 'https://learn.microsoft.com/en-us/azure/confidential-computing/virtual-tpms-in-azure-confidential-vm',
     apis: ['virtualMachines:listAll'],
 
