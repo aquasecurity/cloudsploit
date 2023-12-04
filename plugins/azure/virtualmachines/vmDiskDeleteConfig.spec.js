9bf4224d80770a422c577725bb28123befda7ece
Muzzammil <muzzamil.hussain@inovaqo.com>
2023-12-04 22:39:51 +0500

    updated with recommended changes

--- plugins/azure/virtualmachines/vmDiskDeleteConfig.spec.js
+++ plugins/azure/virtualmachines/vmDiskDeleteConfig.spec.js
@@ -56,7 +56,7 @@ describe('autoDeleteDisks', function() {
             autoDeleteDisks.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(3);
-                expect(results[0].message).to.include('Unable to query for virtual machines');
+                expect(results[0].message).to.include('Unable to query for Virtual Machines');
                 expect(results[0].region).to.equal('eastus');
                 done();
             });
