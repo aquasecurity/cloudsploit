86dad4cd48b75bf84390b16693e32547f68a7fcb
Muzzammil <muzzamil.hussain@inovaqo.com>
2023-12-04 17:01:45 +0500

    updated with recommended changes

--- plugins/azure/sqldatabases/dbLedgerDigestStorageEnabled.spec.js
+++ plugins/azure/sqldatabases/dbLedgerDigestStorageEnabled.spec.js
@@ -119,7 +119,7 @@ describe('enableAutomaticLedgerDigestStorage', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
-                expect(results[0].message).to.include('Automatic Ledger digest storage is disabled for SQL database');
+                expect(results[0].message).to.include('Automatic Ledger digest storage is not enabled for SQL database');
                 expect(results[0].region).to.equal('eastus');
                 done();
             };
