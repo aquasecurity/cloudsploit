5e69dda47986039e860db72d45a62f45d861c2d1
muzzamil <140418718+muzzamilinovaqo@users.noreply.github.com>
2023-12-04 20:46:06 +0500

    Update plugins/azure/virtualmachines/vmSecureBootEnabled.js
    Co-authored-by: mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>

--- plugins/azure/virtualmachines/vmSecureBootEnabled.js
+++ plugins/azure/virtualmachines/vmSecureBootEnabled.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'VM Secure Boot Enabled',
     category: 'Virtual Machines',
     domain: 'Compute',
-    description: 'Ensure Secure Boot is enabled for Azure virtual machines (VM) to protect against boot kits, rootkits, and kernel-level malware.',
+    description: 'Ensure Secure Boot is enabled for Azure virtual machines (VM).',
     more_info: 'Secure Boot helps protect VMs by ensuring that only signed and trusted components are allowed to execute during the boot process.',
     recommended_action: 'Enable Secure Boot for Azure virtual machines to enhance security and protect against advanced threats during the boot process.',
     link: 'https://learn.microsoft.com/en-us/azure/virtual-machines/trusted-launch-portal?tabs=portal%2Cportal3%2Cportal2',
