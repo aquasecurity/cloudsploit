a11ea13a975be0cef705f87e8aed64e92270ad9d
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2023-12-05 20:27:21 +0500

    Update plugins/azure/sqldatabases/dbLedgerEnabled.js

--- plugins/azure/sqldatabases/dbLedgerEnabled.js
+++ plugins/azure/sqldatabases/dbLedgerEnabled.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Databases',
     description: 'Ensure ledger is enabled for SQL databases.',
     more_info: 'Azure ledger helps protect the integrity of data by enabling customers to use cryptographic seals on their data. The database ledger incrementally captures the state of a database as the database evolves over time, while updates occur on ledger tables',
-    recommended_action: 'Enable Azure ledger for all future tables in the SQL database to enhance data integrity.',
+    recommended_action: 'Enable Azure ledger for all SQL databases.',
     link: 'https://learn.microsoft.com/en-us/sql/relational-databases/security/ledger/ledger-overview?view=sql-server-ver16',
     apis: ['servers:listSql', 'databases:listByServer'],
     
