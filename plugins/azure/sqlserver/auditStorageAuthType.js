fc99fb2917ba526556935468972a41a35de820c3
muzzamil <140418718+muzzamilinovaqo@users.noreply.github.com>
2023-12-06 14:23:29 +0500

    Update auditStorageAuthType.js

--- plugins/azure/sqlserver/auditStorageAuthType.js
+++ plugins/azure/sqlserver/auditStorageAuthType.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Auditing Storage Authentication Type',
     category: 'SQL Server',
     domain: 'Databases',
-    description: 'Ensure managed identity is set as the authentication type when storage account is chosen as the destination for audit logs on SQL server.',
+    description: 'Ensure that managed identity is set as the authentication type when storage account is chosen as the destination for audit logs on SQL server.',
     more_info: 'Enabling managed identity as authentication type enhances security when using a storage account as the destination for audit logs. Managed Identity can be a system-assigned managed identity (SMI) or user-assigned managed identity (UMI).',
     recommended_action: 'Configure managed identity as the authentication type when choosing a storage account as the destination for audit logs on SQL server.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-sql/database/auditing-setup?view=azuresql',
@@ -54,7 +54,7 @@ module.exports = {
                                         helpers.addResult(results, 2, 'Managed identity is not configured as authentication type for audit logs storage on SQL server', location, server.id);
                                     }
                                 } else {
-                                    helpers.addResult(results, 0, 'Auditing storage authentication type is not using account storage for SQL serverr', location, server.id);
+                                    helpers.addResult(results, 0, 'Auditing storage authentication is not using account storage for SQL serverr', location, server.id);
                                 }
                             } else {
                                 helpers.addResult(results, 0, 'Auditing is not enabled for SQL server', location, server.id);
