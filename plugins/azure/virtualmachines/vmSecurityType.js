0ecd6ec8e6c63b009253aefa7b890391edb72dc9
muzzamil <140418718+muzzamilinovaqo@users.noreply.github.com>
2023-12-06 13:30:38 +0500

    Update plugins/azure/virtualmachines/vmSecurityType.js
    Co-authored-by: alphadev4 <113519745+alphadev4@users.noreply.github.com>

--- plugins/azure/virtualmachines/vmSecurityType.js
+++ plugins/azure/virtualmachines/vmSecurityType.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'VM Security Type',
     category: 'Virtual Machines',
     domain: 'Compute',
-    description: 'Ensure Trusted Launch is selected for Azure virtual machines (VM).',
+    description: 'Ensures that Trusted Launch is selected for Azure virtual machines.',
     more_info: 'Trusted launch protects against advanced and persistent attack techniques. Trusted launch is composed of several, coordinated infrastructure technologies that can be enabled independently. Each technology provides another layer of defense against sophisticated threats.',
     recommended_action: 'Select Trusted Launch as security type for Azure virtual machines.',
     link: 'https://learn.microsoft.com/en-us/azure/virtual-machines/trusted-launch-portal?tabs=portal%2Cportal3%2Cportal2',
