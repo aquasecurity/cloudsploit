145739ed109ef471482e5ac69caf0e4eb0576d9c
alphadev4 <devalpha4@gmail.com>
2023-12-26 15:06:39 +0500

    Azure/ACR-Content-Trust

---
+++ plugins/azure/containerregistry/acrContentTrustEnabled.js
@@ -0,0 +1,61 @@
+var async = require('async');
+var helpers = require('../../../helpers/azure/');
+
+module.exports = {
+    title: 'ACR Content Trust Enabled',
+    category: 'Container Registry',
+    domain: 'Containers',
+    description: 'Ensure that content trust is enabled for Azure premium container registries.',
+    more_info: 'Content trust allows you to sign the images you push to your registry. Consumers of your images (people or systems pulling images from your registry) can configure their clients to pull only signed images.',
+    recommended_action: 'Modify your container registry and enable content trust.',
+    link: 'https://learn.microsoft.com/en-us/azure/container-registry/container-registry-content-trust#enable-registry-content-trust',
+    apis: ['registries:list'],
+
+    run: function(cache, settings, callback) {
+        var results = [];
+        var source = {};
+        var locations = helpers.locations(settings.govcloud);
+
+        async.each(locations.registries, function(location, rcb) {
+
+            var registries = helpers.addSource(cache, source,
+                ['registries', 'list', location]);
+
+            if (!registries) return rcb();
+
+            if (registries.err || !registries.data) {
+                helpers.addResult(results, 3,
+                    'Unable to query for container registries: ' + helpers.addError(registries), location);
+                return rcb();
+            }
+
+            if (!registries.data.length) {
+                helpers.addResult(results, 0, 'No existing container registries found', location);
+                return rcb();
+            }
+
+            var found = false;
+            for (let registry of registries.data) {
+                if (!registry.id || (registry.sku && registry.sku.tier && registry.sku.tier!= 'Premium')) continue;
+
+                found = true;
+                var trustPolicy = registry.policies && registry.policies.trustPolicy? registry.policies.trustPolicy : null;
+
+                if (trustPolicy && trustPolicy.status && trustPolicy.status.toLowerCase() == 'enabled'){
+                    helpers.addResult(results, 0, 'Content trsut is enabled for container registry', location, registry.id);
+                } else {
+                    helpers.addResult(results, 2, 'Content trsut is not enabled for container registry', location, registry.id);
+                } 
+            }
+            
+            if(!found) {
+                helpers.addResult(results, 2, 'No existing container registries found', location);
+            }
+
+            rcb();
+        }, function() {
+            // Global checking goes here
+            callback(null, results, source);
+        });
+    }
+};
---
+++ plugins/azure/containerregistry/acrContentTrustEnabled.spec.js
@@ -0,0 +1,110 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var acrContentTrustEnabled = require('./acrContentTrustEnabled');
+
+registries = [
+    {
+        "id": "/subscriptions/ade0e01e-f9cd-49d3-bba7-d5a5362a3414/resourceGroups/devresourcegroup/providers/Microsoft.ContainerRegistry/registries/testregistry12543",
+        "name": "testregistry12543",
+        "type": "Microsoft.ContainerRegistry/registries",
+        "location": "eastus",
+        "tags": {},
+        "sku": {
+            "name": "Premium",
+            "tier": "Premium"
+        },
+        "policies": {
+            "quarantinePolicy": {
+              "status": "disabled"
+            },
+            "trustPolicy": {
+              "type": "Notary",
+              "status": "disabled"
+            },
+        }
+    },
+    {
+        "id": "/subscriptions/ade0e01e-f9cd-49d3-bba7-d5a5362a3414/resourceGroups/devresourcegroup/providers/Microsoft.ContainerRegistry/registries/testregistry12543",
+        "name": "testregistry12543",
+        "type": "Microsoft.ContainerRegistry/registries",
+        "location": "eastus",
+        "tags": {},
+        "sku": {
+            "name": "Premium",
+            "tier": "Premium"
+        },
+        "policies": {
+            "quarantinePolicy": {
+              "status": "disabled"
+            },
+            "trustPolicy": {
+              "type": "Notary",
+              "status": "enabled"
+            },
+        }
+    }
+
+];
+const createCache = (err, data) => {
+    return {
+        registries: {
+            list: {
+                'eastus': {
+                    err: err,
+                    data: data
+                }
+            }
+        }
+    }
+};
+
+describe('acrContentTrustEnabled', function() {
+    describe('run', function() {
+        it('should give passing result if no container registries', function(done) {
+            const cache = createCache(null, []);
+            acrContentTrustEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('No existing container registries found');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            });
+        });
+
+        it('should give failing result if content trsut is not enabled for container registry', function(done) {
+            const cache = createCache(null,[registries[0]]);
+            acrContentTrustEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('Content trsut is not enabled for container registry');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            });
+
+        });
+
+        it('should give passing result if content trsut is enabled for container registry', function(done) {
+            const cache = createCache(null, [registries[1]]);
+            acrContentTrustEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Content trsut is enabled for container registry');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            });
+        });
+
+        it('should give passing result unable to query container registry', function(done) {
+            const cache = createCache(null, null);
+            acrContentTrustEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                expect(results[0].message).to.include('Unable to query for container registries:');
+                expect(results[0].region).to.equal('eastus');
+                done()
+            });
+
+        });
+
+    })
+});
