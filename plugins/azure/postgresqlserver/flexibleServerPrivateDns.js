2241c2037461c8a81a2dc390ed27b8737a51fc43
Fatima <66124862+fatima99s@users.noreply.github.com>
2023-12-27 15:12:01 +0500

    Update flexibleServerPrivateDns.js

--- plugins/azure/postgresqlserver/flexibleServerPrivateDns.js
+++ plugins/azure/postgresqlserver/flexibleServerPrivateDns.js
@@ -2,7 +2,7 @@ const async = require('async');
 const helpers = require('../../../helpers/azure');
 
 module.exports = {
-    title: 'Private DNS Integrated',
+    title: 'Private DNS Zone Integrated',
     category: 'PostgreSQL Server',
     domain: 'Databases',
     description: 'Ensure that PostgreSQL flexible servers has Private DNS Zone integrated.',
