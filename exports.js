602cc2998b1554b881bab718581f94b3cec1dadf
alphadev4 <devalpha4@gmail.com>
2023-12-28 13:11:59 +0500

    Azure/App-Configuration-Managed-identity

--- exports.js
+++ exports.js
@@ -1040,7 +1040,7 @@ module.exports = {
         'amsManagedIdentityEnabled'     : require(__dirname + '/plugins/azure/mediaServices/amsManagedIdentityEnabled.js'),
         'amsClassicApiDisabled'         : require(__dirname + '/plugins/azure/mediaServices/amsClassicApiDisabled.js'),
     
-        'appConfigManagedIdentity': require(__dirname + '/plugins/azure/appConfigurations/appConfigManagedIdentity.js'),
+        'appConfigManagedIdentity'      : require(__dirname + '/plugins/azure/appConfigurations/appConfigManagedIdentity.js'),
     },
     github: {
         'publicKeysRotated'             : require(__dirname + '/plugins/github/users/publicKeysRotated.js'),
