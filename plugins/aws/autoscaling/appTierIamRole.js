97ffc6ea530e5e8c193e160b98d991a3d135ffcd
fatima99s <fatimasarfraz15@gmail.com>
2023-12-28 12:56:15 +0500

    resolve

--- plugins/aws/autoscaling/appTierIamRole.js
+++ plugins/aws/autoscaling/appTierIamRole.js
@@ -18,7 +18,7 @@ module.exports = {
             default: ''
         }
     },
-    realtime_triggers: ['autoscaling:CreateAutoScalingGroup','autoscaling:DeleteAutoScalingGroup','autoscaling:UpdateAutoScalingGroup','autoscaling:createLaunchConfiguration','autoscaling:DeleteLaunchConfiguration'],
+    realtime_triggers: ['autoscaling:CreateAutoScalingGroup','autoscaling:DeleteAutoScalingGroup','autoscaling:UpdateAutoScalingGroup','autoscaling:CreateLaunchConfiguration','autoscaling:DeleteLaunchConfiguration'],
 
     run: function(cache, settings, callback) {
         var results = [];
