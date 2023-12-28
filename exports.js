24c445b191ab0e0bab2d46d151220d9a506f5126
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2023-12-28 13:11:24 +0500

    Update exports.js

--- exports.js
+++ exports.js
@@ -1040,7 +1040,7 @@ module.exports = {
         'amsManagedIdentityEnabled'     : require(__dirname + '/plugins/azure/mediaServices/amsManagedIdentityEnabled.js'),
         'amsClassicApiDisabled'         : require(__dirname + '/plugins/azure/mediaServices/amsClassicApiDisabled.js'),
     
-        'appConfigurationManagedIdentity': require(__dirname + '/plugins/azure/appConfigurations/appConfigurationManagedIdentity.js'),
+        'appConfigManagedIdentity': require(__dirname + '/plugins/azure/appConfigurations/appConfigManagedIdentity.js'),
     },
     github: {
         'publicKeysRotated'             : require(__dirname + '/plugins/github/users/publicKeysRotated.js'),
