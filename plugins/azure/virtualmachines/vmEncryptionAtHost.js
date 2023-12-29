c1bff237cd888c399860eeb880a0263ee1de7210
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2023-12-29 14:08:58 +0500

    Update plugins/azure/virtualmachines/vmEncryptionAtHost.js

--- plugins/azure/virtualmachines/vmEncryptionAtHost.js
+++ plugins/azure/virtualmachines/vmEncryptionAtHost.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'Virtual Machines',
     domain: 'Compute',
     description: 'Ensures that encryption at host is enabled for Azure Virtual Machine disks.',
-    more_info: 'The data for your temporary disk and OS/data disk caches is stored on the VM host, enabling encyrption at host for Azure Virtual Machine disks allows that data to be end-to-end encrypted, ensuring compliance and bolstering overall security with Azure Disk Encryption.',
+    more_info: 'The data for temporary disk and OS/data disk caches is stored on the VM host. Enabling encryption at host for Azure Virtual Machine disks allows the data to be end-to-end encrypted, ensuring compliance and bolstering overall security with Azure Disk Encryption.',
     recommended_action: 'Ensure that all Azure Virtual Machines have encryption at host enabled for disks.',
     link: 'https://learn.microsoft.com/en-us/azure/virtual-machines/disk-encryption#encryption-at-host---end-to-end-encryption-for-your-vm-data',
     apis: ['virtualMachines:listAll'],
