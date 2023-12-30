e3820977831ba5f4d0546696721ed7238434f0d2
abdullahaslam306 <f178135@nu.edu.pk>
2023-12-30 14:10:08 +0500

    pr issues resolved

--- plugins/azure/virtualmachinescaleset/vmssManagedIdentityEnabled.js
+++ plugins/azure/virtualmachinescaleset/vmssManagedIdentityEnabled.js
@@ -5,10 +5,10 @@ module.exports = {
     title: 'VM Scale Set Managed Identity Enabled',
     category: 'Virtual Machines',
     domain: 'Compute',
-    description: 'Ensures that Azure Virtual Machine scale sets have managed identity enabled.',
-    more_info: 'Managed identities for Azure resources provide Azure services with an automatically managed identity in Microsoft Entra ID. You can use this identity to authenticate to any service that supports Microsoft Entra authentication, without having credentials in your code.',
+    description: 'Ensures that Azure Virtual Machine Scale Sets have managed identity enabled.',
+    more_info: 'Enabling managed identities eliminate the need for developers having to manage credentials by providing an identity for the Azure resource in Azure AD and using it to obtain Azure Active Directory (Azure AD) tokens.',
     link: 'https://learn.microsoft.com/en-us/entra/identity/managed-identities-azure-resources/qs-configure-portal-windows-vmss',
-    recommended_action: 'Modify VM scale set and enable user or system assigned identities.',
+    recommended_action: 'Modify VM Scale Set and enable managed identity.',
     apis: ['virtualMachineScaleSets:listAll'],
 
     run: function(cache, settings, callback) {
