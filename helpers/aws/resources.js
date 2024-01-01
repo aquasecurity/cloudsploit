858a08a5428cf2a21ac2184dc694e2885498b579
alphadev4 <devalpha4@gmail.com>
2024-01-01 23:09:59 +0500

    empty commit

--- helpers/aws/resources.js
+++ helpers/aws/resources.js
@@ -1,6 +1,7 @@
 // This file contains a list of ARN paths for each API call type
 // that are used to extract ARNs for resources
 
+
 module.exports = {
     acm: {
         listCertificates: 'CertificateArn',
