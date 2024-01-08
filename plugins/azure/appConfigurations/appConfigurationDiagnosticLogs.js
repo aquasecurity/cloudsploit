44ec5f0bfc059b2f9d9a80ff0b282389497861c0
alphadev4 <devalpha4@gmail.com>
2024-01-08 17:15:48 +0500

    SAAS-21769/Az-realtime-triggers-a-b-c-d

--- plugins/azure/appConfigurations/appConfigurationDiagnosticLogs.js
+++ plugins/azure/appConfigurations/appConfigurationDiagnosticLogs.js
@@ -11,7 +11,6 @@ module.exports = {
     link: 'https://learn.microsoft.com/en-us/azure/azure-app-configuration/monitor-app-configuration?tabs=portal#monitoringdata',
     apis: ['appConfigurations:list','diagnosticSettings:listByAppConfigurations'],
     realtime_triggers: ['microsoftappconfiguration:configurationstores:write','microsoftinsights:diagnosticsettings:write','microsoftinsights:diagnosticsettings:delete','microsoftappconfiguration:configurationstores:delete'],
-
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
