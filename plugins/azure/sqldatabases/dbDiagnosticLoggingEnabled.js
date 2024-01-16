1be483116b59822d2c63f7ce861d3239ed699536
Muzzammil <muzzamil.hussain@inovaqo.com>
2024-01-16 15:31:05 +0500

    updated plugin code struct...

--- plugins/azure/sqldatabases/dbDiagnosticLoggingEnabled.js
+++ plugins/azure/sqldatabases/dbDiagnosticLoggingEnabled.js
@@ -7,15 +7,16 @@ module.exports = {
     domain: 'Databases',
     description: 'Ensures diagnostic logging is enabled for SQL databases.',
     more_info: 'Enabling diagnostic logging provides valuable insights into SQL database that helps to monitor resources for their availability, performance, and operation.',
-    recommended_action: 'Enable diagnostic logging for SQL databases with the minimum required data recording settings: SQLInsights, ErrorsTimeouts, BlocksDeadlocks, BasicInstanceAndApp, AdvancedWorkloadManagement.',
+    recommended_action: 'Enable diagnostic logging for SQL databases with the minimum required data recording settings: SQLInsights, Errors, Timeouts, Blocks, Deadlocks, Basic, InstanceAndAppAdvanced, WorkloadManagement.',
     link: 'https://learn.microsoft.com/en-us/azure/azure-sql/database/monitoring-sql-database-azure-monitor?view=azuresql',
     apis: ['servers:listSql', 'databases:listByServer', 'diagnosticSettings:listByDatabase'],
     settings: {
-        database_diagnostic_settings: {
-            name: 'Database Diagnostic Logs/Metrics settings',
-            description: 'Desired diagnostic logging provides valuable insights into SQL database that helps to monitor resources for their availability, performance, and operation.',
-            regex: '/^(Basic|InstanceAndAppAdvanced|WorkloadManagement|SQLInsights|Errors|Timeouts|Blocks|Deadlocks|allLogs|audit)(,s*(Basic|InstanceAndAppAdvanced|WorkloadManagement|SQLInsights|Errors|Timeouts|Blocks|Deadlocks|allLogs|audit))*$/i',  
-            default: 'basic,InstanceAndAppAdvanced,WorkloadManagement,audit'
+        diagnostic_logs: {
+            name: 'Diagnostic Logs Enabled',
+            description: 'Comma separated list of diagnostic logs/metrics that should be enabled at minimum i.e. SQLInsights, AutomaticTuning, InstanceAndAppAdvanced etc. If you have enabled allLogs, then resource produces pass result. If you only want to check if logging is enabled or not, irrespecitve of log type, then add * in setting.',
+            regex: '^.*$',
+            default: 'SQLInsights, Errors, Timeouts, Blocks, Deadlocks, Basic, InstanceAndAppAdvanced, WorkloadManagement'
+
         },
     },
     
@@ -23,7 +24,7 @@ module.exports = {
         var results = [];
         var source = {};
         var locations = helpers.locations(settings.govcloud);
-        var recommendedDiagnosticSettings = settings.database_diagnostic_settings || this.settings.database_diagnostic_settings.default;
+        var config = settings.diagnostic_logs || this.settings.diagnostic_logs.default;
 
         async.each(locations.servers, function(location, rcb) {
             var servers = helpers.addSource(cache, source, ['servers', 'listSql', location]);
@@ -47,44 +48,46 @@ module.exports = {
                 if (!databases || databases.err || !databases.data) {
                     helpers.addResult(results, 3,
                         'Unable to query for SQL server databases: ' + helpers.addError(databases), location, server.id);
+                    return;
+                }
+                if (!databases.data.length) {
+                    helpers.addResult(results, 0,
+                        'No databases found for SQL server', location, server.id);
+                    return;
+                    
                 } else {
-                    if (!databases.data.length) {
-                        helpers.addResult(results, 0,
-                            'No databases found for SQL server', location, server.id);
-                    } else {
-                        databases.data.forEach(database=> {
+                    databases.data.forEach(database=> {
                             
-                            var diagnosticSettings = helpers.addSource(cache, source, ['diagnosticSettings', 'listByDatabase', location, database.id]);
+                        var diagnosticSettings = helpers.addSource(cache, source, ['diagnosticSettings', 'listByDatabase', location, database.id]);
                         
-                            if (!diagnosticSettings || diagnosticSettings.err || !diagnosticSettings.data) {
-                                helpers.addResult(results, 3, 'Unable to query SQL database diagnostic settings: ' + helpers.addError(diagnosticSettings), location, database.id);
-                            } else {
-                                if (!diagnosticSettings.data.length) {
-                                    helpers.addResult(results, 2, 'Diagnostic settings are not configured for SQL database', location, database.id);
-                                } else { 
-                                    var enabledDiagnosticLogs = [];
-                                    var enabledDiagnosticMetrics = [];
-                                    var enabledSettings = [];
-                                    diagnosticSettings.data.forEach(settings => { 
-                                        settings.logs.forEach((e) => e.enabled && !enabledDiagnosticLogs.includes(e.category || e.categoryGroup) && enabledDiagnosticLogs.push(e.category || e.categoryGroup));
-                                        settings.metrics.forEach((e) => e.enabled && !enabledDiagnosticMetrics.includes(e.category) && enabledDiagnosticMetrics.push(e.category));
-
-                                    });
-                                    enabledSettings = [...enabledDiagnosticLogs, ...enabledDiagnosticMetrics].toString().toLocaleLowerCase().split(',');
-                                    recommendedDiagnosticSettings = recommendedDiagnosticSettings.toLowerCase().split(',');
-                                    var skippedRecommendedSettings = recommendedDiagnosticSettings.filter((e) => !enabledSettings.includes(e));
-
-                                    if (skippedRecommendedSettings.length) {
-                                        helpers.addResult(results, 2, `Database diagnostic settings are not configured with minimum requirements. Missing: ${skippedRecommendedSettings.join(', ')} `, location, database.id);
-                                    } else {
-                                        helpers.addResult(results, 0,
-                                            'Database diagnostic settings are configured with minimum requirements', location, database.id);
-                                    }
-
-                                }
-                            }
-                        });
-                    }
+                        if (!diagnosticSettings || diagnosticSettings.err || !diagnosticSettings.data) {
+                            helpers.addResult(results, 3, 'Unable to query SQL database diagnostic settings: ' + helpers.addError(diagnosticSettings), location, database.id);
+                            return;
+                            
+                        } 
+                        var found = true;
+                        var missingConfig = [];
+                        if (config == '*') {
+                            found = diagnosticSettings.data.some(ds => ds.logs && ds.logs.length && ds.logs.some(log=>log.enabled) || ds.metrics && ds.metrics.length && ds.metrics.some(metrics=>metrics.enabled));
+                        } else {
+                            config = config.replace(/\s/g, '');
+                            missingConfig = config.toLowerCase().split(',');
+                            diagnosticSettings.data.forEach(settings => {
+                                const logs_metrics = [...settings.logs, ...settings.metrics];
+                                missingConfig = missingConfig.filter(requiredCategory =>
+                                    !logs_metrics.some(log => (log.category && log.category.toLowerCase() === requiredCategory && log.enabled) || log.categoryGroup === 'allLogs' && log.enabled)
+                                );
+                            });
+        
+                        }
+                        if (!missingConfig.length && found) {
+                            helpers.addResult(results, 0, 'SQL database has diagnostic logs enabled', location, database.id);
+        
+                        } else {
+                            helpers.addResult(results, 2, `SQL database does not have diagnostic logs enabled ${missingConfig.length ? `for following: ${missingConfig}` : ''}`, location, database.id);
+                        }
+                        
+                    });
                 }
             });
 
--- plugins/azure/sqldatabases/dbDiagnosticLoggingEnabled.spec.js
+++ plugins/azure/sqldatabases/dbDiagnosticLoggingEnabled.spec.js
@@ -109,7 +109,7 @@ describe('dbDiagnosticLoggingEnabled', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('Database diagnostic settings are configured with minimum requirements');
+                expect(results[0].message).to.include('SQL database has diagnostic logs enabled');
                 expect(results[0].region).to.equal('eastus');
                 done();
             };
@@ -127,7 +127,7 @@ describe('dbDiagnosticLoggingEnabled', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
-                expect(results[0].message).to.include('Diagnostic settings are not configured for SQL database');
+                expect(results[0].message).to.include('SQL database does not have diagnostic logs enabled');
                 expect(results[0].region).to.equal('eastus');
                 done();
             };
@@ -145,7 +145,7 @@ describe('dbDiagnosticLoggingEnabled', function() {
             const callback = (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(2);
-                expect(results[0].message).to.include('Database diagnostic settings are configured with minimum requirements.');
+                expect(results[0].message).to.include('SQL database does not have diagnostic logs enabled');
                 expect(results[0].region).to.equal('eastus');
                 done();
             };
