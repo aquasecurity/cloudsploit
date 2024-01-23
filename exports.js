b83df8116ab76d567d834d0b0c1553406d21970a
alphadev4 <devalpha4@gmail.com>
2024-01-23 22:36:53 +0500

    Azure/Redis-VNet-Integration

--- exports.js
+++ exports.js
@@ -1072,8 +1072,6 @@ module.exports = {
 
         'automationAcctDiagnosticLogs'  : require(__dirname + '/plugins/azure/automationAccounts/automationAcctDiagnosticLogs.js'),
         'automationAcctManagedIdentity' : require(__dirname + '/plugins/azure/automationAccounts/automationAcctManagedIdentity.js'),
-        'automationAcctEncryptedVars'   : require(__dirname + '/plugins/azure/automationAccounts/automationAcctEncryptedVars.js'),
-
 
     },
     github: {
