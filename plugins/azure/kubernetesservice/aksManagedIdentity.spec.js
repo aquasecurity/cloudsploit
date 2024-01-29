d604d93e63432a47d6fa2d57963cfc674b7a91a8
mehakseedat63 <87388442+mehakseedat63@users.noreply.github.com>
2024-01-30 01:17:24 +0500

    Update plugins/azure/kubernetesservice/aksManagedIdentity.spec.js

--- plugins/azure/kubernetesservice/aksManagedIdentity.spec.js
+++ plugins/azure/kubernetesservice/aksManagedIdentity.spec.js
@@ -93,7 +93,7 @@ describe('aksManagedIdentity', function() {
             aksManagedIdentity.run(cache, {}, (err, results) => {
                 expect(results.length).to.equal(1);
                 expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('AKS cluster has managed identity enabled'');
+                expect(results[0].message).to.include('AKS cluster has managed identity enabled');
                 expect(results[0].region).to.equal('eastus');
                 done();
             });
