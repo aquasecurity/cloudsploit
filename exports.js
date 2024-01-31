376862e6c76da7b25550ef745680ba886fd84069
mehakseedat63 <mehakseedat63@gmail.com>
2024-01-31 19:55:15 +0500

    GCP - Vertex AI Dataset Encryption Plugin

--- exports.js
+++ exports.js
@@ -1477,6 +1477,8 @@ module.exports = {
         'environmentEncryption'         : require(__dirname + '/plugins/google/composer/environmentEncryption.js'),
         'envDefaultServiceAccount'      : require(__dirname + '/plugins/google/composer/envDefaultServiceAccount.js'),
         'webserverPublicAccess'         : require(__dirname + '/plugins/google/composer/webserverPublicAccess.js'),
+
+        'vertexAIDatasetEncryption'     : require(__dirname + '/plugins/google/vertexai/vertexAIDatasetEncryption.js'),
     },
     alibaba: {
         'passwordMinLength'             : require(__dirname + '/plugins/alibaba/ram/passwordMinLength.js'),
