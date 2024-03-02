e4321d35ad6b198425171073e7386a5136af4d56
alphadev4 <devalpha4@gmail.com>
2024-03-02 23:39:08 +0500

    Azure/App-Configuration-Tags

---
+++ plugins/azure/appConfigurations/appConfigHasTags.spec.js
@@ -0,0 +1,79 @@
+var expect = require('chai').expect;
+var appConfigurationPublicAccess = require('./appConfigurationPublicAccess.js');
+
+const appConfigurations = [
+    {
+        "type": "Microsoft.AppConfiguration/configurationStores",
+        "location": "eastus",
+        "provisioningState": "Succeeded",
+        "tags": { "key": "value" },
+    },
+    {
+        "type": "Microsoft.AppConfiguration/configurationStores",
+        "location": "eastus",
+        "provisioningState": "Succeeded",
+        "tags": {},
+    }
+];
+
+const createCache = (appConfigurations,err) => {
+    return {
+        appConfigurations: {
+            list: {
+                'eastus': {
+                    data: appConfigurations,
+                    err: err
+                }
+            }
+        }
+    }
+};
+
+describe('appConfigurationPublicAccess', function () {
+    describe('run', function () {
+
+        it('should give pass result if No existing app configurations found', function (done) {
+            const cache = createCache([]);
+            appConfigurationPublicAccess.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('No existing App Configurations found');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give unknown result if Unable to query app configurations:', function (done) {
+            const cache = createCache(null, 'Error');
+            appConfigurationPublicAccess.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                expect(results[0].message).to.include('Unable to query App Configuration:');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give passing result if App Configuration has tags associated', function (done) {
+            const cache = createCache([appConfigurations[1]]);
+            appConfigurationPublicAccess.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('App Configuration has tags associated');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give failing result if App Configuration does not have tags associated', function (done) {
+            const cache = createCache([appConfigurations[0]]);
+            appConfigurationPublicAccess.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('App Configuration does not have tags associated');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+    });
+});
