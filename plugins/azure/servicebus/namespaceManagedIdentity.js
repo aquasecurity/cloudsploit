b849aeb68b43ff20a60e6a77954a899e1c22bee6
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2024-03-04 17:40:25 +0500

    Update plugins/azure/servicebus/namespaceManagedIdentity.js

--- plugins/azure/servicebus/namespaceManagedIdentity.js
+++ plugins/azure/servicebus/namespaceManagedIdentity.js
@@ -8,7 +8,7 @@ module.exports = {
     severity: 'Medium',
     description: 'Ensure that Azure Service Bus namespaces have managed identity enabled.',
     more_info: 'Enabling managed identities eliminate the need for developers having to manage credentials by providing an identity for the Azure resource in Azure AD and using it to obtain Azure Active Directory (Azure AD) tokens.',
-    recommended_action: 'Modify Service Bus namespace and enable managed identity..',
+    recommended_action: 'Modify Service Bus namespace and enable managed identity.',
     link: 'https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-managed-service-identity',
     apis: ['serviceBus:listNamespacesBySubscription'],
     realtime_triggers: ['microsoftservicebus:namespaces:write','microsoftservicebus:namespaces:delete'],
