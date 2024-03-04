91c16e705431163674ebd1b2876a669adb6c8144
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2024-03-05 02:14:08 +0500

    Update plugins/azure/keyvaults/keyVaultsPrivateEndpoint.js

--- plugins/azure/keyvaults/keyVaultsPrivateEndpoint.js
+++ plugins/azure/keyvaults/keyVaultsPrivateEndpoint.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'Application Integration',
     severity: 'High',
     description: 'Ensure that Azure Key vaults have private endpoints configured.',
-    more_info: 'Configuring private link ensures connection of virtual networks to Azure services without a public IP address at the source or destination. Private endpoints minimize the risk of public internet exposure and protect against external attacks.',
+    more_info: 'Configuring private link endpoints ensures secure network traffic from Azure Key Vault to the resources requesting secrets and keys. Using Private endpoints minimize the risk of public internet exposure and protect against external attacks.',
     recommended_action: 'Ensure that private endpoints are configured properly and public network access is disabled for Key Vaults.',
     link: 'https://learn.microsoft.com/en-us/azure/key-vault/general/private-link-service',
     apis: ['vaults:list'],
