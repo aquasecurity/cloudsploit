d6a2eff227310c2136a25d8cbe6606bcfaf10dc9
alphadev4 <devalpha4@gmail.com>
2024-03-25 16:10:05 +0500

    Azure-Databricks-Workspace-secure-cluster

--- exports.js
+++ exports.js
@@ -1135,7 +1135,7 @@ module.exports = {
         'accountPublicAccessDisabled'   : require(__dirname + '/plugins/azure/openai/accountPublicAccessDisabled.js'),
 
         'workspaceDbfsInfraEncryption'  : require(__dirname + '/plugins/azure/databricks/workspaceDbfsInfraEncryption.js'),
-
+        'workspaceSecureCluster'        : require(__dirname + '/plugins/azure/databricks/workspaceSecureCluster.js'),
 
     },
     github: {
