518527ef36a331f9618d02ed1375b00eb8bdb012
muzzamilinovaqo <muzzamil.hussain@inovaqo.com>
2024-03-26 16:29:32 +0500

    workspaces operational state

--- plugins/aws/workspaces/workSpacesOperationalState.js
+++ plugins/aws/workspaces/workSpacesOperationalState.js
@@ -8,15 +8,19 @@ module.exports = {
     severity: 'Medium',
     description: 'Ensure that the AWS WorkSpaces instances are healthy.',
     more_info: 'The AWS WorkSpaces service periodically sends status requests to the WorkSpaces instances. An instance is pronounced unhealthy when it fails to respond to these HealthCheck requests.',
-    recommended_action: '',
+    recommended_action: 'Troubleshoot the workspace when state is unhealthy.',
     link: 'https://docs.aws.amazon.com/workspaces/latest/adminguide/workspace-maintenance.html',
     apis: ['WorkSpaces:describeWorkspaces'],
-    realtime_triggers: ['workspaces:CreateWorkspaces'],
+    realtime_triggers: ['workspaces:CreateWorkspaces','workspaces:DeleteWorkspaces'],
 
     run: function(cache, settings, callback) {
         var results = [];
         var source = {};
         var regions = helpers.regions(settings);
+        var acctRegion = helpers.defaultRegion(settings);
+        var awsOrGov = helpers.defaultPartition(settings);
+
+        var accountId = helpers.addSource(cache, source, ['sts', 'getCallerIdentity', acctRegion, 'data']);
 
         async.each(regions.workspaces, function(region, rcb){
             var listWorkspaces = helpers.addSource(cache, source, ['workspaces', 'describeWorkspaces', region]);
@@ -36,7 +40,19 @@ module.exports = {
                     'No WorkSpaces instances found', region);
                 return rcb();
             }
+            listWorkspaces.data.forEach(workspace => {
+                if (!workspace.WorkspaceId) return;
 
+                let resource = `arn:${awsOrGov}:region:${region}:${accountId}:worskpace/${workspace.WorkspaceId}`;
+
+                if (workspace.State === 'UNHEALTHY') {
+                    helpers.addResult(results, 2,
+                        'The workspace is not operational', region, resource);
+                } else {
+                    helpers.addResult(results, 0,
+                        'WorkSpace is operational', region, resource);
+                }
+            });
 
             rcb();
         }, function() {
