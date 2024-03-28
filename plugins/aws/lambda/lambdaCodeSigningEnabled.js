882abf5bc542dc8ca104f9bf697566e5f3f5a9d1
muzzamil <140418718+muzzamilinovaqo@users.noreply.github.com>
2024-03-28 14:02:11 +0500

    Update plugins/aws/lambda/lambdaCodeSigningEnabled.js
    Co-authored-by: Fatima <66124862+fatima99s@users.noreply.github.com>

--- plugins/aws/lambda/lambdaCodeSigningEnabled.js
+++ plugins/aws/lambda/lambdaCodeSigningEnabled.js
@@ -45,7 +45,7 @@ module.exports = {
                 if (codeSigningConfig && codeSigningConfig.data && codeSigningConfig.data.CodeSigningConfigArn) {
                     helpers.addResult(results, 0, ' Lambda Function has code signing config enabled ', region,resource);
                 } else {
-                    helpers.addResult(results, 2, 'Code signing config is not enabled for Lambda Function', region,resource);
+                    helpers.addResult(results, 2, 'Lambda Function does not have code signing config enabled', region,resource);
                 }
             }
             
