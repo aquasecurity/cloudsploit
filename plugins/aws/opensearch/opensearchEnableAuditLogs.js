c9cdfec8b687f73aebd2b4d5a854eb46e368f278
muzzamilinovaqo <muzzamil.hussain@inovaqo.com>
2024-03-28 13:27:10 +0500

    opensearch audit logs

--- plugins/aws/opensearch/opensearchEnableAuditLogs.js
+++ plugins/aws/opensearch/opensearchEnableAuditLogs.js
@@ -5,12 +5,12 @@ module.exports = {
     title: 'OpenSearch Enable Audit Logs',
     category: 'OpenSearch',
     domain: 'Databases',
-    severity: 'High',
+    severity: 'Medium',
     description: 'Ensures the Audit Logs feature is enabled for all the Amazon OpenSearch domains',
     more_info: 'The Audit Logs feature allows you to log all user activity on your Amazon OpenSearch domains (clusters), including failed login attempts, and which users accessed certain indices, documents, or fields. ',
-    link: 'https://docs.aws.amazon.com/opensearch-service/latest/developerguide/encryption-at-rest.html',
-    recommended_action: 'Ensure encryption-at-rest is enabled for all OpenSearch domains.',
-    apis: ['ES:listDomainNames', 'ES:describeElasticsearchDomain'],
+    link: 'https://docs.aws.amazon.com/opensearch-service/latest/developerguide/audit-logs.html',
+    recommended_action: 'Modify Opensearch domain and enable audit logs.',
+    apis: ['OpenSearch:listDomainNames', 'OpenSearch:describeDomain'],
     realtime_triggers: ['opensearch:CreateDomain', 'opensearch:UpdateDomainConfig', 'opensearch:DeleteDomain'],
 
     run: function(cache, settings, callback) {
@@ -20,7 +20,7 @@ module.exports = {
 
         async.each(regions.es, function(region, rcb) {
             var listDomainNames = helpers.addSource(cache, source,
-                ['es', 'listDomainNames', region]);
+                ['opensearch', 'listDomainNames', region]);
 
             if (!listDomainNames) return rcb();
 
@@ -38,7 +38,7 @@ module.exports = {
 
             listDomainNames.data.forEach(function(domain){
                 var describeDomain = helpers.addSource(cache, source,
-                    ['es', 'describeElasticsearchDomain', region, domain.DomainName]);
+                    ['opensearch', 'describeDomain', region, domain.DomainName]);
                 if (!describeDomain ||
                     describeDomain.err ||
                     !describeDomain.data ||
@@ -64,69 +64,6 @@ module.exports = {
             rcb();
         }, function() {
             callback(null, results, source);
-        });
-    },
-    remediate: function(config, cache, settings, resource, callback) {
-        var putCall = this.actions.remediate;
-        var pluginName = 'opensearchEncryptedDomain';
-        let defaultKeyDesc = 'Default master key that protects my OpenSearch data when no other key is defined';
-        var domainNameArr = resource.split(':');
-        var domain = domainNameArr[domainNameArr.length - 1].split('/')[1];
-
-        // find the location of the domain needing to be remediated
-        var domainLocation = domainNameArr[3];
-
-        // add the location of the domain to the config
-        config.region = domainLocation;
-
-        var params = {};
-
-        // create the params necessary for the remediation
-        if (settings.input &&
-            settings.input.kmsKeyIdForES) {
-            params = {
-                DomainName: domain,
-                EncryptionAtRestOptions: {
-                    'Enabled': true,
-                    'KmsKeyId': settings.input.kmsKeyIdForES
-                },
-            };
-        } else {
-            let defaultKmsKeyId = helpers.getDefaultKeyId(cache, config.region, defaultKeyDesc);
-            if (!defaultKmsKeyId) return callback(`No default OpenSearch key for the region ${config.region}`);
-            params = {
-                DomainName: domain,
-                EncryptionAtRestOptions: {
-                    'Enabled': true,
-                    'KmsKeyId': defaultKmsKeyId
-                },
-            };
-        }
-
-        var remediation_file = settings.remediation_file;
-        remediation_file['pre_remediate']['actions'][pluginName][resource] = {
-            'Encryption': 'Disabled',
-            'OpenSearch': resource
-        };
-
-        // passes the config, put call, and params to the remediate helper function
-        helpers.remediatePlugin(config, putCall[0], params, function(err) {
-            if (err) {
-                remediation_file['remediate']['actions'][pluginName]['error'] = err;
-                return callback(err, null);
-            }
-
-            let action = params;
-            action.action = putCall;
-
-            remediation_file['post_remediate']['actions'][pluginName][resource] = action;
-            remediation_file['remediate']['actions'][pluginName][resource] = {
-                'Action': 'ENCRYPTED',
-                'OpenSearch': domain
-            };
-
-            settings.remediation_file = remediation_file;
-            return callback(null, action);
         });
     }
 };
