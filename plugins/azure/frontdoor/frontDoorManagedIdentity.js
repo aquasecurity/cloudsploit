21dc1ce6984dd10a5b3714421f7ef9e6e9e8bb31
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2024-05-19 20:35:46 +0500

    Update plugins/azure/frontdoor/frontDoorManagedIdentity.js

--- plugins/azure/frontdoor/frontDoorManagedIdentity.js
+++ plugins/azure/frontdoor/frontDoorManagedIdentity.js
@@ -6,7 +6,7 @@ module.exports = {
     category: 'Front Door',
     domain: 'Content Delivery',
     severity: 'Medium',
-    description: 'Ensures that Azure Front Door Standard and Premium profile have managed identity enabled.',
+    description: 'Ensures that Azure Front Door Standard and Premium profiles have managed identity enabled.',
     more_info: 'Enabling managed identities eliminate the need for developers having to manage credentials by providing an identity for the Azure resource in Azure AD and using it to obtain Azure Active Directory (Azure AD) tokens.',
     recommended_action: 'Modify the Front Door Standard and Premium profile and add managed identity.',
     link: 'https://learn.microsoft.com/en-us/azure/frontdoor/managed-identity',
