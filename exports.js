b111cda9f835b8718e7bdf47e67a521776521f01
fatima99s <fatimasarfraz15@gmail.com>
2024-06-13 03:35:05 +0500

    F/Azure-mysqlServerManagedIdentity

--- exports.js
+++ exports.js
@@ -855,6 +855,7 @@ module.exports = {
         'enforceMySQLSSLConnection'     : require(__dirname + '/plugins/azure/mysqlserver/enforceMySQLSSLConnection.js'),
         'mysqlFlexibleServersMinTls'    : require(__dirname + '/plugins/azure/mysqlserver/mysqlFlexibleServersMinTls.js'),
         'mysqlServerHasTags'            : require(__dirname + '/plugins/azure/mysqlserver/mysqlServerHasTags.js'),
+        'mysqlServerManagedIdentity'    : require(__dirname + '/plugins/azure/mysqlserver/mysqlServerManagedIdentity.js'),
 
         'logRetentionDays'              : require(__dirname + '/plugins/azure/postgresqlserver/logRetentionDays.js'),
         'connectionThrottlingEnabled'   : require(__dirname + '/plugins/azure/postgresqlserver/connectionThrottlingEnabled.js'),
