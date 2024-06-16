88d31063b626c9af86e0de96fe944262a91d557e
alphadev4 <113519745+alphadev4@users.noreply.github.com>
2024-06-16 15:46:27 +0500

    Update plugins/azure/batchAccounts/batchAccountsAADEnabled.js

--- plugins/azure/batchAccounts/batchAccountsAADEnabled.js
+++ plugins/azure/batchAccounts/batchAccountsAADEnabled.js
@@ -8,7 +8,7 @@ module.exports = {
     severity: 'Medium',
     description: 'Ensures that Batch account has Azure Active Directory (AAD) authentication mode enabled.',
     more_info: 'Enabling Azure Active Directory (AAD) authentication for Batch account ensures enhanced security by restricting the service API authentication to Microsoft Entra ID that prevents access through less secure shared key methods, thereby safeguarding batch resources from unauthorized access.',
-    recommended_action: 'Enable diagnostic logging for all Batch accounts.',
+    recommended_action: 'Enable Active Directory (AD) authentication mode for all Batch accounts.',
     link: 'https://learn.microsoft.com/en-us/azure/batch/batch-aad-auth',
     apis: ['batchAccounts:list'],
     realtime_triggers: ['microsoftbatch:batchaccounts:write', 'microsoftbatch:batchaccounts:delete'],
