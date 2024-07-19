8e5d54c09cbcc7572ab0425b1ba6d59e0249ef19
Gio Rodriguez <gioroddev@gmail.com>
2024-07-19 09:45:43 -0400

    Update modelInvocationLoggingEnabled.js

--- plugins/aws/bedrock/modelInvocationLoggingEnabled.js
+++ plugins/aws/bedrock/modelInvocationLoggingEnabled.js
@@ -9,7 +9,7 @@ module.exports = {
     description: 'Ensure that Amazon Bedrock model invocation logging is enabled.',
     more_info: 'With invocation logging enabled, you can collect the full request data, response data, and metadata associated with all calls performed in account. This detailed logging provides valuable insights into model usage patterns, helps in troubleshooting, and enhances security by allowing for thorough analysis of model interactions. It also facilitates compliance with auditing requirements, offering a comprehensive record of model invocations.',
     recommended_action: 'Enable invocation logging for Amazon Bedrock models.',
-    link: 'https://docs.aws.amazon.com/bedrock/latest/userguide/settings.html#model-invocation-logging',
+    link: 'https://docs.aws.amazon.com/bedrock/latest/userguide/model-invocation-logging.html',
     apis: ['Bedrock:getModelInvocationLoggingConfiguration'],
 
     run: function(cache, settings, callback) {
