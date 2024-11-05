3a927f12f85e3c264deb63d2c595e3ddc33b6a51
AkhtarAmir <AkhtarAmir>
2024-11-05 05:36:00 -0700

    disk byok file bring back

---
+++ plugins/azure/virtualmachines/unAttachedDiskByokEncryptionEnabled.spec.js
@@ -0,0 +1,120 @@
+var expect = require('chai').expect;
+var unAttachedDiskByokEncryptionEnabled = require('./UnAttachedDiskByokEncryptionEnabled');
+
+const disks = [
+    {
+        'name': 'test',
+        'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Compute/disks/test',
+        'type': 'Microsoft.Compute/disks',
+        'location': 'eastus',
+        'diskState': 'Unattached',
+        'encryption': {
+            'type': 'EncryptionAtRestWithPlatformKey'
+        }
+    },
+    {
+        'name': 'test',
+        'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Compute/disks/test',
+        'type': 'Microsoft.Compute/daisks',
+        'location': 'eastus',
+        'diskState': 'Unattached',
+        'encryption': {
+            'type': 'EncryptionAtRestWithCustomerKey',
+            'diskEncryptionSetId': '/subscriptions/123/resourceGroups/AQUA-RESOURCE-GROUP/providers/Microsoft.Compute/diskEncryptionSets/test-encrypt-set'
+        }
+    },
+    {
+        'name': 'test',
+        'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Compute/disks/test',
+        'type': 'Microsoft.Compute/disks',
+        'location': 'eastus',
+        'diskState': 'Unattached',
+        'encryption': {
+            'type': 'EncryptionAtRestWithPlatformAndCustomerKeys',
+            'diskEncryptionSetId': '/subscriptions/123/resourceGroups/AQUA-RESOURCE-GROUP/providers/Microsoft.Compute/diskEncryptionSets/test-encrypt-set'
+        }
+    },
+    {
+        'name': 'test',
+        'id': '/subscriptions/123/resourceGroups/aqua-resource-group/providers/Microsoft.Compute/disks/test',
+        'type': 'Microsoft.Compute/disks',
+        'location': 'eastus',
+        'diskState': 'Attached',
+        'encryption': {
+            'type': 'EncryptionAtRestWithPlatformKey'
+        }
+    }
+];
+
+const createCache = (disks) => {
+    const disk = {};
+    if (disks) {
+        disk['data'] = disks;
+    }
+    return {
+        disks: {
+            list: {
+                'eastus': disk
+            }
+        }
+    };
+};
+
+describe('unAttachedDiskByokEncryptionEnabled', function() {
+    describe('run', function() {
+        it('should give passing result if no disk volumes found', function(done) {
+            const cache = createCache([]);
+            unAttachedDiskByokEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('No existing disk volumes found');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give unknown result if unable to query for disk volumes', function(done) {
+            const cache = createCache();
+            unAttachedDiskByokEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(3);
+                expect(results[0].message).to.include('Unable to query for virtual machine disk volumes');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give passing result if Disk volume has BYOK encryption enabled only', function(done) {
+            const cache = createCache([disks[1]]);
+            unAttachedDiskByokEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Unattached disk volume has BYOK encryption enabled');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give passing result if Disk volume has BYOK encryption enabled along with platform key ', function(done) {
+            const cache = createCache([disks[2]]);
+            unAttachedDiskByokEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Unattached disk volume has BYOK encryption enabled');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+
+        it('should give failing result if Disk volume has BYOK encryption disabled', function(done) {
+            const cache = createCache([disks[0]]);
+            unAttachedDiskByokEncryptionEnabled.run(cache, {}, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('Unattached disk volume has BYOK encryption disabled');
+                expect(results[0].region).to.equal('eastus');
+                done();
+            });
+        });
+    });
+});
