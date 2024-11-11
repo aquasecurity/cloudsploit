a3fef907d4c902411ec59430c21d30c55afead94
AkhtarAmir <31914988+AkhtarAmir@users.noreply.github.com>
2024-11-11 16:41:51 +0500

    Enable defender for sql servers (#2102)
    * Revised enabeDefenderForSqlServers Plugin Update
    
    * Revised enabeDefenderForSqlServers Plugin Update (2)
    
    * Revised enableDefenderForSqlSever function
    
    ---------
    
    Co-authored-by: AkhtarAmir <AkhtarAmir>

--- plugins/azure/defender/enableDefenderForSqlServers.spec.js
+++ plugins/azure/defender/enableDefenderForSqlServers.spec.js
@@ -1,87 +1,127 @@
-var assert = require('assert');
-var expect = require('chai').expect;
-var auth = require('./enableDefenderForSqlServers');
+const assert = require('assert');
+const expect = require('chai').expect;
+const plugin = require('./enableDefenderForSqlServers');
 
-const createCache = (err, data) => {
+describe('enableDefenderForSqlServers', function() {
+    describe('run', function() {
+        it('should give passing result if Defender is enabled at subscription level', function(done) {
+            const cache = createCache([{
+                id: '/subscriptions/123/providers/Microsoft.Security/pricings/SqlServers',
+                name: 'SqlServers',
+                pricingTier: 'Standard'
+            }]);
+            const settings = {
+                check_level: 'subscription'
+            };
+
+            plugin.run(cache, settings, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Azure Defender is enabled for SQL Servers');
+                done();
+            });
+        }); 
+
+        it('should give failing result if Defender is not enabled at subscription level', function(done) {
+            const cache = createCache([{
+                id: '/subscriptions/123/providers/Microsoft.Security/pricings/SqlServers',
+                name: 'SqlServers',
+                pricingTier: 'Free'
+            }]);
+            const settings = {
+                check_level: 'subscription'
+            };
+
+            plugin.run(cache, settings, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('Azure Defender is not enabled for SQL Servers');
+                done();
+            });
+        });
+
+        it('should give passing result if Defender is enabled at resource level', function(done) {
+            const cache = createCache(
+                [{
+                    id: '/subscriptions/123/providers/Microsoft.Security/pricings/SqlServers',
+                    name: 'SqlServers',
+                    pricingTier: 'Free'
+                }],
+                [{
+                    id: '/subscriptions/123/servers/test-server'
+                }],
+                [{
+                    id: '/subscriptions/123/servers/test-server/security',
+                    state: 'Enabled'
+                }]
+            );
+            const settings = {
+                check_level: 'resource'
+            };
+
+            plugin.run(cache, settings, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(0);
+                expect(results[0].message).to.include('Azure Defender is enabled for SQL server');
+                done();
+            });
+        });
+
+        it('should give failing result if Defender is not enabled at resource level', function(done) {
+            const cache = createCache(
+                [{
+                    id: '/subscriptions/123/providers/Microsoft.Security/pricings/SqlServers',
+                    name: 'SqlServers',
+                    pricingTier: 'Free'
+                }],
+                [{
+                    id: '/subscriptions/123/servers/test-server'
+                }],
+                [{
+                    id: '/subscriptions/123/servers/test-server/security',
+                    state: 'Disabled'
+                }]
+            );
+            const settings = {
+                check_level: 'resource'
+            };
+
+            plugin.run(cache, settings, (err, results) => {
+                expect(results.length).to.equal(1);
+                expect(results[0].status).to.equal(2);
+                expect(results[0].message).to.include('Azure Defender is not enabled for SQL server');
+                done();
+            });
+        });
+
+        // Add other necessary test cases for error conditions
+    });
+});
+
+function createCache(pricingData, serversData, securityData) {
     return {
         pricings: {
             list: {
-                'global': {
-                    err: err,
-                    data: data
+                global: {
+                    data: pricingData
+                }
+            }
+        },
+        servers: {
+            listSql: {
+                'eastus': {
+                    data: serversData
+                }
+            }
+        },
+        serverSecurityAlertPolicies: {
+            listByServer: {
+                'eastus': {
+                    '/subscriptions/123/servers/test-server': {
+                        data: securityData
+                    }
                 }
             }
         }
-    }
-};
-
-describe('enableDefenderForSqlDatabases', function() {
-    describe('run', function() {
-        it('should give passing result if no pricings found', function(done) {
-            const callback = (err, results) => {
-                expect(results.length).to.equal(1);
-                expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('No Pricing information found');
-                expect(results[0].region).to.equal('global');
-                done()
-            };
-
-            const cache = createCache(
-                null,
-                []
-            );
-
-            auth.run(cache, {}, callback);
-        });
-
-        it('should give failing result if Azure Defender for SQL Server Databases is not enabled', function(done) {
-            const callback = (err, results) => {
-                expect(results.length).to.equal(1);
-                expect(results[0].status).to.equal(2);
-                expect(results[0].message).to.include('Azure Defender is not enabled for SQL Server Databases');
-                expect(results[0].region).to.equal('global');
-                done()
-            };
-
-            const cache = createCache(
-                null,
-                [
-                    {
-                        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/providers/Microsoft.Security/pricings/default",
-                        "name": "SqlServers",
-                        "type": "Microsoft.Security/pricings",
-                        "pricingTier": "free",
-                        "location": "global"
-                    }
-                ]
-            );
-
-            auth.run(cache, {}, callback);
-        });
-
-        it('should give passing result if Azure Defender for SQL Server Databases is enabled', function(done) {
-            const callback = (err, results) => {
-                expect(results.length).to.equal(1);
-                expect(results[0].status).to.equal(0);
-                expect(results[0].message).to.include('Azure Defender is enabled for SQL Server Databases');
-                expect(results[0].region).to.equal('global');
-                done()
-            };
-
-            const cache = createCache(
-                null,
-                [
-                    {
-                        "id": "/subscriptions/e79d9a03-3ab3-4481-bdcd-c5db1d55420a/providers/Microsoft.Security/pricings/default",
-                        "name": "SqlServers",
-                        "type": "Microsoft.Security/pricings",
-                        "pricingTier": "Standard",
-                        "location": "global"
-                    }
-                ]
-            );
-
-            auth.run(cache, {}, callback);
-        })
-    })
-}); 
+    };
+} 
