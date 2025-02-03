3523760cdf0b25fd26d4c0dce3cf3f985aa50992
AkhtarAmir <makhtar.pucit@gmail.com>
2025-02-03 10:11:43 +0000

    updated spec

--- postprocess/suppress.spec.js
+++ postprocess/suppress.spec.js
@@ -8,50 +8,20 @@ describe('create', function () {
     });
 
     it('should return the filter if matches', function () {
-        var filter = suppress.create(['*:*:n*']);
-        expect(filter('plugin:region:name')).to.equal('*:*:n*');
+        var filter = suppress.create(['plugin123:us-east-1:n*']);
+        expect(filter('plugin123:us-east-1:name')).to.equal('plugin123:us-east-1:n*');
     });
 
     it('should return the filter if matches whole word', function () {
-        var filter = suppress.create(['*:*:longer']);
-        expect(filter('plugin:region:longer')).to.equal('*:*:longer');
+        var filter = suppress.create(['plugin123:us-east-1:longer']);
+        expect(filter('plugin123:us-east-1:longer')).to.equal('plugin123:us-east-1:longer');
     });
 
     it('should return the filter if multiple and second matches', function () {
         var filter = suppress.create([
-            '*:*:first*',
-            'plugin:region:second'
+            'plugin123:us-east-1:first*',
+            'plugin456:us-west-2:second'
         ]);
-        expect(filter('plugin:region:second')).to.equal('plugin:region:second');
-    });
-
-    describe('validation', function() {
-        it('should accept valid suppression patterns', function() {
-            const validPatterns = [
-                'acmValidation:us-east-1:*',
-                'plugin_name:*:resource-123',
-                'test_plugin:region-1:certificate/123',
-                '*:*:*'
-            ];
-            expect(() => suppress.create(validPatterns)).to.not.throw();
-        });
-
-        it('should reject invalid suppression formats', function() {
-            const invalidPatterns = [
-                'invalid',                    // Missing parts
-                'too:many:parts:here',       // Too many parts
-                'invalid!:region:resource',   // Invalid characters in pluginId
-                'plugin:reg##ion:resource',   // Invalid characters in region
-                'plugin:region:res$$ource'    // Invalid characters in resourceId
-            ];
-            invalidPatterns.forEach(pattern => {
-                expect(() => suppress.create([pattern])).to.throw();
-            });
-        });
-
-        it('should reject overly long patterns', function() {
-            const longPattern = 'a'.repeat(65) + ':region:resource';
-            expect(() => suppress.create([longPattern])).to.throw();
-        });
+        expect(filter('plugin456:us-west-2:second')).to.equal('plugin456:us-west-2:second');
     });
 });
