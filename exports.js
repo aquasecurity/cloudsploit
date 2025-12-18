b711937e293dbc48ed6af73ac2827c44a61bd902
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-18 13:20:44 +0500

    neptune audit logging enabled

--- exports.js
+++ exports.js
@@ -534,6 +534,8 @@ module.exports = {
         'neptuneDBMultiAz'              : require(__dirname + '/plugins/aws/neptune/neptuneDBMultiAz.js'),
         'neptuneDbDeletionProtection'   : require(__dirname + '/plugins/aws/neptune/neptuneDbDeletionProtection.js'),
         'neptuneDBIamAuth'              : require(__dirname + '/plugins/aws/neptune/neptuneDBIamAuth.js'),
+        'neptuneAuditLoggingEnabled'    : require(__dirname + '/plugins/aws/neptune/neptuneAuditLoggingEnabled.js'),
+
 
         'monitoringMetrics'             : require(__dirname + '/plugins/aws/cloudwatchlogs/monitoringMetrics.js'),
 
